dccb5ebaf2d922f83ef9706e7b88915a0ae9d9af
José Carlos Fernández Amado <jcarlos.fernandez.work@gmail.com>
2017-01-09 14:36:30 +0100

    feat(lists/tagcloud): tagcloud component with playground
    ISSUES CLOSED: #6984

--- components/lists/tagcloud/src/index.js
+++ components/lists/tagcloud/src/index.js
@@ -2,14 +2,11 @@ import React, {PropTypes} from 'react'
 
 const ListTagcloud = ({tags, title} = {}) =>
   <div className='sui-ListTagcloud'>
-    {!!title && <h4>{title}</h4>}
-    <div className='sui-ListTagcloud-title'>
-      {tags.map(({link, label}, index) =>
-        <a key={index} href={link} className='sui-ListTagcloud-link'>
-          {label}
-        </a>
-      )}
-    </div>
+    {tags.map(({link, label}, index) =>
+      <a key={index} href={link} className='sui-ListTagcloud-link'>
+        {label}
+      </a>
+    )}
   </div>
 
 ListTagcloud.displayName = 'ListTagcloud'
@@ -28,11 +25,6 @@ ListTagcloud.propTypes = {
      */
     link: PropTypes.string.isRequired
   })).isRequired,
-
-  /**
-   * Optional string tagcloud title
-   */
-  title: PropTypes.string
 }
 
 export default ListTagcloud

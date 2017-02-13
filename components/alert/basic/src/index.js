cef4e9311802ec938989f46f28869c4471eb23bb
Joan Claret <dpam23@gmail.com>
2017-02-13 14:57:55 +0100

    feat(alert/basic): add action buttons. refactor. Add default styles.

--- components/alert/basic/src/index.js
+++ components/alert/basic/src/index.js
@@ -1,37 +1,70 @@
+/* eslint-disable react/prop-types */
 import React, {PropTypes} from 'react'
-import Info from '@schibstedspain/sui-svgiconset/lib/Info'
+import cx from 'classnames'
+import {
+  Info,
+  Bell,
+  Check
+} from '@schibstedspain/sui-svgiconset/lib/'
 
-export default function AlertBasic ({ type, children, icon: AlertIcon }) {
+const icons = {
+  'info': Info,
+  'error': Bell,
+  'success': Check
+}
+
+function _renderAction ({ handle, text, className }) {
+  const actionClassName = cx(`sui-AlertBasic-button`, {
+    [className]: !!className
+  })
+  return (
+    <button className={actionClassName} onClick={handle}>
+      {text}
+    </button>
+  )
+}
+
+function _renderActions (actions) {
+  return (
+    <div className='sui-AlertBasic-actions'>
+      {actions.map((action, index) => _renderAction(action))}
+    </div>
+  )
+}
+
+export default function AlertBasic ({
+  type,
+  children,
+  icon,
+  actions
+}) {
+  const AlertIcon = icon || icons[type]
   return (
     <div>
       <div className={`sui-AlertBasic sui-AlertBasic--${type}`}>
-        <div>
-          <AlertIcon svgClass='sui-AlertBasic-icon' />
+        <div className='sui-AlertBasic-message'>
+          <div>
+            <AlertIcon svgClass='sui-AlertBasic-icon' />
+          </div>
+          <div>{children}</div>
         </div>
-        <div>{children}</div>
+        {actions && !!actions.length && _renderActions(actions)}
       </div>
     </div>
   )
 }
 
 AlertBasic.propTypes = {
-  /**
-   * Alert type
-   */
-  type: PropTypes.oneOf(['info']),
-  /**
-   * Alert content
-   */
+  type: PropTypes.oneOf(['info', 'error', 'success']),
   children: PropTypes.element,
-  /**
-   * Alert icon
-   */
-  icon: PropTypes.function
-}
-
-AlertBasic.defaultProps = {
-  type: 'info',
-  icon: Info
+  icon: PropTypes.function,
+  actions: PropTypes.arrayOf(
+    PropTypes.shape({
+      handle: PropTypes.func,
+      text: PropTypes.string.isRequired,
+      className: PropTypes.string
+    })
+  )
 }
 
 AlertBasic.displayName = 'AlertBasic'

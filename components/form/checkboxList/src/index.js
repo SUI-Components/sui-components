4d9316871ef384e503400cee4433eda179fd32c8
José Carlos Fernández Amado <jcarlos.fernandez.work@gmail.com>
2017-02-14 09:02:22 +0100

    feat(form/checkboxList): checkbox list component

---
+++ components/form/checkboxList/src/index.js
@@ -0,0 +1,53 @@
+import React, {PropTypes} from 'react'
+import cx from 'classnames'
+
+const isChecked = ({selectedValues = [], value} = {}) => {
+  return selectedValues.indexOf(value) !== -1
+}
+
+const labelClassName = ({classNameItem, checked}) => cx('sui-FormCheckboxList-label', {
+  'is-active': checked
+})
+
+const FormCheckboxList = ({name, className, classNameItem, options, handleChange, selectedValues} = {}) =>
+  <div className='sui-FormCheckboxList'>
+    {options.map(({ value, label, checked = isChecked({selectedValues, value}) }, index) =>
+      <label key={index} className={labelClassName({classNameItem, checked})}>
+        <input
+          type='checkbox'
+          value={value}
+          checked={checked}
+          name={name}
+          onChange={handleChange}
+          className='sui-FormCheckboxList-input'
+        />
+        {label}
+      </label>
+    )}
+  </div>
+
+FormCheckboxList.displayName = 'FormCheckboxList'
+
+FormCheckboxList.propTypes = {
+  name: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  classNameItem: PropTypes.string,
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.oneOfType([
+        PropTypes.number.isRequired,
+        PropTypes.string.isRequired
+      ]),
+      label: PropTypes.string.isRequired
+    }
+  )),
+  handleChange: PropTypes.func.isRequired,
+  selectedValue: PropTypes.arrayOf(
+    PropTypes.oneOfType([
+      PropTypes.number.isRequired,
+      PropTypes.string.isRequired
+    ])
+  )
+}
+
+export default FormCheckboxList

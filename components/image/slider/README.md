e54b612029f4753c6b62a1bd73c50331104e12e6
Beto Esposito <alberto.esposito@scmspain.com>
2017-05-25 18:20:53 +0200

    feat(image/slider): simple gallery using sui-multimedia and react-slidy

---
+++ components/image/slider/README.md
@@ -0,0 +1,5 @@
+
+### ImageSlider
+Image slider gallery given a list of images.
+
+Includes sui-multimedia features.
---
+++ components/image/slider/src/index.js
@@ -0,0 +1,57 @@
+import React, {PropTypes} from 'react'
+import ReactSlidy from 'react-slidy'
+import SuiMultimedia from '@schibstedspain/sui-multimedia'
+
+const ImageSlider = (props) => {
+
+  const slides = getSlides(props.images, props.lazyLoad)
+
+  return (
+    <div className="sui-ImageSlider">
+      { hasMoreThanOneImage(props.images)
+        ? (<ReactSlidy>{ slides }</ReactSlidy>)
+        : slides
+      }
+    </div>
+  )
+}
+
+/**
+ * Says if there are more than one image in the given list.
+ * Implemented for semantic purposes.
+ * @param {Array} images List given by props.images.
+ * @return {boolean}
+ */
+const hasMoreThanOneImage = (images) => (1 < images.length)
+
+/**
+ * @param {Array} images List given by props.images.
+ * @param {boolean} lazyLoad
+ * @return {Array} List of SuiMultimedia components.
+ */
+const getSlides = (images, lazyLoad) => {
+  if (images.length) {
+    return images.map( (image, index) => {
+      return (<SuiMultimedia key={index} images={image} lazyLoad={lazyLoad} />)
+    })
+  }
+}
+
+ImageSlider.propTypes = {
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      src: PropTypes.string.isRequired,
+      alt: PropTypes.string,
+      link: PropTypes.string
+    }).isRequired
+  ),
+  lazyLoad: PropTypes.bool
+}
+
+ImageSlider.defaultProps = {
+  lazyLoad: false
+}
+
+ImageSlider.displayName = 'ImageSlider'
+
+export default ImageSlider

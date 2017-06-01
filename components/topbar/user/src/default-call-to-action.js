216e338f6ae861af8afca533503c3203cc191a5f
Ignacio Tolstoy <ignacio.tolstoy@scmspain.com>
2017-06-01 17:10:12 +0200

    feat(topbar/user): allo setting notifications in the cta

--- components/topbar/user/src/default-call-to-action.js
+++ components/topbar/user/src/default-call-to-action.js
@@ -1,10 +1,11 @@
 import React, {PropTypes} from 'react'
 
-const DefaultCallToAction = ({ text, url, icon: Icon, linkFactory: Link }) =>
+const DefaultCallToAction = ({ text, url, notifications, icon: Icon, linkFactory: Link }) =>
   <div className='sui-TopbarUser-navCTA'>
     <Link href={url} className='sui-TopbarUser-navCTALink' title={text}>
       {Icon && <Icon svgClass='sui-TopbarUser-navCTAIcon' />}
       <span>{text}</span>
+      {!!notifications && <span className='sui-TopbarUser-navCTANotifications'>{notifications}</span>}
     </Link>
   </div>
 
@@ -12,9 +13,14 @@ DefaultCallToAction.displayName = 'DefaultCallToAction'
 
 DefaultCallToAction.propTypes = {
   linkFactory: PropTypes.func.isRequired,
+  notifications: PropTypes.number,
   text: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
   icon: PropTypes.func
+}
+
+DefaultCallToAction.defaultProps = {
+  notifications: 0
 }
 
 export default DefaultCallToAction

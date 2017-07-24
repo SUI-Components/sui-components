9dedbc0e1100d8e55343ba1bfea3fff0284747bb
Beto Esposito <alberto.esposito@scmspain.com>
2017-07-24 08:48:37 +0200

    feat(cover/basic): cover with image and buttons.

---
+++ components/cover/basic/README.md
@@ -0,0 +1,4 @@
+
+### CoverBasic
+
+It shows an image an also accepts a list of buttons.
---
+++ components/cover/basic/src/index.js
@@ -0,0 +1,90 @@
+import React, {PropTypes} from 'react'
+
+const CoverBasic = (props) => {
+  const buttons = buildButtons(props)
+
+  // Custom css class is attached when component is clickable.
+  const clickableClass = isClickable(props) ? ' clickable' : ''
+
+  return (
+    <div className={'sui-CoverBasic' + clickableClass} onClick={buildClickHandler(props)}>
+      <div className={(props.gradient ? 'gradient' : '')}>
+        <img src={props.src} />
+      </div>
+      <div className='buttonList'>
+        {buttons}
+      </div>
+    </div>
+  )
+}
+
+const buildClickHandler = (props) => {
+  return (event) => {
+    isClickable(props) && props.handleClick(event)
+  }
+}
+
+const isClickable = (props) => {
+  return !!props.handleClick
+}
+
+const buildButtons = (props) => {
+  return props.buttons.map((item, index) => {
+    // Build click handler for each button
+    function handler (event) {
+      event.stopPropagation()
+      item.handleClick(event)
+    }
+
+    return (
+      <button key={index} onClick={handler}>
+        {item.svg ? item.svg : ''}
+        <span>{item.label}</span>
+      </button>
+    )
+  })
+}
+
+CoverBasic.displayName = 'CoverBasic'
+
+CoverBasic.propTypes = {
+  /**
+   * Image source.
+   */
+  src: PropTypes.string.isRequired,
+  /**
+   * If true, adds a linear gradient layer over the image.
+   */
+  gradient: PropTypes.bool,
+  /**
+   * Handles click over the whole image.
+   */
+  handleClick: PropTypes.func,
+  /**
+   * List of objects with label, svg and handleClick properties.
+   * Each object defines a button.
+   */
+  buttons: PropTypes.arrayOf(
+    PropTypes.shape({
+      /**
+       * Text to display.
+       */
+      label: PropTypes.string.isRequired,
+      /**
+       * Button behaviour.
+       */
+      handleClick: PropTypes.func.isRequired,
+      /**
+       * Icon to display on the left of the text.
+       */
+      svg: PropTypes.node
+    })
+  )
+}
+
+CoverBasic.defaultProps = {
+  buttons: [],
+  gradient: false
+}
+
+export default CoverBasic

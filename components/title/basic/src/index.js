4b11910e77c661341531c67051b1115d3f782d65
Ronaldo Medina Huerta <ronaldo.medina@AFE080.local>
2017-07-27 11:21:54 +0200

    feat(title/basic): move const function "_buildAlignClassName" outside the component.

--- components/title/basic/src/index.js
+++ components/title/basic/src/index.js
@@ -1,26 +1,22 @@
 import React, {PropTypes} from 'react'
 import cx from 'classnames'
 
-export default function TitleBasic ({align, className, subtitle, title}) {
-  /**
-   * Builds the modifier className to apply to the component following the SUIT naming convention (sui-TitleBasic--modifierInCamelCase).
-   * @param alignProp {string} property value for Title alignment.
-   * @returns {string} SUIT formatted classname.
-   */
-  function _buildAlignClassName (alignProp) {
-    let alignClassName = ''
-    if (alignProp) {
-      alignClassName = 'sui-TitleBasic--align' + alignProp.charAt(0).toUpperCase() + alignProp.slice(1)
-    }
-    return alignClassName
-  }
+/**
+ * Builds the modifier className to apply to the component following the SUIT naming convention (sui-TitleBasic--modifierInCamelCase).
+ * @param alignProp {string} property value for Title alignment.
+ * @returns {string} SUIT formatted classname.
+ */
+const _buildAlignClassName = (alignProp) => {
+  return alignProp ? 'sui-TitleBasic--align' + alignProp.charAt(0).toUpperCase() + alignProp.slice(1) : ''
+}
 
+export default function TitleBasic ({align, className, subtitle, title}) {
   const titleBasicClassName = cx('sui-TitleBasic', className, `${_buildAlignClassName(align)}`)
 
   return (
     <div className={titleBasicClassName}>
-      {title ? <h1 className={`sui-TitleBasic-title`}>{title}</h1> : null}
-      {subtitle ? <h2 className={`sui-TitleBasic-subtitle`}>{subtitle}</h2> : null}
+      {title && <h1 className={`sui-TitleBasic-title`}>{title}</h1>}
+      {subtitle && <h2 className={`sui-TitleBasic-subtitle`}>{subtitle}</h2>}
     </div>
   )
 }

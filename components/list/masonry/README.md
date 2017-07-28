3b19f7e6d762a441214327411ef4f10e28957c3f
Joan Claret <dpam23@gmail.com>
2017-07-28 11:30:35 +0200

    feat(list/masonry): init masonry component

---
+++ components/list/masonry/README.md
@@ -0,0 +1,13 @@
+### ListMasonry
+ListMasonry is a React grid layout component. It works by placing elements in optimal position based on available vertical space.
+
+```javascript
+const breakpoints = [600, 900]
+
+<ListMasonry
+  breakpoints={breakpoints}
+  className='extra-class'
+/>
+```
+
+Check the playground in order to get a complete view of the possibilities of the component. Try resizing your browser and check how this component build columns based on the breakpoints array passed via props.
---
+++ components/list/masonry/src/index.js
@@ -0,0 +1,63 @@
+import React, { PropTypes } from 'react'
+import cx from 'classnames'
+
+class ListMasonry extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {columns: 1}
+  }
+  componentDidMount () {
+    this._onResize()
+    window.addEventListener('resize', this._onResize)
+  }
+
+  _getColumns (width) {
+    return this.props.breakPoints.reduceRight((collector, current, index) => {
+      return current < width ? collector : index
+    }, this.props.breakPoints.length) + 1
+  }
+
+  _onResize = () => {
+    const columns = this._getColumns(this.refs.ListMasonry.offsetWidth)
+    if (columns !== this.state.columns) {
+      this.setState({columns: columns})
+    }
+  }
+
+  _mapChildren () {
+    let col = []
+    const numColumns = this.state.columns
+    for (let i = 0; i < numColumns; i++) {
+      col.push([])
+    }
+    return this.props.children.reduce((collector, current, index) => {
+      collector[index % numColumns].push(current)
+      return collector
+    }, col)
+  }
+
+  render () {
+    const masonryClassName = cx('sui-ListMasonry', this.props.className)
+    return (
+      <div className={masonryClassName} ref='ListMasonry'>
+        {this._mapChildren().map((col, columnIndex) => {
+          return (
+            <div className='sui-ListMasonry-column' key={columnIndex} >
+              {col.map((child, index) => {
+                return <div key={index} >{child}</div>
+              })}
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+export default ListMasonry
+
+ListMasonry.propTypes = {
+  children: PropTypes.node.isRequired,
+  breakPoints: PropTypes.array.isRequired,
+  className: PropTypes.string
+}

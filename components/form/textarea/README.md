e9459ab926e502d424322148986cf610c329f0f8
Ronaldo Medina <fr.medinah@gmail.com>
2017-09-29 11:27:55 +0200

    feat(form/textarea): first commit of the text area form element.

---
+++ components/form/textarea/README.md
@@ -0,0 +1,11 @@
+### FormTextarea
+
+Basic text-area form element.
+
+A placeholder to specify a short hint of the expected value of the text area (which will be displayed only when the text-area is empty) can be passed in ```placeholder``` property.
+
+The default height and width of the text area can be defined whether with ```cols``` and ```rows``` property, or with a custom CSS class name.
+
+The spellcheck of the inner text of the text area can be disabled (HTML5).
+
+You can pass a custom callback to be executed when the text area value is changed, through ```onChange``` property.
---
+++ components/form/textarea/src/index.js
@@ -0,0 +1,66 @@
+import React, {PropTypes} from 'react'
+import cx from 'classnames'
+
+const FormTextarea = ({className, cols, label, name, onChange, placeholder, rows, spellCheck, value}) => (
+  <div className={cx('sui-FormTextarea', className)}>
+    {label && <label className='sui-FormTextarea-label'>{label}</label>}
+    <textarea
+      className='sui-FormTextarea-element'
+      cols={cols}
+      name={name}
+      onChange={onChange}
+      placeholder={placeholder}
+      rows={rows}
+      spellCheck={spellCheck}
+      value={value} />
+  </div>
+)
+
+FormTextarea.displayName = 'FormTextarea'
+
+FormTextarea.propTypes = {
+  /**
+   * CSS classname to apply to component's container.
+   */
+  className: PropTypes.string,
+  /**
+   * Specifies the visible width of text area.
+   */
+  cols: PropTypes.number,
+  /**
+   * Title label to show above the text area element.
+   */
+  label: PropTypes.string,
+  /**
+   * Specifies a name for the text area.
+   */
+  name: PropTypes.string.isRequired,
+  /**
+   * Custom function callback to execute when the value of the text area has changed.
+   */
+  onChange: PropTypes.func,
+  /**
+   * Specifies a shot hint that describes the expected value of the text area when it is empty.
+   */
+  placeholder: PropTypes.string,
+  /**
+   * Specifies the visible number of lines in a text area.
+   */
+  rows: PropTypes.number,
+  /**
+   * Specifies whether the element is to have its spelling and grammar checked or not.
+   */
+  spellCheck: PropTypes.bool,
+  /**
+   * Text value of the text area.
+   */
+  value: PropTypes.string
+}
+
+FormTextarea.defaultProps = {
+  placeholder: '',
+  spellCheck: false,
+  value: ''
+}
+
+export default FormTextarea

0554272625c7ab775d36965b5acd49fd231b0dcc
Ronaldo Medina <fr.medinah@gmail.com>
2017-09-29 11:34:32 +0200

    feat(form/textInput): review propTypes and add doc comments.

--- components/form/textInput/src/index.js
+++ components/form/textInput/src/index.js
@@ -25,11 +25,29 @@ const FormTextInput = ({className, errorMessage, name, onChange, placeholder, va
 FormTextInput.displayName = 'FormTextInput'
 
 FormTextInput.propTypes = {
+  /**
+   * CSS classname to apply to component's container.
+   */
   className: PropTypes.string,
+  /**
+   * Error message text to show below the input.
+   */
   errorMessage: PropTypes.string,
+  /**
+   * Specifies a name for a text input.
+   */
   name: PropTypes.string.isRequired,
+  /**
+   * Custom callback function to execute when the value of the text input has changed.
+   */
   onChange: PropTypes.func,
+  /**
+   * Specifies a short hint that describes the expected value of the text input (when it is empty).
+   */
   placeholder: PropTypes.string,
+  /**
+   * Inner text value of the text input component.
+   */
   value: PropTypes.string
 }
 

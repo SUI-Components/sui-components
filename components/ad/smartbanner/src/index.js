f398876e3b8f26016e050e826e97a9a1ba989ef8
Carlos Villuendas <carlosvillu@gmail.com>
2017-10-02 15:11:52 +0200

    feat(ad/smartbanner): Migrate to PropTypes package

--- components/ad/smartbanner/src/index.js
+++ components/ad/smartbanner/src/index.js
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes} from 'react'
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
 import IconCloseDefault from '@schibstedspain/sui-svgiconset/lib/X'
 import cx from 'classnames'
 

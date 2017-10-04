b042bae257dc3ba1d6fda5b0baeea3bc7bda1a46
Carlos Villuendas <carlosvillu@gmail.com>
2017-10-04 18:19:34 +0200

    feat(tag/selectable): migrate to PropTypes package

--- components/tag/selectable/src/index.js
+++ components/tag/selectable/src/index.js
@@ -1,4 +1,5 @@
-import React, {PropTypes} from 'react'
+import PropTypes from 'prop-types'
+import React from 'react'
 import cx from 'classnames'
 import Check from '@schibstedspain/sui-svgiconset/lib/Check'
 

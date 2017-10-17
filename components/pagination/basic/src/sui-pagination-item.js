450758ba36a18ea5cd00e7b923294e1bc868fedb
Miguel Ángel Durán <miguel@miduga.es>
2017-10-17 17:15:11 +0200

    feat(pagination/basic): add new sui-pagination-basic

---
+++ components/pagination/basic/src/sui-pagination-item.js
@@ -0,0 +1,62 @@
+import React, { PropTypes } from 'react'
+import cx from 'classnames'
+
+const baseClass = 'sui-PaginationBasic'
+const PAGE_NUMBER_HOLDER = '%{pageNumber}'
+
+export default function PaginationItem (props) {
+  const getText = () => (
+    <span className={`${baseClass}-text`}>
+      {props.pageText}
+    </span>
+  )
+
+  const onClick = (e) => props.handlePaginate(props.pageNumber, e)
+
+  const itemBaseClass = `${baseClass}-item`
+  const classNames = cx(itemBaseClass, {
+    [`${itemBaseClass}--control`]: props.isControl,
+    [`${itemBaseClass}--selected`]: props.isCurrent
+  })
+
+  return (
+    <li className={classNames}>
+      { props.isCurrent
+        ? getText()
+        : props.factoryLink({
+          onClick,
+          className: `${baseClass}-link`,
+          destinationURL: props.createUrl(props),
+          pageText: props.pageText
+        })
+      }
+    </li>
+  )
+}
+
+PaginationItem.propTypes = {
+  createUrl: PropTypes.func,
+  factoryLink: PropTypes.func,
+  handlePaginate: PropTypes.func,
+  isControl: PropTypes.bool,
+  isCurrent: PropTypes.bool,
+  pageNumber: PropTypes.number.isRequired,
+  pageText: PropTypes.any.isRequired,
+  patternUrl: PropTypes.string
+}
+
+PaginationItem.defaultProps = {
+  createUrl: ({pageNumber, patternUrl}) => {
+    return patternUrl.replace(PAGE_NUMBER_HOLDER, pageNumber)
+  },
+  isCurrent: false,
+  factoryLink: ({className, destinationURL, onClick, pageText}) => { // eslint-disable-line
+    const linkProps = {className, onClick}
+    return (
+      <a {...linkProps} href={destinationURL}>
+        {pageText}
+      </a>
+    )
+  },
+  patternUrl: '#'
+}

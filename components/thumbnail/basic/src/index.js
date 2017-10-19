a7ca43e8127e02afe62b95429506460f8ff53d9a
Julian Gernun <julian.gernun@AFE0805651.local>
2017-10-19 09:53:34 +0200

    feat(thumbnail/basic): placeholder while loading image

--- components/thumbnail/basic/src/index.js
+++ components/thumbnail/basic/src/index.js
@@ -2,14 +2,31 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
 class ThumbnailBasic extends Component {
+  state = {
+    imageLoaded: false
+  }
+
+  onLoad = () => {
+    this.setState({imageLoaded: true})
+  }
+
   render () {
-    const { image, captionText } = this.props
+    const {image, captionText} = this.props
     return (
       <figure className='sui-ThumbnailBasic'>
-        <img className='sui-ThumbnailBasic-image' src={image.src} alt={image.alt} />
-        <figcaption className='sui-ThumbnailBasic-caption'>
-          <span>{captionText}</span>
-        </figcaption>
+        <img className='sui-ThumbnailBasic-image'
+          onLoad={() => this.onLoad()}
+          {...image}
+        />
+        {
+          !this.state.imageLoaded && this.props.placeholder
+        }
+        {
+          captionText &&
+            <figcaption className='sui-ThumbnailBasic-caption'>
+              <span>{captionText}</span>
+            </figcaption>
+        }
       </figure>
     )
   }
@@ -22,7 +39,8 @@ ThumbnailBasic.propTypes = {
     src: PropTypes.string.isRequired,
     alt: PropTypes.string.isRequired
   }),
-  captionText: PropTypes.string.isRequired
+  captionText: PropTypes.string,
+  placeholder: PropTypes.node
 }
 
 export default ThumbnailBasic

05a9ff4fa0fa9404ecfc5232b393659c8526d4f0
Julian Gernun <julian.gernun@infojobs.net>
2017-10-20 14:14:26 +0200

    feat(thumbnail/list): sui-image-placeholder migration

--- components/thumbnail/list/src/index.js
+++ components/thumbnail/list/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import ThumbnailBasic from '@schibstedspain/sui-thumbnail-basic'
 
-const ThumbnailList = ({items, captionText}) =>
+const ThumbnailList = ({items, captionText, placeholder}) =>
   <ul className='sui-ThumbnailList'>
     {
       items.map((item, index) =>
@@ -10,7 +10,9 @@ const ThumbnailList = ({items, captionText}) =>
           <ThumbnailBasic
             image={item.image}
             link={item.link}
+            fallbackImage={item.fallbackImage}
             captionText={captionText}
+            placeholder={placeholder}
           />
         </li>
       )
@@ -20,16 +22,18 @@ const ThumbnailList = ({items, captionText}) =>
 ThumbnailList.displayName = 'ThumbnailList'
 
 ThumbnailList.propTypes = {
+  /**
+   * array of thumbnail/basic images
+   */
+  items: PropTypes.any.isRequired,
+  /**
+   * see thumbnail/basic for description
+   */
   captionText: PropTypes.string.isRequired,
-  items: PropTypes.arrayOf(
-    PropTypes.shape({
-      image: PropTypes.shape({
-        alt: PropTypes.string.isRequired,
-        src: PropTypes.string.isRequired
-      }),
-      link: PropTypes.string
-    })
-  )
+  /**
+   * see thumbnail/basic for description
+   */
+  placeholder: PropTypes.node.isRequired
 }
 
 ThumbnailList.defaultProps = {

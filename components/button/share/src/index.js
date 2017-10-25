437833a571e194efae534842cb76a71819c1e51c
Carles Nuñez <carles.nunez@schibsted.com>
2017-10-25 18:52:14 +0200

    feat(button/share): added icons and refactorized some code, also added 1 comment
    Refactorized the const shareDefinitionByType to have an Icon definition per share type.

--- components/button/share/src/index.js
+++ components/button/share/src/index.js
@@ -1,48 +1,69 @@
-import React, { PropTypes } from 'react'
+import React, {PropTypes} from 'react'
 import cx from 'classnames'
+import { Socialtwitter, Socialfacebook, Commentsquare, Envelopeclosed } from '@schibstedspain/sui-svgiconset'
 
-function ButtonShare ({ type, icon, onClick, shareText, url }) {
+function ButtonShare ({type, icon, onClick, shareText, url}) {
   const parseHttpSchemeOfUrl = (urlToEval) => {
     const isUrlValid = urlToEval.match(/http:\/\/|https:\/\//)
     return !isUrlValid ? `http://${urlToEval}` : urlToEval
   }
 
-  const getOnClickHandleByType = (type) => {
-    const httpSchemeParseUrl = parseHttpSchemeOfUrl(url)
+  const parsedShareUrl = parseHttpSchemeOfUrl(url)
 
-    const urlShareDefinitionByType = {
-      facebook: `https://www.facebook.com/share.php?u=${httpSchemeParseUrl}?opi=140&tti=1&pagination=1&RowGrid=2`,
-      twitter: `http://twitter.com/share?url=${httpSchemeParseUrl}&tti=1&pagination=1&RowGrid=2&text=${shareText}`,
-      whatsapp: `whatsapp://send?text=${shareText} - ${httpSchemeParseUrl}`
+  const shareDefinitionByType = {
+    facebook: {
+      src: `https://www.facebook.com/share.php?u=${parsedShareUrl}?opi=140&tti=1&pagination=1&RowGrid=2`,
+      icon: Socialfacebook
+    },
+    twitter: {
+      src: `http://twitter.com/share?url=${parsedShareUrl}&tti=1&pagination=1&RowGrid=2&text=${shareText}`,
+      icon: Socialtwitter
+    },
+    whatsapp: {
+      src: `whatsapp://send?text=${shareText} - ${parsedShareUrl}`,
+      icon: Commentsquare
+    },
+    email: {
+      src: '',
+      icon: Envelopeclosed
     }
-
-    return () => window.open(urlShareDefinitionByType[type])
   }
 
+  const getOnClickHandleByType = type => () => window.open(shareDefinitionByType[type].src)
+
+  const Icon = icon || shareDefinitionByType[type].icon
   return <button
     className={cx('sui-ButtonShare', `type-${type}`)}
     onClick={!onClick ? getOnClickHandleByType(type) : onClick}
-  />
+  >
+    <Icon
+      svgClass='sui-ButtonShare-svgIcon'
+      fillColor={false}
+    />
+  </button>
 }
 
 ButtonShare.displayName = 'ButtonShare'
 
 ButtonShare.propTypes = {
-  /*
+  /**
    * One of the enum types ['facebook', 'whatsapp', 'twitter', 'email']
    * used to know which social media button is going to be rendered
    */
   type: PropTypes.oneOf(['facebook', 'whatsapp', 'twitter', 'email']).isRequired,
-  icon: PropTypes.string,
-  /*
+  /**
+   * A class definition, not instance, of our react icon component.
+   */
+  icon: PropTypes.func,
+  /**
    * Callback function to be called onClick button. Default type onClick is handled if no onClick is provided
    */
   onClick: PropTypes.func,
-  /*
+  /**
    * The text to put as message on the share. Only twitter and whatsapp have this feature.
    */
   shareText: PropTypes.string,
-  /*
+  /**
    * A valid scheme url. Starting with http:// or https:// if non protocol is provided the component will fall setting http:// by default
    */
   url: PropTypes.string

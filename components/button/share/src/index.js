505391e3660621488807933e1edac4754f3be62b
Carles Nuñez <carles.nunez@schibsted.com>
2017-10-26 12:56:11 +0200

    feat(button/share): refactorized methods to be out of render scope. Added custom option

--- components/button/share/src/index.js
+++ components/button/share/src/index.js
@@ -1,40 +1,47 @@
 import React, {PropTypes} from 'react'
 import cx from 'classnames'
-import { Socialtwitter, Socialfacebook, Commentsquare, Envelopeclosed } from '@schibstedspain/sui-svgiconset'
+import {Socialtwitter, Socialfacebook, Commentsquare, Envelopeclosed} from '@schibstedspain/sui-svgiconset'
 
-function ButtonShare ({type, icon, onClick, shareText, url}) {
-  const parseHttpSchemeOfUrl = (urlToEval) => {
-    const isUrlValid = urlToEval.match(/http:\/\/|https:\/\//)
-    return !isUrlValid ? `http://${urlToEval}` : urlToEval
+const getOnClickHandle = src => () => window.open(src)
+
+const parseHttpSchemeOfUrl = (urlToEval) => {
+  const isUrlValid = urlToEval.match(/http:\/\/|https:\/\//)
+  return !isUrlValid ? `http://${urlToEval}` : urlToEval
+}
+
+const getShareDefinition = (url, shareText, type) => {
+  const parsedShareUrl = url && parseHttpSchemeOfUrl(url)
+  switch (type) {
+    case 'facebook':
+      return {
+        src: `https://www.facebook.com/share.php?u=${parsedShareUrl}?opi=140&quote=${shareText}&tti=1&pagination=1&RowGrid=2`,
+        icon: Socialfacebook
+      }
+    case 'twitter':
+      return {
+        src: `http://twitter.com/share?url=${parsedShareUrl}&tti=1&pagination=1&RowGrid=2&text=${shareText}`,
+        icon: Socialtwitter
+      }
+    case 'whatsapp':
+      return {
+        src: `whatsapp://send?text=${shareText} - ${parsedShareUrl}`,
+        icon: Commentsquare
+      }
+    case 'email':
+      return {
+        icon: Envelopeclosed
+      }
+    default:
+      return {}
   }
+}
 
-  const parsedShareUrl = parseHttpSchemeOfUrl(url)
-
-  const shareDefinitionByType = {
-    facebook: {
-      src: `https://www.facebook.com/share.php?u=${parsedShareUrl}?opi=140&quote=${shareText}&tti=1&pagination=1&RowGrid=2`,
-      icon: Socialfacebook
-    },
-    twitter: {
-      src: `http://twitter.com/share?url=${parsedShareUrl}&tti=1&pagination=1&RowGrid=2&text=${shareText}`,
-      icon: Socialtwitter
-    },
-    whatsapp: {
-      src: `whatsapp://send?text=${shareText} - ${parsedShareUrl}`,
-      icon: Commentsquare
-    },
-    email: {
-      src: '',
-      icon: Envelopeclosed
-    }
-  }
-
-  const getOnClickHandleByType = type => () => window.open(shareDefinitionByType[type].src)
-
-  const Icon = icon || shareDefinitionByType[type].icon
+const ButtonShare = ({type, icon, onClick, shareText, url}) => {
+  const shareDefinition = getShareDefinition(url, shareText, type)
+  const Icon = icon || shareDefinition.icon
   return <button
-    className={cx('sui-ButtonShare', `type-${type}`)}
-    onClick={!onClick ? getOnClickHandleByType(type) : onClick}
+    className={cx('sui-ButtonShare', `sui-ButtonShare--${type}`)}
+    onClick={!onClick ? getOnClickHandle(shareDefinition.src) : onClick}
   >
     <Icon
       svgClass='sui-ButtonShare-svgIcon'
@@ -50,7 +57,7 @@ ButtonShare.propTypes = {
    * One of the enum types ['facebook', 'whatsapp', 'twitter', 'email']
    * used to know which social media button is going to be rendered
    */
-  type: PropTypes.oneOf(['facebook', 'whatsapp', 'twitter', 'email']).isRequired,
+  type: PropTypes.oneOf(['facebook', 'whatsapp', 'twitter', 'email', 'custom']).isRequired,
   /**
    * A class definition, not instance, of our react icon component.
    */

0e38cfd75ea7570786df1b81ce87066c1866963e
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2017-10-26 08:47:18 +0200

    feat(topbar/user): propagate link factory to dropdown basic

--- components/topbar/user/src/index.js
+++ components/topbar/user/src/index.js
@@ -134,6 +134,7 @@ class TopbarUser extends Component {
         button={{ icon, text, arrowButtonIcon }}
         menu={menu}
         expandOnMouseOver={isToggleHidden}
+        linkFactory={this.props.linkFactory}
       />
     )
   }

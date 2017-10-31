793bb79a31c23b290c6a7b4b95398e6ada7a2a8e
Carles Nuñez <carles.nunez@schibsted.com>
2017-10-31 11:48:56 +0100

    feat(map/basic): first step, component creation

---
+++ components/map/basic/README.md
@@ -0,0 +1,23 @@
+# MapBasic
+
+> Description
+
+<!-- ![](./assets/preview.png) -->
+
+## Installation
+
+```sh
+$ npm install @schibstedspain/sui-map-basic --save
+```
+
+## Usage
+
+### Basic usage
+```js
+import MapBasic from '@schibstedspain/sui-map-basic'
+
+return (<MapBasic />)
+```
+
+
+> **Find full description and more examples in the [demo page](#).**
---
+++ components/map/basic/src/index.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+class MapBasic extends Component {
+  render () {
+    return (
+      <div className='sui-MapBasic'>
+        <h1>MapBasic</h1>
+      </div>
+    )
+  }
+}
+
+MapBasic.displayName = 'MapBasic'
+
+// Remove these comments if you need
+// MapBasic.contextTypes = {i18n: React.PropTypes.object}
+// MapBasic.propTypes = {}
+// MapBasic.defaultProps = {}
+
+export default MapBasic

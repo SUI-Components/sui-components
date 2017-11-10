0f1d7267fa1ea12d1e0256c55212a104aacde4f9
Julian Gernun <julian.gernun@infojobs.net>
2017-11-10 12:29:36 +0100

    feat(atom/imagePanel): alignment constants defined

--- components/atom/imagePanel/src/index.js
+++ components/atom/imagePanel/src/index.js
@@ -1,10 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import classnames from 'classnames'
 
-const AtomImagePanel = function ({children}) {
+const H_ALIGNMENTS = {
+  LEFT: 'left',
+  CENTER: 'center',
+  RIGHT: 'right'
+}
+
+const V_ALIGNMENTS = {
+  TOP: 'top',
+  CENTER: 'center',
+  BOTTOM: 'bottom'
+}
+
+const getClassNames = function (classNames) {
+  return classnames('sui-AtomImagePanel', classNames)
+}
+
+const getBackgroundImage = function (backgroundImage) {
+  return {
+    backgroundImage: `url(${backgroundImage})`
+  }
+}
+
+const AtomImagePanel = function ({backgroundImage, children, className, vAligment}) {
   return (
-    <div className='sui-AtomImagePanel'>
-      <h1>AtomImagePanel</h1>
+    <div className={getClassNames({className})} style={getBackgroundImage(backgroundImage)}>
       {children}
     </div>
   )
@@ -13,8 +35,14 @@ const AtomImagePanel = function ({children}) {
 AtomImagePanel.displayName = 'AtomImagePanel'
 
 AtomImagePanel.propTypes = {
-  children: PropTypes.node
+  backgroundImage: PropTypes.string.isRequired,
+  children: PropTypes.node,
+  className: PropTypes.string,
+  hAlignment: PropTypes.oneOf(Object.values(H_ALIGNMENTS)),
+  vAligment: PropTypes.oneOf(Object.values(V_ALIGNMENTS))
 }
-// AtomImagePanel.defaultProps = {}
 
 export default AtomImagePanel
+export {
+  H_ALIGNMENTS, V_ALIGNMENTS
+}

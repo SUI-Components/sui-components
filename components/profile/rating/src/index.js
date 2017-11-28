a8833d9d0fc1c0acee9eee3744e7e04747e69e70
Carles Nuñez Tomeo <carles.nunez@AFE0803843.local>
2017-11-28 19:55:07 +0100

    feat(profile/rating): added main logic, styles, calcs, readme and api doc to the first version

--- components/profile/rating/src/index.js
+++ components/profile/rating/src/index.js
@@ -56,12 +56,33 @@ class ProfileRating extends Component {
 ProfileRating.displayName = 'ProfileRating'
 
 ProfileRating.propTypes = {
+  /**
+   * The rating of our user, if our user have 6/10 stars we should pass 6 you can pass it with decimals too. 6.5, 7, 7.5...
+   */
   rating: PropTypes.number.isRequired,
+  /**
+   * The max value that our rating have. For example if it have 10 max star we should put 10.
+   */
   maxValue: PropTypes.number.isRequired,
+  /**
+   * Our Icon to be used as a rating
+   */
   icon: PropTypes.func.isRequired,
+  /**
+   * Our icon size, this one is important to make width calcs on the mask
+   */
   iconSize: PropTypes.number.isRequired,
+  /**
+   * A margin will be added to our we set this to a number(px),
+   */
   spacingBetween: PropTypes.number,
+  /**
+   * The color used for the filled mask. If you don't provide any color, it will fallback to our component scss variable $fill-profile-rating-filled
+   */
   fillColor: PropTypes.string,
+  /**
+   * The color used for the empty mask. If you don't provide any color, it will fallback to our component scss variable $fill-profile-rating-empty
+   */
   emptyColor: PropTypes.string
 }
 

01be8a88c0f69b87c2eecc68251199a4a2ab26bc
Julian Gernun <julian.gernun@infojobs.net>
2018-01-23 17:17:58 +0100

    feat(atom/spinner): delayed loading + playground examples

--- components/atom/spinner/src/index.js
+++ components/atom/spinner/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import cx from 'classnames'
 
@@ -7,14 +7,33 @@ const TYPES = {
   SECTION: 'section'
 }
 
-class AtomSpinner extends React.Component {
-  getLayerClassName ({type, hidden}) {
-    const typeClass = type === TYPES.SECTION
-      ? 'sui-atom-spinner-layer'
-      : 'sui-atom-spinner-layer-full'
+const DELAY = 500 // ms
+
+class AtomSpinner extends Component {
+  state = {
+    waiting: this.props.delayed
+  }
+
+  componentDidMount () {
+    if (!this.state.waiting) return
+
+    this.timer = setTimeout(() => {
+      this.setState({waiting: false})
+    }, DELAY)
+  }
+
+  componentWillUnmount () {
+    clearTimeout(this.timer)
+  }
+
+  getLayerClassName () {
+    const BASE_CLASS = 'sui-atom-spinner-layer'
+    const {type, on} = this.props
+    const {waiting} = this.state
+
     return cx(
-      typeClass,
-      hidden && 'sui-atom-spinner-layer-hide'
+      type === TYPES.SECTION ? BASE_CLASS : `${BASE_CLASS}-full`,
+      (!waiting && on) || `${BASE_CLASS}-hide`
     )
   }
 
@@ -37,7 +56,15 @@ AtomSpinner.displayName = 'AtomSpinner'
 AtomSpinner.propTypes = {
   children: PropTypes.node,
   type: PropTypes.string,
-  hidden: PropTypes.bool
+  on: PropTypes.bool,
+  delayed: PropTypes.bool,
+  contentLoaded: PropTypes.bool
+}
+
+AtomSpinner.defaultProps = {
+  on: true,
+  delayed: false,
+  type: TYPES.SECTION
 }
 
 export default AtomSpinner

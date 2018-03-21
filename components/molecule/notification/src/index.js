7da2e9f5da1e213ebfa4f973971f302325094411
Daniel Muñoz Ruiz <damuru88@gmail.com>
2018-03-21 12:23:11 +0100

    feat(molecule/notification): add close function and flexbox

--- components/molecule/notification/src/index.js
+++ components/molecule/notification/src/index.js
@@ -3,22 +3,32 @@ import PropTypes from 'prop-types'
 import Button from '@schibstedspain/sui-atom-button'
 import IconClose from '@schibstedspain/sui-svgiconset/lib/X'
 import IconSuccess from '@schibstedspain/sui-svgiconset/lib/Circlecheck'
+import IconError from '@schibstedspain/sui-svgiconset/lib/Circlex'
 import IconInfo from '@schibstedspain/sui-svgiconset/lib/Info'
 import IconWarning from '@schibstedspain/sui-svgiconset/lib/Warning'
 
 const baseClass = 'sui-MoleculeNotification'
 const ICONS = {
   info: <IconInfo svgClass={`${baseClass}-icon`} />,
+  error: <IconError svgClass={`${baseClass}-icon`} />,
   success: <IconSuccess svgClass={`${baseClass}-icon`} />,
   system: <IconInfo svgClass={`${baseClass}-icon`} />,
   warning: <IconWarning svgClass={`${baseClass}-icon`} />
 }
 
 class MoleculeNotification extends Component {
-  state = { show: false }
+  state = { show: true }
+
+  handleClose = () => {
+    const show = !this.state.show
+    this.setState({ show })
+  }
 
   render () {
     const { type, text, buttons } = this.props
+    const { show } = this.state
+
+    if (!show) { return null }
 
     return (
       <div className={`${baseClass} ${baseClass}--${type}`}>
@@ -26,14 +36,19 @@ class MoleculeNotification extends Component {
           <div className={`${baseClass}-iconLeft`}>
             { ICONS[type] }
           </div>
+          <div className={`${baseClass}-text`}>
+            <span>{text}</span>
+          </div>
           <div className={`${baseClass}-close`} onClick={this.handleClose}>
             <IconClose svgClass={`${baseClass}-icon`} />
           </div>
-          <span className={`${baseClass}-text`}>{text}</span>
         </div>
-        <div className={`${baseClass}-buttonContainer`}>
-          {buttons && buttons.map(button => <Button {...button} />)}
-        </div>
+        {
+          buttons &&
+          <div className={`${baseClass}-buttonContainer`}>
+            {buttons && buttons.map(button => <Button {...button} />)}
+          </div>
+        }
       </div>
     )
   }

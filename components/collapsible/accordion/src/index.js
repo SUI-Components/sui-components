f472b351fb51bace36e9beb7153f3426263494e3
davidbarna <davidbarna@gmail.com>
2018-05-25 09:57:24 +0200

    refactor(collapsible/accordion): format with new sui-lint rules

--- components/collapsible/accordion/src/index.js
+++ components/collapsible/accordion/src/index.js
@@ -1,51 +1,58 @@
 import PropTypes from 'prop-types'
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import CollapsibleBasic from '@schibstedspain/sui-collapsible-basic'
 
 class CollapsibleAccordion extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {items: props.items}
     this._handleClick = this._handleClick.bind(this)
     this._collapseItems = this._collapseItems.bind(this)
   }
 
-  componentWillMount () {
-    this._collapseItems(false, this.props.items.findIndex(item => !item.collapsed))
+  componentWillMount() {
+    this._collapseItems(
+      false,
+      this.props.items.findIndex(item => !item.collapsed)
+    )
   }
 
-  componentWillReceiveProps (nextProps) {
-    const nextOpenIndex = nextProps.items.findIndex(function (item) {
+  componentWillReceiveProps(nextProps) {
+    const nextOpenIndex = nextProps.items.findIndex(function(item) {
       return !item.collapsed
     })
-    this.setState({ openIndex: nextOpenIndex })
+    this.setState({openIndex: nextOpenIndex})
   }
 
-  _handleClick (id) {
+  _handleClick(id) {
     return collapsed => this._collapseItems(collapsed, id)
   }
 
-  _collapseItems (collapsed, id) {
+  _collapseItems(collapsed, id) {
     this._setOpenIndex(id)
     this.props.onItemChange(collapsed, id)
   }
 
-  _setOpenIndex (id) {
-    this.setState({ openIndex: id === this.state.openIndex ? null : id })
+  _setOpenIndex(id) {
+    this.setState({openIndex: id === this.state.openIndex ? null : id})
   }
 
-  render () {
-    const { items } = this.props
-    const { openIndex } = this.state
+  render() {
+    const {items} = this.props
+    const {openIndex} = this.state
     return (
       <div>
-        {
-          items.map((item, index) => (
-            <CollapsibleBasic key={index} {...item} collapsed={openIndex !== index} handleClick={this._handleClick(index)} icon={this.props.icon}>
-              {item.content}
-            </CollapsibleBasic>
-          ))
-        }
+        {items.map((item, index) => (
+          <CollapsibleBasic
+            key={index}
+            {...item}
+            collapsed={openIndex !== index}
+            handleClick={this._handleClick(index)}
+            icon={this.props.icon}
+          >
+            {item.content}
+          </CollapsibleBasic>
+        ))}
       </div>
     )
   }
@@ -65,20 +72,22 @@ CollapsibleAccordion.propTypes = {
   /**
    * Items array
    */
-  items: PropTypes.arrayOf(PropTypes.shape({
-    /**
-     * label to be displayed.
-     */
-    label: PropTypes.node.isRequired,
-    /**
-     * children to be displayed when expanding component.
-     */
-    content: PropTypes.node.isRequired,
-    /**
-     * first state.
-     */
-    collapsed: PropTypes.bool
-  })).isRequired
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      /**
+       * label to be displayed.
+       */
+      label: PropTypes.node.isRequired,
+      /**
+       * children to be displayed when expanding component.
+       */
+      content: PropTypes.node.isRequired,
+      /**
+       * first state.
+       */
+      collapsed: PropTypes.bool
+    })
+  ).isRequired
 }
 
 CollapsibleAccordion.defaultProps = {

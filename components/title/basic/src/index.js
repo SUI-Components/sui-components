2f5d707388685846cd3e77e3cacb8c1c5bf22d4c
davidbarna <davidbarna@gmail.com>
2018-05-31 17:45:51 +0200

    refactor(title/basic): format js with new rules

--- components/title/basic/src/index.js
+++ components/title/basic/src/index.js
@@ -3,18 +3,22 @@ import React from 'react'
 import cx from 'classnames'
 
 const TITLE_ALIGN_CLASSNAMES = {
-  'left': 'sui-TitleBasic--alignLeft',
-  'center': 'sui-TitleBasic--alignCenter',
-  'right': 'sui-TitleBasic--alignRight'
+  left: 'sui-TitleBasic--alignLeft',
+  center: 'sui-TitleBasic--alignCenter',
+  right: 'sui-TitleBasic--alignRight'
 }
 
-export default function TitleBasic ({align, className, subtitle, title}) {
-  const titleBasicClassName = cx('sui-TitleBasic', className, TITLE_ALIGN_CLASSNAMES[align])
+export default function TitleBasic({align, className, subtitle, title}) {
+  const titleBasicClassName = cx(
+    'sui-TitleBasic',
+    className,
+    TITLE_ALIGN_CLASSNAMES[align]
+  )
 
   return (
     <div className={titleBasicClassName}>
-      <h1 className='sui-TitleBasic-title'>{title}</h1>
-      {subtitle && <h2 className='sui-TitleBasic-subtitle'>{subtitle}</h2>}
+      <h1 className="sui-TitleBasic-title">{title}</h1>
+      {subtitle && <h2 className="sui-TitleBasic-subtitle">{subtitle}</h2>}
     </div>
   )
 }

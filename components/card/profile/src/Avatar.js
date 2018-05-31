3ac2b47cee2cbc5ea6c4bfd4e1c86299f9579fed
davidbarna <davidbarna@gmail.com>
2018-05-31 17:45:48 +0200

    refactor(card/profile): format js with new rules

--- components/card/profile/src/Avatar.js
+++ components/card/profile/src/Avatar.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Avatar = ({ profilePic }) => <div className='sui-CardProfile-avatarHalo'>
-  <div className='sui-CardProfile-avatarRadius'>
-    <img className='sui-CardProfile-avatarImage' src={profilePic} />
+const Avatar = ({profilePic}) => (
+  <div className="sui-CardProfile-avatarHalo">
+    <div className="sui-CardProfile-avatarRadius">
+      <img className="sui-CardProfile-avatarImage" src={profilePic} />
+    </div>
   </div>
-</div>
+)
 
 Avatar.propTypes = {
   profilePic: PropTypes.string.isRequired

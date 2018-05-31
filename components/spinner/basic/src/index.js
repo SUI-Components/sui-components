63e448a21bb3c7d7af4be4076bf379b468fed924
davidbarna <davidbarna@gmail.com>
2018-05-31 17:45:51 +0200

    refactor(spinner/basic): format js with new rules

--- components/spinner/basic/src/index.js
+++ components/spinner/basic/src/index.js
@@ -2,26 +2,19 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import cx from 'classnames'
 
-export default function SpinnerBasic ({ size }) {
-  const className = cx(
-    'sui-SpinnerBasic',
-    `sui-SpinnerBasic--${size}`
-  )
+export default function SpinnerBasic({size}) {
+  const className = cx('sui-SpinnerBasic', `sui-SpinnerBasic--${size}`)
 
   return (
     <span className={className}>
-      <span className='sui-SpinnerBasic-circle sui-SpinnerBasic-circleBackground' />
-      <span className='sui-SpinnerBasic-circle sui-SpinnerBasic-circleArc' />
+      <span className="sui-SpinnerBasic-circle sui-SpinnerBasic-circleBackground" />
+      <span className="sui-SpinnerBasic-circle sui-SpinnerBasic-circleArc" />
     </span>
   )
 }
 
 SpinnerBasic.propTypes = {
-  size: PropTypes.oneOf([
-    'small',
-    'medium',
-    'large'
-  ])
+  size: PropTypes.oneOf(['small', 'medium', 'large'])
 }
 
 SpinnerBasic.defaultProps = {

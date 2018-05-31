1a4bb021b85befedd7584ae51a573ee666dc021a
davidbarna <davidbarna@gmail.com>
2018-05-31 17:45:47 +0200

    refactor(atom/panel): format js with new rules

--- components/atom/panel/src/ColorPanel.js
+++ components/atom/panel/src/ColorPanel.js
@@ -3,19 +3,13 @@ import PropTypes from 'prop-types'
 import cx from 'classnames'
 import {ALPHA, COLORS} from './constants'
 
-const getClassNames = function ({color, alpha}) {
+const getClassNames = function({color, alpha}) {
   const COLOR_PANEL_CLASS = 'sui-atom-panel-color'
-  return cx(
-    `${COLOR_PANEL_CLASS}--${color}-${alpha}`
-  )
+  return cx(`${COLOR_PANEL_CLASS}--${color}-${alpha}`)
 }
 
-const ColorPanel = function ({children, ...props}) {
-  return (
-    <div className={getClassNames(props)} >
-      {children}
-    </div>
-  )
+const ColorPanel = function({children, ...props}) {
+  return <div className={getClassNames(props)}>{children}</div>
 }
 
 ColorPanel.displayName = 'ColorPanel'

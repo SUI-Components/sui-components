a3a68c71ad42acf6ef1f915a5ab0222e92807c69
JuanMa Garrido <juanma.garrido@gmail.com>
2018-06-08 11:58:26 +0200

    feat(atom/tooltip): first Iteration Tooltip based on reactstrap Tooltip

--- components/atom/tooltip/src/index.js
+++ components/atom/tooltip/src/index.js
@@ -1,17 +1,50 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import { Tooltip } from 'reactstrap'
+// import PropTypes from 'prop-types'
 
 const BASE_CLASS = 'sui-AtomTooltip'
 
 class AtomTooltip extends Component {
+  state = {
+    isOpen: false
+  }
+
+  componentDidMount () {
+    document.addEventListener('click', () => {
+      const { isOpen } = this.state
+      if (isOpen) this.toggle()
+    })
+  }
+
+  // TO-DO: removeListener on unmount
+
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
+
   render () {
     return (
-      <div className={BASE_CLASS}>
-        <h1>AtomTooltip</h1>
-      </div>
+      <Tooltip
+        {...this.props}
+        isOpen={this.state.isOpen}
+        toggle={this.toggle}
+        className={BASE_CLASS}
+        innerClassName={`${BASE_CLASS}-inner`}
+        placementPrefix={`${BASE_CLASS}-`}
+      />
     )
   }
 }
 
 AtomTooltip.displayName = 'AtomTooltip'
+
+AtomTooltip.defaultProps = {
+  delay: {
+    show: 300,
+    hide: 1500
+  }
+}
 
 export default AtomTooltip

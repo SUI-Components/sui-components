da2f865c91596e10f49c6c42b408c54c728aec4e
JuanMa Garrido <juanma.garrido@gmail.com>
2018-06-11 12:48:34 +0200

    feat(atom/tooltip): added more use cases according to design feedback

--- components/atom/tooltip/src/index.js
+++ components/atom/tooltip/src/index.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
+import cx from 'classnames'
 import { Tooltip } from 'reactstrap'
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 
 const BASE_CLASS = 'sui-AtomTooltip'
 
 class AtomTooltip extends Component {
   state = {
     isOpen: false
+  }
+
+  get classNames () {
+    const { className, noarrow } = this.props
+    return cx(BASE_CLASS, className, noarrow && `no-arrow`)
   }
 
   componentDidMount () {
@@ -34,7 +40,7 @@ class AtomTooltip extends Component {
         {...this.props}
         isOpen={this.state.isOpen}
         toggle={this.toggle}
-        className={BASE_CLASS}
+        className={this.classNames}
         innerClassName={`${BASE_CLASS}-inner`}
         placementPrefix={`${BASE_CLASS}-`}
       />
@@ -44,11 +50,27 @@ class AtomTooltip extends Component {
 
 AtomTooltip.displayName = 'AtomTooltip'
 
+AtomTooltip.propTypes = {
+  className: PropTypes.string,
+
+  /** Wether to show arrow or not */
+  noarrow: PropTypes.bool,
+
+  /** Offset applied to the tooltip according to https://popper.js.org/popper-documentation.html#modifiers..offset */
+  offset: PropTypes.string
+}
+
+// AtomTooltip.defaultProps = {
+//   modifiers: {
+//     offset: {
+//       offset: 'auto 4px',
+//       enabled: true
+//     }
+//   }
+// }
+
 AtomTooltip.defaultProps = {
-  delay: {
-    show: 300,
-    hide: 1500
-  }
+  offset: 'auto 4px'
 }
 
 export default AtomTooltip

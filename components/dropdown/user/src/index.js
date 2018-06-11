f3b272e4ca48efb69bbaac285e7eccfc8ea31404
Joan Claret <dpam23@gmail.com>
2018-06-11 16:36:38 +0200

    feat(dropdown/user): sending needlogin to the link factory

--- components/dropdown/user/src/index.js
+++ components/dropdown/user/src/index.js
@@ -32,12 +32,16 @@ class DropdownUser extends Component {
     })
   }
 
-  _renderLink = ({text, url, icon: Icon, notifications}, index) => {
+  _renderLink = ({text, url, icon: Icon, notifications, needLogin}, index) => {
     const Link = this.props.linkFactory
-
     return (
       <li key={`${text}-${index}`} className="sui-DropdownUserMenu-listItem">
-        <Link href={url} className="sui-DropdownUserMenu-listLink" title={text}>
+        <Link
+          href={url}
+          className="sui-DropdownUserMenu-listLink"
+          title={text}
+          needLogin={needLogin}
+        >
           <Icon svgClass="sui-DropdownUserMenu-listIcon" />
           <span className="sui-DropdownUserMenu-listText">{text}</span>
           {!!notifications && (

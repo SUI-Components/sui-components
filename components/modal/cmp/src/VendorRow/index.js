7e4ce06001cbc4b38e95bc581a6595488cff0e07
Miguel Ángel Durán <miduga@gmail.com>
2018-06-12 13:30:03 +0200

    feat(modal/cmp): create vendor row to simplify component

--- components/modal/cmp/src/VendorRow/index.js
+++ components/modal/cmp/src/VendorRow/index.js
@@ -3,14 +3,16 @@ import PropTypes from 'prop-types'
 
 import AtomSwitch from '@s-ui/react-atom-switch'
 
-export const VendorRow = ({ enabled, id, handleToggleVendorStatus, title }) => (
-  <tr className='sui-ModalCmp-vendorsRow'>
-    <td className='sui-ModalCmp-vendorsRow-title'>{title}</td>
-    <td className='sui-ModalCmp-vendorsRow-actions'>
+export const VendorRow = ({enabled, id, handleToggleVendorStatus, title}) => (
+  <tr className="sui-ModalCmp-vendorsRow">
+    <td className="sui-ModalCmp-vendorsRow-title">{title}</td>
+    <td className="sui-ModalCmp-vendorsRow-actions">
       <AtomSwitch
         disabled={!enabled}
-        onToggle={_ => handleToggleVendorStatus({ enabled, id })}
-        type='toggle'
+        labelLeft=""
+        labelRight=""
+        onToggle={_ => handleToggleVendorStatus({enabled, id})}
+        type="toggle"
       />
     </td>
   </tr>

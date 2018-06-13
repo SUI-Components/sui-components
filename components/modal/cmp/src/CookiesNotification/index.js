204ed4130963dbd383537d0552606a17b991549a
Miguel Ángel Durán <miduga@gmail.com>
2018-06-13 09:56:31 +0200

    feat(modal/cmp): create separated components for modal and notification

---
+++ components/modal/cmp/src/CookiesNotification/index.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import Notification from '@s-ui/react-molecule-notification'
+
+export class CookiesNotification extends Component {
+  render() {
+    const {buttons} = this.props
+    return (
+      <Notification
+        buttons={buttons}
+        position="bottom"
+        text="Utilizamos &quot;cookies&quot; propias y de terceros para elaborar información estadística y mostrarle publicidad personalizada a través del análisis de su navegación. Si continúa navegando acepta su uso."
+        show
+      />
+    )
+  }
+}
+
+CookiesNotification.propTypes = {
+  buttons: PropTypes.array
+}

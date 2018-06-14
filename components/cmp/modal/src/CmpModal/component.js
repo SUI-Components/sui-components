281c64d2ce58a36df2e3c2b870e44b1c23b7e814
Miguel Ángel Durán <miduga@gmail.com>
2018-06-14 20:01:01 +0200

    feat(cmp/modal): show real data

--- components/cmp/modal/src/CmpModal/component.js
+++ components/cmp/modal/src/CmpModal/component.js
@@ -1,22 +1,50 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import Button from '@schibstedspain/sui-atom-button'
+
+import {Consents} from '../ConsentList'
+
 import {CLASS} from '../settings'
 
-export const CmpModal = ({logo}) => (
-  <div className={CLASS}>
-    <div className={`${CLASS}-content`}>
-      <header className={`${CLASS}-header`}>
-        <img
-          alt="Schibsted Spain logo"
-          className={`${CLASS}-logo`}
-          src={logo}
-        />
-      </header>
+export const CmpModal = ({logo, purposesAndVendors}) => {
+  const {
+    purposes,
+    vendors,
+    purposeConsents,
+    vendorConsents
+  } = purposesAndVendors
+
+  console.log({
+    purposes,
+    vendors,
+    purposeConsents,
+    vendorConsents
+  })
+
+  return (
+    <div className={CLASS}>
+      <div className={`${CLASS}-content`}>
+        <header className={`${CLASS}-header`}>
+          <img
+            alt="Schibsted Spain logo"
+            className={`${CLASS}-logo`}
+            src={logo}
+          />
+        </header>
+        <Consents list={purposes} consents={purposeConsents} />
+        <Consents isVendor list={vendors} consents={vendorConsents} />
+        <Button onClick={() => {}} type="secondary">
+          Guardar y salir
+        </Button>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 CmpModal.propTypes = {
-  logo: PropTypes.string
+  lang: PropTypes.string,
+  logo: PropTypes.string,
+  onAccept: PropTypes.func,
+  purposesAndVendors: PropTypes.object
 }
--- components/cmp/modal/src/CmpModal/index.js
+++ components/cmp/modal/src/CmpModal/index.js
@@ -12,6 +12,7 @@ export class CmpModalContainer extends Component {
     const purposesAndVendors = await getPurposesAndVendors.execute({
       retrieveConsentsFromCmp
     })
+    console.log(purposesAndVendors)
     this.setState({purposesAndVendors, fetchingData: false})
   }
 
@@ -19,25 +20,27 @@ export class CmpModalContainer extends Component {
     console.log('todo')
   }
 
+  _handleToggleAll = ({enabled, isVendor}) => {
+    console.log('todo')
+  }
+
   _handleAccept = async () => {
     const {sendConsents} = this.props
-    const {features, vendors} = this.state
-    sendConsents.execute({features, vendors})
+    const {featuresConsents, vendorsConsents} = this.state.purposesAndVendors
+    sendConsents.execute({featuresConsents, vendorsConsents})
   }
 
   render() {
     const {lang, logo} = this.props
-    const {fetchingData, features, vendors} = this.state
-
+    const {fetchingData, purposesAndVendors} = this.state
     if (fetchingData) return null
 
     return (
       <CmpModal
         lang={lang}
         logo={logo}
-        features={features}
         onAccept={this._handleAccept}
-        vendors={vendors}
+        purposesAndVendors={purposesAndVendors}
       />
     )
   }

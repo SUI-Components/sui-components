ee4baabaeaed5208dced7fa52e71aded4f9add75
Miguel Ángel Durán <miduga@gmail.com>
2018-06-18 14:38:07 +0200

    feat(cmp/modal): use new handleToggle function

--- components/cmp/modal/src/ConsentItem/index.js
+++ components/cmp/modal/src/ConsentItem/index.js
@@ -1,28 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
 import AtomSwitch from '@s-ui/react-atom-switch'
 
+import {ConsentTitle} from '../ConsentTitle'
 import {CLASS} from '../settings'
 
 export const ConsentItem = ({
   enabled,
   id,
-  handleToggleConsent,
+  onToggleConsent,
   title,
   isVendor,
   url
 }) => (
   <div className={`${CLASS}-consent`}>
-    <div className={`${CLASS}-consentTitle`}>{title}</div>
+    <ConsentTitle title={title} url={url} />
     <div className={`${CLASS}-consentActions`}>
       <AtomSwitch
-        disabled={!enabled}
+        initialValue={enabled}
         label=""
         labelLeft=""
         labelRight=""
         name={`${title}-switch`}
-        onToggle={_ => handleToggleConsent({enabled: !enabled, id, isVendor})}
+        onToggle={() => onToggleConsent({enabled: !enabled, id, isVendor})}
         type="toggle"
       />
     </div>
@@ -31,7 +31,7 @@ export const ConsentItem = ({
 
 ConsentItem.propTypes = {
   enabled: PropTypes.bool,
-  handleToggleConsent: PropTypes.func,
+  onToggleConsent: PropTypes.func,
   id: PropTypes.number.isRequired,
   isVendor: PropTypes.bool,
   title: PropTypes.string.isRequired,

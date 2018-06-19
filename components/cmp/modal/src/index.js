6f6acc1961655dfadfca22a92fb0effaeb0df508
Miguel Ángel Durán <miduga@gmail.com>
2018-06-19 11:42:21 +0200

    refactor(cmp/modal): remove commented code

--- components/cmp/modal/src/index.js
+++ components/cmp/modal/src/index.js
@@ -4,36 +4,6 @@ import PropTypes from 'prop-types'
 import {CmpModalContainer} from './CmpModal'
 import CmpServices from '../../services/src/index'
 
-/*
-<div className={CLASS}>
-            <div className={`${CLASS}-content`}>
-              <header className={`${CLASS}-header`}>
-                <img
-                  alt="Schibsted Spain logo"
-                  className={`${CLASS}-logo`}
-                  src={this.props.logo}
-                />
-              </header>
-
-              <section className={`${CLASS}-body`}>
-                <h3 className={`${CLASS}-title`}>{i18n.title}</h3>
-                <p className={`${CLASS}-message`}>{i18n.body}</p>
-                <FeaturesConsents />
-                <VendorsConsents />
-              </section>
-
-              <footer className={`${CLASS}-footer`}>
-                <Button type="tertiary" onClick={() => {}} size="small">
-                  {i18n.cancel}
-                </Button>
-                <Button onClick={() => {}} size="large">
-                  {i18n.saveAndExit}
-                </Button>
-              </footer>
-            </div>
-          </div>
-        )}
-        */
 class CmpModal extends Component {
   render() {
     return (

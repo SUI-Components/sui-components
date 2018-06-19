f15376178e5c10674a7c591f93788490c647de31
Miguel Ángel Durán <miduga@gmail.com>
2018-06-19 11:46:09 +0200

    feat(cmp/modal): add first translations

--- components/cmp/modal/src/settings.js
+++ components/cmp/modal/src/settings.js
@@ -1,13 +1,25 @@
 export const CLASS = 'sui-CmpModal'
 
+const DEFAULT_I18N = {
+  ADVERTISEMENT_TITLE:
+    'Cookies propios o de terceros para publicidad segmentada',
+  ADVERTISEMENT_BODY:
+    'Nos importa mucho tu privacidad por esta razón te queremos dar a conocer las finalidades perseguidas por las cookies de publicidad personalizada además de con quién estamos compartiendo tus datos. Asimismo podrás definir las finalidades y los terceros con los que aceptas o no compartir tus datos de navegación, tus datos de localización y tus datos de carácter personal. Ten en cuenta que estas cookies van ligadas a tu sesión en el navegador por lo que si refrescas tus cookies, cambias de dispositivos o te conectas desde otro navegador, tendrás que volver a configurar tus preferencias.',
+  AUTHORIZE: 'Autorizo',
+  FOR_THE_NEXT_PARTNERS: 'Para los siguientes anunciantes y partners',
+  CANCEL: 'Cancelar',
+  SAVE_AND_EXIT: 'Guardar y salir',
+  ENABLE_ALL: 'Habilitar todo',
+  DISABLE_ALL: 'Deshabilitar todo',
+  TITLE: 'Tu privacidad es importante para nosotros',
+  BODY:
+    'Puedes dar tu consentimiento de manera individual a cada partner. Ver la lista de todos los propósitos para los cuales utilizan tus datos para tener más información. En algunos casos, las empresas pueden revelar que usan tus datos sin pedir tu consentimiento, en función de intereses legítimos. Puedes hacer click en su política de privacidad para obtener más información al respecto o para rechazarlo.'
+}
+
 export const I18N = {
-  es: {
-    cancel: 'Cancel',
-    disableAndExit: 'Guardar y salir',
-    enableAll: 'Habilitar todo',
-    disableAll: 'Deshabilitar todo',
-    title: 'Tu privacidad es importante para nosotros',
-    body:
-      'Puedes dar tu consentimiento de manera individual a cada partner. Ver la lista de todos los propósitos para los cuales utilizan tus datos para tener más información. En algunos casos, las empresas pueden revelar que usan tus datos sin pedir tu consentimiento, en función de intereses legítimos. Puedes hacer click en su política de privacidad para obtener más información al respecto o para rechazarlo.'
-  }
+  es: DEFAULT_I18N,
+  en: DEFAULT_I18N,
+  ca: DEFAULT_I18N,
+  de: DEFAULT_I18N,
+  it: DEFAULT_I18N
 }

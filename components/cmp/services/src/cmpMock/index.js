51acb264e6434026d10b583f2a07432d7e938e5b
Miguel Ángel Durán <miduga@gmail.com>
2018-06-19 16:05:10 +0200

    test(cmp/services): simplify command mock

--- components/cmp/services/src/cmpMock/index.js
+++ components/cmp/services/src/cmpMock/index.js
@@ -1,14 +1,16 @@
 import vendorListMock from './vendorListMock.json'
 import vendorConsentsMock from './vendorConsentsMock.json'
 
+const COMMANDS_RESPONSES = {
+  getVendorList: vendorListMock,
+  getVendorConsents: vendorConsentsMock,
+  sendConsents: undefined,
+  // TEMP UNTIL I KNOW WHAT IS GOING TO RETURN ADS
+  getConsentStatus: {userAcceptedCookies: false}
+}
+
 function cmp(command, parameter, callback) {
-  if (command === 'getVendorList') {
-    return callback(vendorListMock)
-  } else if (command === 'getVendorConsents') {
-    return callback(vendorConsentsMock)
-  } else if (command === 'sendConsents') {
-    return callback()
-  }
+  return callback(COMMANDS_RESPONSES[command])
 }
 
 window.__cmp = window.__cmp || cmp

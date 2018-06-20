d4d779dda0d93dff2365fc52c3fe5a9261d5a4e8
Miguel Ángel Durán <miduga@gmail.com>
2018-06-20 10:26:30 +0200

    fix(cmp/services): resolve with the response of the cmp

--- components/cmp/services/src/repository/cmpRepository.js
+++ components/cmp/services/src/repository/cmpRepository.js
@@ -2,7 +2,7 @@ export class CmpRepository {
   getConsentStatus() {
     return new Promise(resolve => {
       window.__cmp('getConsentStatus', null, function(consentStatus) {
-        resolve({})
+        resolve(consentStatus)
       })
     })
   }
@@ -27,6 +27,7 @@ export class CmpRepository {
     const consents = {purposeConsents, vendorConsents}
     return new Promise(resolve => {
       window.__cmp('sendConsents', consents, function(result) {
+        // TODO: Remove next line
         console.log('sent consents', consents)
         resolve(result)
       })

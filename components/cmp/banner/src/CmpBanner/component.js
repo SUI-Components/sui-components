d9d91bb6ac3c78e9f4805c0468d99f28f5bc40a4
Miguel Ángel Durán <miduga@gmail.com>
2018-06-25 11:02:54 +0200

    feat(cmp/banner): use i18n object instead inline translation

--- components/cmp/banner/src/CmpBanner/component.js
+++ components/cmp/banner/src/CmpBanner/component.js
@@ -3,28 +3,17 @@ import PropTypes from 'prop-types'
 
 import Notification from '@s-ui/react-molecule-notification'
 
-import {CLASS} from '../settings'
+import {CLASS, I18N} from '../settings'
 
 export class CmpBanner extends Component {
   render() {
-    const {buttons} = this.props
+    const {buttons, lang} = this.props
     return (
       <div className={CLASS}>
         <Notification
           buttons={buttons}
           position="bottom"
-          text={
-            <p className={`${CLASS}-text`}>
-              En Schibsted Classified Media Spain, S.L.U utilizamos cookies
-              propias y de terceros para prestar el servicio solicitado,
-              elaborar estadísticas, fomentar el tráfico en la web y mostrarte
-              publicidad personalizada en base a tus datos de navegación o
-              elaborando perfiles publicitarios en función de los datos que
-              recojamos sobre ti, los cuales podemos compartir con terceros. Al
-              clicar en “Aceptar”, aceptas su uso. Puedes configurar o rechazar
-              las cookies clicando en cambiar configuración.
-            </p>
-          }
+          text={<p className={`${CLASS}-text`}>{I18N[lang]}</p>}
           show
         />
       </div>
@@ -33,5 +22,6 @@ export class CmpBanner extends Component {
 }
 
 CmpBanner.propTypes = {
-  buttons: PropTypes.array
+  buttons: PropTypes.array,
+  lang: PropTypes.string
 }

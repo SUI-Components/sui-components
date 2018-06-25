1a4a1c7c4b6df273d115d5c6f7e0c0ea726c05e1
Miguel Ángel Durán <miduga@gmail.com>
2018-06-25 11:02:30 +0200

    feat(cmp/banner): create i18n for banner

--- components/cmp/banner/src/settings.js
+++ components/cmp/banner/src/settings.js
@@ -1,1 +1,14 @@
 export const CLASS = 'sui-cmpBanner'
+
+const DEFAULT_I18N = {
+  BANNER_BODY:
+    'En Schibsted Classified Media Spain, S.L.U utilizamos cookies propias y de terceros para prestar el servicio solicitado, elaborar estadísticas, fomentar el tráfico en la web y mostrarte publicidad personalizada en base a tus datos de navegación o elaborando perfiles publicitarios en función de los datos que recojamos sobre ti, los cuales podemos compartir con terceros. Al clicar en “Aceptar”, aceptas su uso. Puedes configurar o rechazar las cookies clicando en cambiar configuración.'
+}
+
+export const I18N = {
+  es: DEFAULT_I18N,
+  en: DEFAULT_I18N,
+  ca: DEFAULT_I18N,
+  de: DEFAULT_I18N,
+  it: DEFAULT_I18N
+}

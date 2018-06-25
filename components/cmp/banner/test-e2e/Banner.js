de1e0fa9b1484b2a1d6a83e571970a89681bf9d6
Miguel Ángel Durán <miduga@gmail.com>
2018-06-25 10:21:52 +0200

    test(cmp/banner): add first e2e tests

---
+++ components/cmp/banner/test-e2e/Banner.js
@@ -0,0 +1,35 @@
+/* global cy, expect, Cypress */
+describe('Cmp Banner', function() {
+  const freshStart = () => {
+    cy.visit('/workbench/cmp/banner/demo')
+    cy.get('.sui-StudioDemo-fullScreenButton').click()
+  }
+
+  before(freshStart)
+
+  it('show notification correctly', function() {
+    cy.get('.sui-MoleculeNotification').should('exist')
+  })
+
+  it('allow user to accept or change configuration', function() {
+    cy.get('.sui-MoleculeNotification-buttonsContainer')
+      .find('button')
+      .should('have.length', 2)
+  })
+
+  it('open the modal if user wants to know more', function() {
+    cy.get('.sui-MoleculeNotification-buttonsContainer button:first').click()
+  })
+
+  it('close the notification if user accept', function() {
+    freshStart()
+    cy.get('.sui-MoleculeNotification-buttonsContainer button:last').click()
+
+    cy.get('.sui-cmpBanner').should('not.exist')
+
+    cy.get('.sui-CmpModal-consentsActions')
+      .first()
+      .find('.sui-AtomButton--secondary')
+      .click()
+  })
+})

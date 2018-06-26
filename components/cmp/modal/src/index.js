438d82b0ac1197f59644525aec95299ba8eae5dd
Miguel Ángel Durán <miduga@gmail.com>
2018-06-26 19:23:19 +0200

    docs(cmp/modal): Add propTypes documentation

--- components/cmp/modal/src/index.js
+++ components/cmp/modal/src/index.js
@@ -26,20 +26,39 @@ class CmpModal extends Component {
 
 CmpModal.defaultProps = {
   checkCmpLibraryIsLoaded: true,
-  cmpReady: false,
   lang: 'es',
   onExit: () => {},
   retrieveConsentsFromCmp: false
 }
 
 CmpModal.propTypes = {
+  /**
+   * Flag to determine if we have to check if the cmp library is loaded.
+   * Used as this modal could work standalone or opened by another component, that could have already checked
+   * if the cmp is loaded
+   */
   checkCmpLibraryIsLoaded: PropTypes.bool,
-  cmpReady: PropTypes.bool,
+  /**
+   * ISO 639-1 code language in order to get the text translated to it
+   */
   lang: PropTypes.string,
-  logo: PropTypes.string.isRequired,
+  /**
+   * URL of the static image that will be the logo for the Modal
+   */
+  logo: PropTypes.string,
+  /**
+   * URL where the user will go in order to know more about the privacy conditions of the website
+   */
+  privacyUrl: PropTypes.string.isRequired,
+  /**
+   * Function to be executed when the user wants to exit the modal because he accepted the consents
+   */
   onExit: PropTypes.func,
-  retrieveConsentsFromCmp: PropTypes.bool,
-  privacyUrl: PropTypes.string.isRequired
+  /**
+   * Flag to determine if we have to retrieve the consents from the CMP cookie
+   * or if it's the first time the user is selecting the consents
+   */
+  retrieveConsentsFromCmp: PropTypes.bool
 }
 
 CmpModal.displayName = 'CmpModal'

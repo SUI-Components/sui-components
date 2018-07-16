bbc83eb9e4b160c28eb9aa8f8e5739a3bde1aa98
Adria Velardos Palomar <adria.velardos@habitaclia.com>
2018-07-16 13:58:29 +0200

    feat(molecule/collapsible): add constant for icon class

--- components/molecule/collapsible/src/index.js
+++ components/molecule/collapsible/src/index.js
@@ -1,70 +1,64 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import cx from "classnames";
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import cx from 'classnames'
 
-const BASE_CLASS = "sui-MoleculeCollapsible";
-const CONTENT_CLASS = `${BASE_CLASS}-content`;
-const CONTAINER_BUTTON_CLASS = `${BASE_CLASS}-container`;
-const BUTTON_CLASS = `${BASE_CLASS}-btn`;
-const MIN_HEIGHT = 100; // px
+const BASE_CLASS = 'sui-MoleculeCollapsible'
+const CONTENT_CLASS = `${BASE_CLASS}-content`
+const CONTAINER_BUTTON_CLASS = `${BASE_CLASS}-container`
+const BUTTON_CLASS = `${BASE_CLASS}-btn`
+const ICON_CLASS = `${BASE_CLASS}-icon`
+const MIN_HEIGHT = 100 // px
 
 class MoleculeCollapsible extends Component {
   constructor(props) {
-    super(props);
-    this.childrenContainer = React.createRef();
-    this.state = { collapsed: true, showButton: true };
+    super(props)
+    this.childrenContainer = React.createRef()
+    this.state = {collapsed: true, showButton: true}
     this.contentStyles = {
       height: `${MIN_HEIGHT}`
-    };
+    }
   }
 
   toggleCollapse = () => {
-    const { collapsed, showButton } = this.state;
-    const { onClose, onOpen } = this.props;
+    const {collapsed, showButton} = this.state
+    const {onClose, onOpen} = this.props
     if (showButton) {
       if (collapsed) {
-        this.setState({ collapsed: false });
-        onOpen();
+        this.setState({collapsed: false})
+        onOpen()
       } else {
-        this.setState({ collapsed: true });
-        onClose();
+        this.setState({collapsed: true})
+        onClose()
       }
     }
-  };
+  }
 
   componentDidMount() {
-    const offsetHeight = this.childrenContainer.current.offsetHeight;
-    this.setState({ showButton: offsetHeight >= MIN_HEIGHT });
+    const offsetHeight = this.childrenContainer.current.offsetHeight
+    this.setState({showButton: offsetHeight >= MIN_HEIGHT})
   }
 
   render() {
-    const { collapsed, showButton } = this.state;
-    const {
-      children,
-      height,
-      icon,
-      showText,
-      hideText,
-      hasGradient
-    } = this.props;
+    const {collapsed, showButton} = this.state
+    const {children, height, icon, showText, hideText, hasGradient} = this.props
     const wrapperClassName = cx(`${BASE_CLASS}`, {
       [`${BASE_CLASS}--gradient`]: hasGradient,
       [`${BASE_CLASS}--collapsed`]: collapsed
-    });
-    const iconClassName = cx(`${BASE_CLASS}-icon`, {
-      [`${BASE_CLASS}-icon--collapsed`]: collapsed
-    });
+    })
+    const iconClassName = cx(`${ICON_CLASS}`, {
+      [`${ICON_CLASS}--collapsed`]: collapsed
+    })
     const containerClassName = cx(`${CONTAINER_BUTTON_CLASS}`, {
       [`${CONTAINER_BUTTON_CLASS}--gradient`]: hasGradient,
       [`${CONTAINER_BUTTON_CLASS}--collapsed`]: collapsed
-    });
-    const containerHeight = showButton && collapsed ? height : "none";
+    })
+    const containerHeight = showButton && collapsed ? height : 'none'
 
     return (
       <div className={wrapperClassName}>
         <div
           className={CONTENT_CLASS}
-          style={{ maxHeight: `${containerHeight}` }}
+          style={{maxHeight: `${containerHeight}`}}
           ref={this.childrenContainer}
         >
           {children}
@@ -82,11 +76,11 @@ class MoleculeCollapsible extends Component {
           </div>
         )}
       </div>
-    );
+    )
   }
 }
 
-MoleculeCollapsible.displayName = "MoleculeCollapsible";
+MoleculeCollapsible.displayName = 'MoleculeCollapsible'
 
 MoleculeCollapsible.propTypes = {
   /**
@@ -121,13 +115,13 @@ MoleculeCollapsible.propTypes = {
    * On close callback
    */
   onClose: PropTypes.func
-};
+}
 
 MoleculeCollapsible.defaultProps = {
   height: MIN_HEIGHT,
   hasGradient: true,
   onOpen: () => {},
   onClose: () => {}
-};
+}
 
-export default MoleculeCollapsible;
+export default MoleculeCollapsible

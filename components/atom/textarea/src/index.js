9b8ba25f82580f15fc3eec5c3f6e2fac0ac1f272
JuanMa Garrido <juanma.garrido@gmail.com>
2018-07-24 13:50:51 +0200

    feat(atom/textarea): modifications after feedback review

--- components/atom/textarea/src/index.js
+++ components/atom/textarea/src/index.js
@@ -1,12 +1,8 @@
 import React, {Fragment, Component} from 'react'
-import PropTypes from 'prop-types' // eslint-disable-line no-unused-vars
+import PropTypes from 'prop-types'
 import cx from 'classnames'
 
 import AtomHelpText from '@s-ui/react-atom-help-text'
-import AtomValidationText, {
-  AtomValidationTextTypes
-} from '@s-ui/react-atom-validation-text'
-import AtomLabel, {AtomLabelTypes} from '@s-ui/react-atom-label'
 
 const BASE_CLASS = 'sui-AtomTextarea'
 const SIZES = {
@@ -18,18 +14,10 @@ class AtomTextarea extends Component {
   state = {
     value: this.props.children // eslint-disable-line react/prop-types
   }
-  refTextarea = React.createRef()
-  id = this.props.id || `sui-AtomTextarea-${+new Date()}` // eslint-disable-line react/prop-types
 
   get classNames() {
     const {size} = this.props
     return cx(BASE_CLASS, `${BASE_CLASS}--${size}`)
-  }
-
-  handleChange = e => {
-    const value = e.target.value
-    if (value.length > this.props.maxCharacters) return
-    this.setState({value})
   }
 
   get helpTextContent() {
@@ -37,55 +25,23 @@ class AtomTextarea extends Component {
     return `${numCharacters}/${this.props.maxCharacters} characters`
   }
 
-  getTypeValidation(element) {
-    if (this.props.success) {
-      if (element === 'label') return AtomLabelTypes.SUCCESS
-      if (element === 'validationText') return AtomValidationTextTypes.SUCCESS
-    }
-    if (this.props.error) {
-      if (element === 'label') return AtomLabelTypes.ERROR
-      if (element === 'validationText') return AtomValidationTextTypes.ERROR
-    }
-  }
-
-  get statusValidationText() {
-    if (this.props.success) return this.props.successText
-    if (this.props.error) return this.props.errorText
+  handleChange = ev => {
+    const value = ev.target.value
+    const {onChange} = this.props // eslint-disable-line react/prop-types
+    if (value.length > this.props.maxCharacters) return
+    this.setState({value}, () => onChange && onChange({value, ev}))
   }
 
   render() {
-    const {
-      size,
-      label,
-      maxCharacters,
-      successText,
-      errorText,
-      success,
-      error,
-      ...props
-    } = this.props
+    const {onChange, ...props} = this.props
     return (
       <Fragment>
-        <AtomLabel
-          type={this.getTypeValidation('label')}
-          name={this.id}
-          for={this.id}
-          text={label}
-        />
         <textarea
           {...props}
-          ref={this.refTextarea}
           onChange={this.handleChange}
-          id={this.id}
           className={this.classNames}
           value={this.state.value}
         />
-        {(success || error) && (
-          <AtomValidationText
-            type={this.getTypeValidation('validationText')}
-            text={this.statusValidationText}
-          />
-        )}
         <AtomHelpText text={this.helpTextContent} />
       </Fragment>
     )
@@ -95,25 +51,10 @@ class AtomTextarea extends Component {
 AtomTextarea.displayName = 'AtomTextarea'
 
 AtomTextarea.propTypes = {
-  /** Text to be displayed as label of the textarea */
-  label: PropTypes.string.isRequired,
-
   /** Size of button: 'short', 'long' */
   size: PropTypes.oneOf(Object.values(SIZES)),
 
-  /** Sucess state of the component */
-  success: PropTypes.bool,
-
-  /** Error state of the component */
-  error: PropTypes.bool,
-
-  /** Sucess message to display when sucess state  */
-  successText: PropTypes.string,
-
-  /** Error message to display when error state  */
-  errorText: PropTypes.string,
-
-  /** Maximum nomber of characters allowed  */
+  /** Maximum number of characters allowed  */
   maxCharacters: PropTypes.number
 }
 

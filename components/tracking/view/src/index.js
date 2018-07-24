dc9b32366ad7846cdfa62657d9f5de19129c9b05
Carles Núñez Tomeo <carles.nunez@schibsted.com>
2018-07-24 15:06:38 +0200

    feat(tracking/view): added tracking library and base functionality

--- components/tracking/view/src/index.js
+++ components/tracking/view/src/index.js
@@ -1,22 +1,43 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
-
+import ScriptLoader from '@schibstedspain/sui-script-loader'
 class TrackingView extends Component {
-  componentDidMount() {}
-
   shouldComponentUpdate() {
     return false
   }
+
+  proofOfLife(verifier) {
+    return window && window[verifier]
+  }
+
   render() {
-    return null
+    return (
+      <ScriptLoader
+        src={this.props.src}
+        isAsync={false}
+        verifier={() => this.proofOfLife(this.props.verifier)}
+        render={() => !this.props.track() && null}
+      />
+    )
   }
 }
 
 TrackingView.displayName = 'TrackingView'
 
-// Remove these comments if you need
-// TrackingView.contextTypes = {i18n: PropTypes.object}
-// TrackingView.propTypes = {}
-// TrackingView.defaultProps = {}
+TrackingView.propTypes = {
+  /**
+   * It must be a string with a src to the library with the API that our track function is going to use.
+   */
+  src: PropTypes.string.isRequired,
+  /**
+   * It must be a string with the object or even the function that our tracker is going to use. Used in order to ensure that our tracking API exist.
+   */
+  verifier: PropTypes.string.isRequired,
+  /**
+   * A function that will be called once a time our trackingView will be rendered for the first time.
+   * It will be executed just one time.
+   */
+  track: PropTypes.func.isRequired
+}
 
 export default TrackingView

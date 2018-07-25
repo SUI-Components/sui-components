f1582e9353020c091a60a34da36c8a4b3f02f9eb
JuanMa Garrido <juanma.garrido@gmail.com>
2018-07-25 16:42:49 +0200

    feat(molecule/fieldset): component MoleculeFieldset

--- components/molecule/fieldset/README.md
+++ components/molecule/fieldset/README.md
@@ -1,8 +1,8 @@
 # MoleculeFieldset
 
-> Description
 
-<!-- ![](./assets/preview.png) -->
+`MoleculeFieldset` is a component that wraps a composition of Label + some input (input, textarea, ...) + Validations  Messages. It has some properties like:
+
 
 ## Installation
 
@@ -12,12 +12,34 @@ $ npm install @s-ui/react-molecule-fieldset --save
 
 ## Usage
 
-### Basic usage
-```js
-import MoleculeFieldset from '@s-ui/react-molecule-fieldset'
+After importing the component `MoleculeFieldset` like this
 
-return (<MoleculeFieldset />)
+```javascript
+import MoleculeFieldset from '@s-ui/react-molecule-fieldset'
 ```
 
+### Basic usage
 
-> **Find full description and more examples in the [demo page](#).**
+```
+<MoleculeFieldset label="name">
+  <input type="text"/>
+</MoleculeFieldset>
+```
+
+### With Error Message 
+
+```
+<MoleculeFieldset label="name" errorText="Error!">
+  <input type="text"/>
+</MoleculeFieldset>
+```
+
+### With Success Message 
+
+```
+<MoleculeFieldset label="name" successText="Success!">
+  <input type="text"/>
+</MoleculeFieldset>
+```
+
+> **Find full description and more examples in the [demo page](https://sui-components.now.sh/workbench/molecule/fieldset/demo).**
--- components/molecule/fieldset/src/index.js
+++ components/molecule/fieldset/src/index.js
@@ -1,93 +1,72 @@
-import React, {Fragment, Component} from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types' // eslint-disable-line no-unused-vars
-import cx from 'classnames'
 
-import AtomHelpText from '@s-ui/react-atom-help-text'
 import AtomValidationText, {
   AtomValidationTextTypes
 } from '@s-ui/react-atom-validation-text'
 import AtomLabel, {AtomLabelTypes} from '@s-ui/react-atom-label'
 
+import '@s-ui/react-atom-validation-text/lib/index.scss'
+import '@s-ui/react-atom-label/lib/index.scss'
+
 const BASE_CLASS = 'sui-MoleculeFieldset'
-const SIZES = {
-  SHORT: 'short',
-  LONG: 'long'
-}
+const CLASS_INPUT = `${BASE_CLASS}-input`
 
 class MoleculeFieldset extends Component {
   state = {
     value: this.props.children // eslint-disable-line react/prop-types
   }
-  refTextarea = React.createRef()
-  id = this.props.id || `sui-MoleculeFieldset-${+new Date()}` // eslint-disable-line react/prop-types
 
-  get classNames() {
-    const {size} = this.props
-    return cx(BASE_CLASS, `${BASE_CLASS}--${size}`)
-  }
+  idLabelFor = `sui-MoleculeFieldset-${+new Date()}`
 
-  handleChange = e => {
-    const value = e.target.value
-    if (value.length > this.props.maxCharacters) return
-    this.setState({value})
-  }
+  extendChildren(children) {
+    const childrenOnly = React.Children.only(children)
 
-  get helpTextContent() {
-    const numCharacters = this.state.value ? this.state.value.length : 0
-    return `${numCharacters}/${this.props.maxCharacters} characters`
+    return React.Children.map(childrenOnly, child => {
+      this.title = child.props.title
+      const className = CLASS_INPUT
+      const {idLabelFor: id} = this
+      return React.cloneElement(child, {
+        id,
+        className
+      })
+    })
   }
 
   getTypeValidation(element) {
-    if (this.props.success) {
+    if (this.props.successText) {
       if (element === 'label') return AtomLabelTypes.SUCCESS
       if (element === 'validationText') return AtomValidationTextTypes.SUCCESS
     }
-    if (this.props.error) {
+    if (this.props.errorText) {
       if (element === 'label') return AtomLabelTypes.ERROR
       if (element === 'validationText') return AtomValidationTextTypes.ERROR
     }
   }
 
   get statusValidationText() {
-    if (this.props.success) return this.props.successText
-    if (this.props.error) return this.props.errorText
+    if (this.props.successText) return this.props.successText
+    if (this.props.errorText) return this.props.errorText
   }
 
   render() {
-    const {
-      size,
-      label,
-      maxCharacters,
-      successText,
-      errorText,
-      success,
-      error,
-      ...props
-    } = this.props
+    const {label, successText, errorText, children} = this.props
     return (
-      <Fragment>
+      <div className={BASE_CLASS}>
         <AtomLabel
           type={this.getTypeValidation('label')}
-          name={this.id}
-          for={this.id}
+          name={this.idLabelFor}
+          for={this.idLabelFor}
           text={label}
         />
-        <textarea
-          {...props}
-          ref={this.refTextarea}
-          onChange={this.handleChange}
-          id={this.id}
-          className={this.classNames}
-          value={this.state.value}
-        />
-        {(success || error) && (
+        {this.extendChildren(children)}
+        {(successText || errorText) && (
           <AtomValidationText
             type={this.getTypeValidation('validationText')}
             text={this.statusValidationText}
           />
         )}
-        <AtomHelpText text={this.helpTextContent} />
-      </Fragment>
+      </div>
     )
   }
 }
@@ -98,29 +77,11 @@ MoleculeFieldset.propTypes = {
   /** Text to be displayed as label of the textarea */
   label: PropTypes.string.isRequired,
 
-  /** Size of button: 'short', 'long' */
-  size: PropTypes.oneOf(Object.values(SIZES)),
-
-  /** Sucess state of the component */
-  success: PropTypes.bool,
-
-  /** Error state of the component */
-  error: PropTypes.bool,
-
   /** Sucess message to display when sucess state  */
   successText: PropTypes.string,
 
   /** Error message to display when error state  */
-  errorText: PropTypes.string,
-
-  /** Maximum nomber of characters allowed  */
-  maxCharacters: PropTypes.number
-}
-
-MoleculeFieldset.defaultProps = {
-  size: SIZES.SHORT,
-  maxCharacters: 4000
+  errorText: PropTypes.string
 }
 
 export default MoleculeFieldset
-export {SIZES as MoleculeFieldsetSizes}

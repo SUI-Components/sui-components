a0e2d5b1d131024d5e834f0d607e216bb1a853cc
JuanMa Garrido <juanma.garrido@gmail.com>
2018-07-30 14:07:01 +0200

    feat(layout/mediaQuery): component implementation & demo

--- components/layout/mediaQuery/src/index.js
+++ components/layout/mediaQuery/src/index.js
@@ -1,17 +1,30 @@
-import React, {Component} from 'react'
+import React from 'react'
+import {ContainerQuery} from 'react-container-query'
 
-const BASE_CLASS = 'sui-LayoutMediaQuery'
-
-class LayoutMediaQuery extends Component {
-  render() {
-    return (
-      <div className={BASE_CLASS}>
-        <h1>LayoutMediaQuery</h1>
-      </div>
-    )
-  }
+const BREAKPOINTS = {
+  xs: '480',
+  s: '600',
+  m: '840',
+  l: '960',
+  xl: '1280',
+  xxl: '1440'
 }
 
+const getQueryObject = BREAKPOINTS =>
+  Object.entries(BREAKPOINTS).reduce((query, [size, breakpoint]) => {
+    query[size.toUpperCase()] = {
+      minWidth: breakpoint
+    }
+    return query
+  }, {})
+
+const LayoutMediaQueryFactory = BREAKPOINTS => props => {
+  const query = getQueryObject(BREAKPOINTS)
+  return <ContainerQuery query={query}>{props.children}</ContainerQuery> // eslint-disable-line react/prop-types
+}
+
+const LayoutMediaQuery = LayoutMediaQueryFactory(BREAKPOINTS)
 LayoutMediaQuery.displayName = 'LayoutMediaQuery'
 
 export default LayoutMediaQuery
+export {LayoutMediaQueryFactory}

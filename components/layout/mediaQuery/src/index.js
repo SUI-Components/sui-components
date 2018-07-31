f5531f0d0f5c8ca55e1beddb14cd1bc6c6fdb614
JuanMa Garrido <juanma.garrido@gmail.com>
2018-07-31 10:02:38 +0200

    feat(layout/mediaQuery): mediaQuery demo and README

--- components/layout/mediaQuery/src/index.js
+++ components/layout/mediaQuery/src/index.js
@@ -1,14 +1,7 @@
 import React from 'react'
 import {ContainerQuery} from 'react-container-query'
 
-const BREAKPOINTS = {
-  xs: '480',
-  s: '600',
-  m: '840',
-  l: '960',
-  xl: '1280',
-  xxl: '1440'
-}
+import BREAKPOINTS from './breakpoints.json'
 
 const getQueryObject = BREAKPOINTS =>
   Object.entries(BREAKPOINTS).reduce((query, [size, breakpoint]) => {

9fc0f873855e23f5040d92ed09a35efbc4a45b6e
JuanMa Garrido <juanma.garrido@gmail.com>
2018-08-02 12:57:16 +0200

    feat(layout/mediaQuery): viewport prop

--- components/layout/mediaQuery/src/index.js
+++ components/layout/mediaQuery/src/index.js
@@ -1,23 +1,74 @@
-import React from 'react'
-import {ContainerQuery} from 'react-container-query'
+import {Component} from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+import ResizeObserver from 'resize-observer-polyfill'
+import shallowEqual from 'shallowequal'
 
-import BREAKPOINTS from './breakpoints.json'
+import {getWidth, matchQueries} from './helpers'
 
-const getQueryObject = BREAKPOINTS =>
-  Object.entries(BREAKPOINTS).reduce((query, [size, breakpoint]) => {
-    query[size.toUpperCase()] = {
-      minWidth: breakpoint
+import {BREAKPOINTS} from './breakpoints'
+
+const LayoutMediaQueryFactory = BREAKPOINTS =>
+  class extends Component {
+    state = {
+      params: {}
     }
-    return query
-  }, {})
+    containerResizeObserver = null
+    matchQueries = matchQueries(BREAKPOINTS)
 
-const LayoutMediaQueryFactory = BREAKPOINTS => props => {
-  const query = getQueryObject(BREAKPOINTS)
-  return <ContainerQuery query={query}>{props.children}</ContainerQuery> // eslint-disable-line react/prop-types
-}
+    componentDidMount() {
+      const {viewport} = this.props // eslint-disable-line react/prop-types
+      const container = ReactDOM.findDOMNode(this)
+      let initialWidth = 0
+
+      if (viewport) {
+        window.addEventListener('resize', this.handleWindowResize)
+        initialWidth = window.outerWidth
+      } else {
+        this.containerResizeObserver = new ResizeObserver(
+          this.handleContainerResize
+        )
+        this.containerResizeObserver.observe(container)
+        initialWidth = getWidth(container)
+      }
+
+      const result = this.matchQueries(initialWidth)
+      this.setState({params: result})
+    }
+
+    componentWillUnmount() {
+      this.containerResizeObserver.disconnect()
+      this.containerResizeObserver = null
+      window.removeEventListener('resize', this.handleResize)
+    }
+
+    handleWindowResize = e => {
+      const {outerWidth: width} = e.target
+      const {params} = this.state
+      const result = this.matchQueries(width)
+      if (!shallowEqual(result, params)) this.setState({params: result})
+    }
+
+    handleContainerResize = entries => {
+      const target = entries[0].target
+      const width = getWidth(target)
+      const {params} = this.state
+      const result = this.matchQueries(width)
+      if (!shallowEqual(result, params)) this.setState({params: result})
+    }
+
+    render() {
+      return this.props.children(this.state.params) // eslint-disable-line react/prop-types
+    }
+  }
 
 const LayoutMediaQuery = LayoutMediaQueryFactory(BREAKPOINTS)
 LayoutMediaQuery.displayName = 'LayoutMediaQuery'
+
+LayoutMediaQuery.propTypes = {
+  /** Wether to listen to the viewport resize or not (it listens to the container resize by default) */
+  viewport: PropTypes.bool
+}
 
 export default LayoutMediaQuery
 export {LayoutMediaQueryFactory}

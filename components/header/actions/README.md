079f2495441836665e651560b95f5e419d9e4b5e
Julian Gernun <julian.gernun@schibsted.com>
2018-08-17 09:14:19 +0200

    feat(header/actions): first commit

---
+++ components/header/actions/README.md
@@ -0,0 +1,23 @@
+# HeaderActions
+
+> Description
+
+<!-- ![](./assets/preview.png) -->
+
+## Installation
+
+```sh
+$ npm install @schibstedspain/sui-header-actions --save
+```
+
+## Usage
+
+### Basic usage
+```js
+import HeaderActions from '@schibstedspain/sui-header-actions'
+
+return (<HeaderActions />)
+```
+
+
+> **Find full description and more examples in the [demo page](#).**
---
+++ components/header/actions/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Button from '@schibstedspain/sui-atom-button'
+
+const getClass = type => `sui-HeaderActions${type ? `-${type}` : ''}`
+
+const HeaderActions = ({title, icon, buttonLabel}) => (
+  <div className={getClass()}>
+    <span className={getClass('icon')}>{icon}</span>
+    <h1 className={getClass('title')}>{title}</h1>
+    <span className={getClass('button')}>
+      <Button type="secondary" negative>
+        {buttonLabel}
+      </Button>
+    </span>
+  </div>
+)
+
+HeaderActions.displayName = 'HeaderActions'
+
+HeaderActions.propTypes = {
+  title: PropTypes.string.isRequired,
+  buttonLabel: PropTypes.string.isRequired,
+  icon: PropTypes.node
+}
+
+export default HeaderActions

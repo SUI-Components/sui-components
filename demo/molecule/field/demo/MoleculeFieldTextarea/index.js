1ca903fc34b50d4ab8bd4983e028151f0acde1b5
JuanMa Garrido <juanma.garrido@gmail.com>
2018-08-17 11:12:09 +0200

    feat(META): fixed import

--- demo/molecule/field/demo/MoleculeFieldTextarea/index.js
+++ demo/molecule/field/demo/MoleculeFieldTextarea/index.js
@@ -4,7 +4,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import MoleculeField from '../../../../../components/molecule/field/src'
-import AtomTextarea from '@s-ui/react-atom-textarea/src/index'
+import AtomTextarea from '@s-ui/react-atom-textarea'
 
 import WithCharacterCount from './hoc/WithCharacterCount'
 

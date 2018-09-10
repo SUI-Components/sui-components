c31969f1588e8cf3905f3f75183fa98c5ac4e8ea
Adria Velardos Palomar <adria.velardos@habitaclia.com>
2018-09-10 13:10:36 +0200

    feat(atom/accordion): add initial generated files for Atom Accordion

---
+++ components/atom/accordion/README.md
@@ -0,0 +1,23 @@
+# AtomAccordion
+
+> Description
+
+<!-- ![](./assets/preview.png) -->
+
+## Installation
+
+```sh
+$ npm install @s-ui/react-atom-accordion --save
+```
+
+## Usage
+
+### Basic usage
+```js
+import AtomAccordion from '@s-ui/react-atom-accordion'
+
+return (<AtomAccordion />)
+```
+
+
+> **Find full description and more examples in the [demo page](#).**
---
+++ components/atom/accordion/src/index.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+// import PropTypes from 'prop-types'
+
+class AtomAccordion extends Component {
+  render() {
+    return (
+      <div className="react-AtomAccordion">
+        <h1>AtomAccordion</h1>
+      </div>
+    )
+  }
+}
+
+AtomAccordion.displayName = 'AtomAccordion'
+
+// Remove these comments if you need
+// AtomAccordion.contextTypes = {i18n: PropTypes.object}
+// AtomAccordion.propTypes = {}
+// AtomAccordion.defaultProps = {}
+
+export default AtomAccordion

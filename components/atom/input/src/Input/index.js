df6bedc4f74b8d4ea837fba607840a5c93f6ae49
JuanMa Garrido <juanma.garrido@gmail.com>
2018-09-24 20:07:00 +0200

    feat(atom/input): icon Left

--- components/atom/input/src/Input/index.js
+++ components/atom/input/src/Input/index.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import AddonHoc from './Features/Addon'
+import WithIconHoc from './Features/WithIcon'
 import Component, {inputSizes} from './Component'
 
 const WithAddonComponent = AddonHoc(Component)
+const WithIconComponent = WithIconHoc(Component)
 
 export default props => {
-  const {leftAddon, rightAddon} = props // eslint-disable-line react/prop-types
-  return leftAddon || rightAddon ? (
-    <WithAddonComponent {...props} />
-  ) : (
-    <Component {...props} />
-  )
+  const {leftAddon, leftIcon, rightAddon} = props // eslint-disable-line react/prop-types
+  if (leftAddon || rightAddon) return <WithAddonComponent {...props} />
+  if (leftIcon) return <WithIconComponent {...props} />
+  return <Component {...props} />
 }
 
 export {inputSizes}

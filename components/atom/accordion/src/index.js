8634f930fb02d7277fc5de6a0a3c29e1bca25575
Adria Velardos Palomar <adria.velardos@habitaclia.com>
2018-09-26 17:39:21 +0200

    feat(atom/accordion): check proptype children and set all children with new props

--- components/atom/accordion/src/index.js
+++ components/atom/accordion/src/index.js
@@ -1,21 +1,50 @@
 import React, {Component} from 'react'
 // import PropTypes from 'prop-types'
+import MoleculeCollapsible from '../../../molecule/collapsible/src'
+
+const BASE_CLASS = 'sui-AtomAccordion'
 
 class AtomAccordion extends Component {
+  onOpen = () => {
+    window.alert('open!')
+  }
+
   render() {
-    return (
-      <div className="react-AtomAccordion">
-        <h1>AtomAccordion</h1>
-      </div>
+    const children = React.Children.map(
+      this.props.children,
+      child => {
+        return React.cloneElement(child, {
+          minHeight: 0,
+          withGradient: false,
+          withTransition: false,
+          onOpen: () => {
+            this.onOpen()
+          }
+        })
+      },
+      this
     )
+    return <div className={BASE_CLASS}>{children}</div>
   }
 }
 
 AtomAccordion.displayName = 'AtomAccordion'
 
-// Remove these comments if you need
-// AtomAccordion.contextTypes = {i18n: PropTypes.object}
-// AtomAccordion.propTypes = {}
+AtomAccordion.propTypes = {
+  children: (props, propName, componentName) => {
+    let childrenList = props[propName]
+    let error = null
+    let beFalse = false
+    React.Children.forEach(childrenList, child => {
+      if (beFalse && child.type.prototype instanceof MoleculeCollapsible) {
+        error = new Error(`
+          ${componentName} children should be of type MoleculeCollapsible.
+        `)
+      }
+    })
+    return error
+  }
+}
 // AtomAccordion.defaultProps = {}
 
 export default AtomAccordion

254d79d64a037dd58a9dbf71ab3d93cdfc0f0a54
Beto Esposito <alberto.esposito@scmspain.com>
2018-10-15 11:46:42 +0200

    fix(atom/input): remove scss import from js file

--- components/atom/input/src/Password/index.js
+++ components/atom/input/src/Password/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Input from '../Input'
 
-import './index.scss'
+// import './index.scss'
 
 const TEXT = 'text'
 const PASSWORD = 'password'

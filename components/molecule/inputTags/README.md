bf7e68a4e2b8d764b477be84fbb1fe0a9902b58a
JuanMa Garrido <juanma.garrido@gmail.com>
2018-10-31 12:22:14 +0100

    feat(molecule/inputTags): new component MoleculeAtomTags

---
+++ components/molecule/inputTags/README.md
@@ -0,0 +1,23 @@
+# MoleculeInputTags
+
+> Description
+
+<!-- ![](./assets/preview.png) -->
+
+## Installation
+
+```sh
+$ npm install @s-ui/react-molecule-input-tags --save
+```
+
+## Usage
+
+### Basic usage
+```js
+import MoleculeInputTags from '@s-ui/react-molecule-input-tags'
+
+return (<MoleculeInputTags />)
+```
+
+
+> **Find full description and more examples in the [demo page](#).**
---
+++ components/molecule/inputTags/src/index.js
@@ -0,0 +1,130 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import AtomTag, {atomTagSizes} from '@schibstedspain/sui-atom-tag'
+import AtomInput, {inputSizes} from '@s-ui/react-atom-input'
+import cx from 'classnames'
+
+const BASE_CLASS = 'sui-AtomInput'
+const CLASS_TAGS = `${BASE_CLASS}--withTags`
+const CLASS_TAGS_FOCUS = `${CLASS_TAGS}--focus`
+const CLASS_TAGS_ERROR = `${CLASS_TAGS}--error`
+const CLASS_TAGS_SUCCESS = `${CLASS_TAGS}--success`
+
+// eslint-disable-next-line react/prop-types
+const AtomTagItem = ({onClose, id, ...props}) => {
+  const _onClose = e => {
+    onClose && onClose(id)
+  }
+  return <AtomTag onClose={_onClose} {...props} />
+}
+
+class MoleculeInputTags extends Component {
+  state = {
+    tags: this.props.tags,
+    value: this.props.value,
+    focus: false
+  }
+
+  getClassNames = (focus, size, errorState) => {
+    return cx(CLASS_TAGS, {
+      [CLASS_TAGS_FOCUS]: focus === true,
+      [CLASS_TAGS_ERROR]: errorState === true,
+      [CLASS_TAGS_SUCCESS]: errorState === false,
+      [`${CLASS_TAGS}-${size}`]: size
+    })
+  }
+
+  removeTag = indexTag => {
+    const {onChangeTags} = this.props
+    this.setState(
+      {
+        tags: this.state.tags.filter((_, i) => i !== indexTag)
+      },
+      () => {
+        onChangeTags && onChangeTags(this.state.tags)
+      }
+    )
+  }
+
+  addTag = ev => {
+    const {onChangeTags} = this.props
+    const {
+      target: {value}
+    } = ev
+    if (value) {
+      this.setState(
+        {
+          tags: [...this.state.tags, value],
+          value: ''
+        },
+        () => {
+          onChangeTags && onChangeTags(this.state.tags)
+        }
+      )
+    }
+  }
+
+  onChange = ({value}) => {
+    this.setState({value})
+  }
+
+  handleFocusIn = () => {
+    this.setState({focus: true})
+  }
+
+  handleFocusOut = () => {
+    this.setState({focus: false})
+  }
+
+  render() {
+    const {tagsCloseIcon, ...props} = this.props
+    const {focus, value, tags} = this.state
+    return (
+      <div className={this.getClassNames(focus, props.size, props.errorState)}>
+        {tags.map((label, index) => (
+          <AtomTagItem
+            key={index}
+            id={index}
+            closeIcon={tagsCloseIcon}
+            onClose={this.removeTag}
+            label={label}
+            size={atomTagSizes.SMALL}
+          />
+        ))}
+        <AtomInput
+          {...props}
+          value={value}
+          onChange={this.onChange}
+          onEnter={this.addTag}
+          onFocus={this.handleFocusIn}
+          onBlur={this.handleFocusOut}
+        />
+      </div>
+    )
+  }
+}
+
+MoleculeInputTags.displayName = 'MoleculeInputTags'
+
+MoleculeInputTags.propTypes = {
+  /* list of values displayed as tags */
+  tags: PropTypes.array,
+
+  /* value of the input */
+  value: PropTypes.string,
+
+  /* close icon to be displayed on tags */
+  tagsCloseIcon: PropTypes.node,
+
+  /* callback to be called with every update of the list of tags */
+  onChangeTags: PropTypes.func
+}
+
+MoleculeInputTags.defaultProps = {
+  size: inputSizes.MEDIUM,
+  value: '',
+  tags: []
+}
+
+export default MoleculeInputTags
+export {inputSizes}

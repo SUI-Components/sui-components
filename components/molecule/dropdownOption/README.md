901f24c1ded3a65e7e3958ac9155f5ff4e6ce220
Daniel Muñoz Ruiz <damuru88@gmail.com>
2018-11-06 09:48:54 +0100

    feat(molecule/dropdownOption): create component

---
+++ components/molecule/dropdownOption/README.md
@@ -0,0 +1,23 @@
+# MoleculeDropdownOption
+
+> Description
+
+<!-- ![](./assets/preview.png) -->
+
+## Installation
+
+```sh
+$ npm install @s-ui/react-molecule-dropdown-option --save
+```
+
+## Usage
+
+### Basic usage
+```js
+import MoleculeDropdownOption from '@s-ui/react-molecule-dropdown-option'
+
+return (<MoleculeDropdownOption />)
+```
+
+
+> **Find full description and more examples in the [demo page](#).**
---
+++ components/molecule/dropdownOption/src/index.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+// import PropTypes from 'prop-types'
+
+class MoleculeDropdownOption extends Component {
+  render() {
+    return (
+      <div className="sui-MoleculeDropdownOption">
+        <h1>MoleculeDropdownOption</h1>
+      </div>
+    )
+  }
+}
+
+MoleculeDropdownOption.displayName = 'MoleculeDropdownOption'
+
+// Remove these comments if you need
+// MoleculeDropdownOption.contextTypes = {i18n: PropTypes.object}
+// MoleculeDropdownOption.propTypes = {}
+// MoleculeDropdownOption.defaultProps = {}
+
+export default MoleculeDropdownOption

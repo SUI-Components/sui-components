7f37cc8e552101745c18ea4d76b74ed68beff7c6
Daniel Muñoz Ruiz <damuru88@gmail.com>
2018-11-07 14:57:51 +0100

    feat(molecule/dropdownOption): fix async

--- components/molecule/dropdownOption/src/index.js
+++ components/molecule/dropdownOption/src/index.js
@@ -16,7 +16,9 @@ class MoleculeDropdownOption extends Component {
         prevState => ({
           selected: !prevState.selected
         }),
-        this.props.onChange(this.state.selected)
+        () => {
+          this.props.onChange(this.state.selected)
+        }
       )
     }
   }

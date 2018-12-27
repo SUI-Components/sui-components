4c7ee47405c1ae48392e1c877f11e97aba81e47c
JuanMa Garrido <juanma.garrido@gmail.com>
2018-12-27 10:00:06 +0100

    feat(molecule/autosuggest): moleculeSelect like implementation

---
+++ components/molecule/autosuggest/src/components/MultipleSelection.js
@@ -0,0 +1,71 @@
+import React, {Fragment} from 'react'
+
+import MoleculeDropdownList from '@s-ui/react-molecule-dropdown-list'
+import MoleculeInputTags from '@s-ui/react-molecule-input-tags'
+
+import WithSelectUi from '../hoc/withSelectUi'
+
+const MoleculeInputSelect = WithSelectUi(MoleculeInputTags)
+
+const MoleculeSelectFieldMultiSelection = props => {
+  /* eslint-disable react/prop-types */
+  const {
+    children,
+    isOpen,
+    onToggle,
+    onChange,
+    closeOnSelect,
+    iconArrowDown,
+    iconArrowUp,
+    iconCloseTag,
+    value: values,
+    innerRef
+  } = props
+
+  const handleMultiSelection = (ev, {value: valueOptionSelected}) => {
+    const newValues = values.includes(valueOptionSelected)
+      ? values.filter(value => value !== valueOptionSelected)
+      : [...values, valueOptionSelected]
+
+    onChange(ev, {value: newValues})
+    closeOnSelect && onToggle(ev, {open: false})
+  }
+
+  const handleChangeTags = (ev, {tags: value}) => {
+    onChange(ev, {value})
+    closeOnSelect && onToggle(ev, {open: false})
+  }
+
+  return (
+    <Fragment>
+      <MoleculeInputSelect
+        tags={values}
+        onClick={onToggle}
+        tagsCloseIcon={iconCloseTag}
+        iconArrowDown={iconArrowDown}
+        iconArrowUp={iconArrowUp}
+        onChangeTags={handleChangeTags}
+        isOpen={isOpen}
+        noBorder
+      />
+      <MoleculeDropdownList
+        innerRef={innerRef}
+        checkbox
+        visible={isOpen}
+        onSelect={handleMultiSelection}
+        value={values}
+      >
+        {children}
+      </MoleculeDropdownList>
+    </Fragment>
+  )
+}
+
+MoleculeSelectFieldMultiSelection.displayName =
+  'MoleculeSelectFieldMultiSelection'
+
+MoleculeSelectFieldMultiSelection.defaultProps = {
+  value: []
+}
+
+export default MoleculeSelectFieldMultiSelection
---
+++ components/molecule/autosuggest/src/components/SingleSelection.js
@@ -0,0 +1,59 @@
+import React, {Fragment} from 'react'
+
+import MoleculeDropdownList from '@s-ui/react-molecule-dropdown-list'
+import AtomInput from '@s-ui/react-atom-input'
+
+import WithSelectUi from '../hoc/withSelectUi'
+
+const MoleculeInputSelect = WithSelectUi(AtomInput)
+
+const MoleculeSelectSingleSelection = props => {
+  /* eslint-disable react/prop-types */
+  const {
+    value = '',
+    children,
+    isOpen,
+    onToggle,
+    onChange,
+    iconArrowDown,
+    iconArrowUp,
+    closeOnSelect,
+    innerRef,
+    size
+  } = props
+
+  const handleSelection = (ev, {value}) => {
+    onChange(ev, {value})
+    closeOnSelect && onToggle(ev, {open: false})
+  }
+
+  return (
+    <Fragment>
+      <MoleculeInputSelect
+        isOpen={isOpen}
+        value={value}
+        onClick={onToggle}
+        iconArrowDown={iconArrowDown}
+        iconArrowUp={iconArrowUp}
+      />
+      <MoleculeDropdownList
+        size={size}
+        innerRef={innerRef}
+        visible={isOpen}
+        onSelect={handleSelection}
+        value={value}
+      >
+        {children}
+      </MoleculeDropdownList>
+    </Fragment>
+  )
+}
+
+MoleculeSelectSingleSelection.displayName = 'MoleculeSelectSingleSelection'
+
+MoleculeSelectSingleSelection.defaultProps = {
+  value: '',
+  closeOnSelect: true
+}
+
+export default MoleculeSelectSingleSelection

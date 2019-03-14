5eabb0ff64b66b836af0fdba9a92370d32621a14
Ferran Simon <ferran.simon@infojobs.net>
2019-03-14 10:01:18 +0100

    feat(atom/progressBar): dinamyc transition time

--- components/atom/progressBar/src/CircleProgressBar/Circle/index.js
+++ components/atom/progressBar/src/CircleProgressBar/Circle/index.js
@@ -2,8 +2,28 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
 class Circle extends Component {
+
+  state = {
+    currentPercent: this.props.percent,
+    transitionTime: 0
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    if (Math.abs(props.percent - state.currentPercent) < 5) {
+      return {
+        currentPercent: props.percent,
+        transitionTime: 0
+      }
+    }
+    return {
+        currentPercent: props.percent,
+        transitionTime: (1250*props.percent/100)/1000
+      }
+  }
+
   getPathStyles(percent, strokeWidth) {
     const radius = 50 - strokeWidth / 2
+    const {transitionTime} = this.state
     const d = `M 50,50 m 0,-${radius}
      a ${radius},${radius} 0 1 1 0,${2 * radius}
      a ${radius},${radius} 0 1 1 0,-${2 * radius}`
@@ -11,7 +31,7 @@ class Circle extends Component {
     const style = {
       strokeDasharray: `${len}px ${len}px`,
       strokeDashoffset: `${((100 - percent) / 100) * len}px`,
-      transition: 'stroke-dashoffset 0.01s ease 0s, stroke 0.01s ease'
+      transition: `stroke-dashoffset ${transitionTime}s ease 0s, stroke ${transitionTime}s ease`
     }
     return {
       d,

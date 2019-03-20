4e376c5afd18a9184f07bf59b73d9291f918086f
JuanMa Garrido <juanma.garrido@gmail.com>
2019-03-20 10:12:04 +0100

    feat(behavior/sticky): first draft & demo

--- components/behavior/sticky/src/index.js
+++ components/behavior/sticky/src/index.js
@@ -1,27 +1,19 @@
-import React, {Component} from 'react'
-// import PropTypes from 'prop-types'
-import cx from 'classnames'
+import React from 'react'
+import * as ReactSticky from 'react-stickup'
 
 const BASE_CLASS = 'sui-BehaviorSticky'
-// const CLASS_TOP = `${BASE_CLASS}--top`
-// const CLASS_BOTTOM = `${BASE_CLASS}--bottom`
 
-class BehaviorSticky extends Component {
-  render() {
-    const {children /*, bottom */} = this.props // eslint-disable-line react/prop-types
-    return <div className={cx(BASE_CLASS)}>{children}</div>
-  }
+const BehaviorStickyProvider = ReactSticky.StickyProvider
+
+const BehaviorSticky = props => {
+  // eslint-disable-line react/prop-types
+  return (
+    <ReactSticky.Sticky className={BASE_CLASS} {...props}>
+      {props.children}
+    </ReactSticky.Sticky>
+  )
 }
 
 BehaviorSticky.displayName = 'BehaviorSticky'
 
-// Remove these comments if you need
-// BehaviorSticky.contextTypes = {i18n: PropTypes.object}
-
-// BehaviorSticky.propTypes = {
-//   bottom: PropTypes.bool
-// }
-
-// BehaviorSticky.defaultProps = {}
-
-export default BehaviorSticky
+export {BehaviorStickyProvider, BehaviorSticky}

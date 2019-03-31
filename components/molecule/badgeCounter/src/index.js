853a3dfad0d92e8c1aa43563d684f09257772980
JuanMa Garrido <juanma.garrido@gmail.com>
2019-03-31 14:10:59 +0200

    feat(molecule/badgeCounter): position bullet when text

--- components/molecule/badgeCounter/src/index.js
+++ components/molecule/badgeCounter/src/index.js
@@ -3,9 +3,11 @@ import cx from 'classnames'
 import PropTypes from 'prop-types'
 
 const BASE_CLASS = `sui-MoleculeBadgeCounter`
-const CLASS_WITH_CHILDREN = `${BASE_CLASS}-with-children`
-const CLASS_SMALL = `${BASE_CLASS}-small`
-const CLASS_MEDIUM = `${BASE_CLASS}-medium`
+const CLASS_WITH_CHILDREN = `${BASE_CLASS}--with-children`
+
+const CLASS_BULLET = `${BASE_CLASS}-bullet`
+const CLASS_SMALL = `${CLASS_BULLET}-small`
+const CLASS_MEDIUM = `${CLASS_BULLET}-medium`
 const CLASS_MEDIUM_ONE_CHAR = `${CLASS_MEDIUM}-one-character`
 const CLASS_MEDIUM_TWO_CHARS = `${CLASS_MEDIUM}-two-characters`
 const CLASS_MEDIUM_THREE_CHARS = `${CLASS_MEDIUM}-three-characters`
@@ -34,12 +36,19 @@ const getClassLengthLabel = label => {
 }
 const MoleculeBadgeCounter = ({children, label, size, variant}) => {
   const hasLabel = Boolean(label)
+  const hasChildren = Boolean(children)
+
   const CLASS_SIZE =
     size === SIZES.MEDIUM || hasLabel ? CLASS_MEDIUM : CLASS_SMALL
   const CLASS_LENGTH_LABEL = hasLabel ? getClassLengthLabel(label) : ''
   const CLASS_VARIANT = variant ? `${CLASS_SIZE}-${variant}` : ''
-  const className = cx(
-    BASE_CLASS,
+
+  const className = cx(BASE_CLASS, {
+    [CLASS_WITH_CHILDREN]: hasChildren
+  })
+
+  const classNameBullet = cx(
+    CLASS_BULLET,
     CLASS_SIZE,
     CLASS_VARIANT,
     CLASS_LENGTH_LABEL,
@@ -47,7 +56,12 @@ const MoleculeBadgeCounter = ({children, label, size, variant}) => {
       [CLASS_WITH_CHILDREN]: Boolean(children)
     }
   )
-  return <span className={className}>{label}</span>
+  return (
+    <span className={className}>
+      <span className={classNameBullet}>{label}</span>
+      <span>{children}</span>
+    </span>
+  )
 }
 
 MoleculeBadgeCounter.displayName = 'MoleculeBadgeCounter'
@@ -70,6 +84,6 @@ MoleculeBadgeCounter.propTypes = {
 
 export default MoleculeBadgeCounter
 export {
-  SIZES as MoleculeBadgeCounterSizes,
-  VARIANTS as MoleculeBadgeCounterVariants
+  SIZES as moleculeBadgeCounterSizes,
+  VARIANTS as moleculeBadgeCounterVariants
 }

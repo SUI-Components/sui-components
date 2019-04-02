bba203c93a08f7e611ec0baf711a08d58826a260
Adria Velardos Palomar <adria.velardos@habitaclia.com>
2019-04-02 10:44:03 +0200

    refactor(molecule/accordion): transform subcomponent Tab to function

--- components/molecule/accordion/src/Tab/index.js
+++ components/molecule/accordion/src/Tab/index.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import cx from 'classnames'
 
@@ -13,60 +13,47 @@ const BUTTON_TITLE_CLASS = `${BUTTON_CLASS}Title`
 const ICON_CLASS = `${BASE_CLASS}Icon`
 const MAX_HEIGHT = 100
 
-class Tab extends Component {
-  toggleCollapse = () => {
-    const {onToggle} = this.props
-    onToggle()
-  }
+const Tab = ({
+  onToggle,
+  children,
+  icon,
+  title,
+  withTransition,
+  isOpen,
+  maxHeight
+}) => {
+  const wrapperClassName = cx(BASE_CLASS, {
+    [OPEN_CLASS]: isOpen
+  })
+  const iconClassName = cx(ICON_CLASS, {
+    [OPEN_CLASS]: isOpen
+  })
+  const containerClassName = cx(CONTAINER_BUTTON_CLASS, {
+    [OPEN_CLASS]: isOpen
+  })
+  const contentClassName = cx(CONTENT_CONTAINER_CLASS, {
+    [`${CONTENT_CONTAINER_CLASS}--withTransition`]: withTransition
+  })
+  const containerHeight = isOpen ? `${maxHeight}px` : `0px`
 
-  render() {
-    const {
-      children,
-      icon,
-      title,
-      withTransition,
-      isOpen,
-      maxHeight
-    } = this.props
-    const wrapperClassName = cx(BASE_CLASS, {
-      [OPEN_CLASS]: isOpen
-    })
-    const iconClassName = cx(ICON_CLASS, {
-      [OPEN_CLASS]: isOpen
-    })
-    const containerClassName = cx(CONTAINER_BUTTON_CLASS, {
-      [OPEN_CLASS]: isOpen
-    })
-    const contentClassName = cx(CONTENT_CONTAINER_CLASS, {
-      [`${CONTENT_CONTAINER_CLASS}--withTransition`]: withTransition
-    })
-    const containerHeight = isOpen ? `${maxHeight}px` : `0px`
-
-    return (
-      <div className={wrapperClassName}>
-        <div className={containerClassName}>
-          <button
-            type="button"
-            className={BUTTON_CLASS}
-            onClick={this.toggleCollapse}
-          >
-            <span className={BUTTON_CONTENT_CLASS} tabIndex="-1">
-              <span className={BUTTON_TITLE_CLASS}>{title}</span>
-              <span className={iconClassName}>{icon}</span>
-            </span>
-          </button>
-        </div>
-        <div
-          className={contentClassName}
-          style={{maxHeight: `${containerHeight}`}}
-        >
-          <div className={CONTENT_CLASS} ref={this.childrenContainer}>
-            {children}
-          </div>
-        </div>
+  return (
+    <div className={wrapperClassName}>
+      <div className={containerClassName}>
+        <button type="button" className={BUTTON_CLASS} onClick={onToggle}>
+          <span className={BUTTON_CONTENT_CLASS} tabIndex="-1">
+            <span className={BUTTON_TITLE_CLASS}>{title}</span>
+            <span className={iconClassName}>{icon}</span>
+          </span>
+        </button>
       </div>
-    )
-  }
+      <div
+        className={contentClassName}
+        style={{maxHeight: `${containerHeight}`}}
+      >
+        <div className={CONTENT_CLASS}>{children}</div>
+      </div>
+    </div>
+  )
 }
 
 Tab.displayName = 'Tab'

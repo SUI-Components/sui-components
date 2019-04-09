88d5eb18822ac05159f609bfdbd7b46f514fa2fb
midudev <miduga@gmail.com>
2019-04-09 22:14:29 +0200

    feat(atom/tag): Move atom/tag to React hooks

--- components/atom/tag/src/Standard.js
+++ components/atom/tag/src/Standard.js
@@ -1,39 +1,31 @@
-import React, {Component} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import cx from 'classnames'
 
-class StandardTag extends Component {
-  get _classNames() {
-    const {className, closeIcon} = this.props
-    return cx(className, closeIcon && 'sui-AtomTag-hasClose')
-  }
+const StandardTag = ({className, closeIcon, icon, label, onClose}) => {
+  const classNames = cx(className, closeIcon && 'sui-AtomTag-hasClose')
 
-  handleClick = ev => {
-    const {onClose} = this.props
+  const handleClick = ev => {
     onClose(ev)
     ev.stopPropagation()
   }
 
-  render() {
-    const {closeIcon, icon, label} = this.props
-    const {handleClick} = this
-    return (
-      <span className={this._classNames}>
-        {icon && <span className="sui-AtomTag-icon">{icon}</span>}
-        <span className="sui-AtomTag-label" title={label}>
-          {label}
-        </span>
-        {closeIcon && (
-          <span
-            className="sui-AtomTag-secondary-closeable sui-AtomTag-secondary-icon"
-            onClick={handleClick}
-          >
-            {closeIcon}
-          </span>
-        )}
+  return (
+    <span className={classNames}>
+      {icon && <span className="sui-AtomTag-icon">{icon}</span>}
+      <span className="sui-AtomTag-label" title={label}>
+        {label}
       </span>
-    )
-  }
+      {closeIcon && (
+        <span
+          className="sui-AtomTag-secondary-closeable sui-AtomTag-secondary-icon"
+          onClick={handleClick}
+        >
+          {closeIcon}
+        </span>
+      )}
+    </span>
+  )
 }
 
 StandardTag.propTypes = {

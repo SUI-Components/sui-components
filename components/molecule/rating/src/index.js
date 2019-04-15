01a420b519989cbea82ff5711a39b6dc4753da68
JuanMa Garrido <juanma.garrido@gmail.com>
2019-04-15 10:31:09 +0200

    feat(molecule/rating): component creation

--- components/molecule/rating/src/index.js
+++ components/molecule/rating/src/index.js
@@ -1,12 +1,29 @@
 import React, {Component} from 'react'
 // import PropTypes from 'prop-types'
+import RatingSVG from 'react-rating-svg'
 
 const BASE_CLASS = `sui-MoleculeRating`
 class MoleculeRating extends Component {
+  state = {
+    value: 0
+  }
+
+  handleChange(value, name) {
+    console.log(name, value)
+    this.setState({value: value})
+  }
+
   render() {
+    const {value} = this.state
+    const {handleChange} = this
+
     return (
       <div className={BASE_CLASS}>
-        <h1>MoleculeRating</h1>
+        <RatingSVG
+          name="my-controlled-rating"
+          value={value}
+          onChange={handleChange}
+        />
       </div>
     )
   }

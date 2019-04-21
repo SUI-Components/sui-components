3f66860fc965a5a30c9c11487659289af4a19889
JuanMa Garrido <juanma.garrido@gmail.com>
2019-04-21 19:37:48 +0200

    feat(molecule/dataCounter): functional draft w/ hoc

--- components/molecule/dataCounter/src/index.js
+++ components/molecule/dataCounter/src/index.js
@@ -1,23 +1,73 @@
-import React, {Component} from 'react'
-// import PropTypes from 'prop-types'
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import AtomButton from '@schibstedspain/sui-atom-button'
+import AtomInput, {inputSizes} from '@s-ui/react-atom-input'
+import AtomLabel from '@s-ui/react-atom-label'
+
+import {withStateValue} from '@s-ui/hoc'
 
 const BASE_CLASS = `sui-MoleculeDataCounter`
+const CLASS_INPUT_CONTAINER = `${BASE_CLASS}-container`
 
-class MoleculeDataCounter extends Component {
-  render() {
-    return (
-      <div className={BASE_CLASS}>
-        <h1>MoleculeDataCounter</h1>
-      </div>
-    )
+const MoleculeDataCounter = ({
+  id,
+  label,
+  value = 1,
+  charsSize = 1,
+  placeholder = '1',
+  onChange
+}) => {
+  const incrementValue = e => {
+    onChange(e, {value: value + 1})
   }
+
+  const decrementValue = e => {
+    onChange(e, {value: value - 1})
+  }
+
+  return (
+    <div className={BASE_CLASS}>
+      <AtomLabel name={id} text={label} />
+      <div className={CLASS_INPUT_CONTAINER}>
+        <AtomButton onClick={decrementValue} type="secondary">
+          -
+        </AtomButton>
+        <AtomInput
+          id={id}
+          size={inputSizes.SMALL}
+          charsSize={charsSize}
+          placeholder={placeholder}
+          value={value}
+        />
+        <AtomButton onClick={incrementValue} type="secondary">
+          +
+        </AtomButton>
+      </div>
+    </div>
+  )
 }
 
 MoleculeDataCounter.displayName = 'MoleculeDataCounter'
 
-// Remove these comments if you need
-// MoleculeDataCounter.contextTypes = {i18n: PropTypes.object}
-// MoleculeDataCounter.propTypes = {}
-// MoleculeDataCounter.defaultProps = {}
+MoleculeDataCounter.propTypes = {
+  /** Text to be displayed as label */
+  label: PropTypes.string.isRequired,
 
-export default MoleculeDataCounter
+  /** used as label for attribute and input element id */
+  id: PropTypes.string.isRequired,
+
+  /** A hint to the user of what can be entered in the control. The placeholder text must not contain carriage returns or line-feeds. */
+  placeholder: PropTypes.string,
+
+  /** width of input based in number of characters (native "size" attribute) */
+  charsSize: PropTypes.number,
+
+  /** value of the control */
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
+
+  /* callback to be called with every update of the input value */
+  onChange: PropTypes.func
+}
+
+export default withStateValue(MoleculeDataCounter)

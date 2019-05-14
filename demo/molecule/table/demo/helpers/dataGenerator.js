d9ca6d0e93cedbadf9c9efb53e9d6770575aa103
JuanMa Garrido <juanma.garrido@gmail.com>
2019-05-14 10:07:19 +0300

    feat(Root): data generator complex table

--- demo/molecule/table/demo/helpers/dataGenerator.js
+++ demo/molecule/table/demo/helpers/dataGenerator.js
@@ -7,3 +7,17 @@ export const generateDataTable = numRows =>
     age: Math.round(Math.random() * 80) + 20,
     address: faker.address.streetAddress()
   }))
+
+export const generateDataTableComplexCell = numRows =>
+  new Array(numRows).fill(0).map(() => ({
+    id: faker.random.uuid(),
+    user: {
+      name: faker.name.findName(),
+      image: faker.image.avatar(),
+      company: faker.company.companyName(),
+      age: Math.round(Math.random() * 80) + 20,
+      address: faker.address.streetAddress(),
+      tags: faker.random.words()
+    },
+    comments: faker.lorem.text()
+  }))

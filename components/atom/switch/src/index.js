f9f9f5325bbb25268bf67966aae1b73499f345f5
JuanMa Garrido <juanma.garrido@gmail.com>
2019-08-02 15:31:54 +0200

    feat(atom/switch): improved code based on PR feedback

--- components/atom/switch/src/index.js
+++ components/atom/switch/src/index.js
@@ -5,7 +5,13 @@ import {SingleSwitchTypeRender} from './SwitchType/single'
 import {SIZES, TYPES, SUPPORTED_KEYS} from './config'
 
 const AtomSwitch = props => {
-  const {initialValue, disabled, onToggle: _onToggle, value, type} = props
+  const {
+    initialValue,
+    disabled,
+    onToggle: onToggleCallback,
+    value,
+    type
+  } = props
   const [isToggle, setIsToggle] = useState(initialValue)
   const [isFocus, setIsFocus] = useState(false)
   const [isClick, setIsClick] = useState(false)
@@ -34,14 +40,14 @@ const AtomSwitch = props => {
 
     const newIsToggle = forceValue !== undefined ? forceValue : !isToggle
     setIsToggle(newIsToggle)
-    _onToggle(newIsToggle)
+    onToggleCallback(newIsToggle)
   }
 
   const onKeyDown = ev => {
     if (disabled === true) return
 
     if (SUPPORTED_KEYS.includes(ev.key)) {
-      _onToggle()
+      onToggleCallback()
       ev.preventDefault()
     }
   }
@@ -68,49 +74,37 @@ const AtomSwitch = props => {
 AtomSwitch.displayName = 'AtomSwitch'
 
 AtomSwitch.propTypes = {
-  /**
-   * Whether switch is checked on init. Uncontrolled state component
-   */
+  /** Whether switch is checked on init. Uncontrolled state component */
   initialValue: PropTypes.bool,
-  /**
-   * Size of switch: 'default', 'large'
-   */
+
+  /** Size of switch: 'default', 'large' */
   size: PropTypes.oneOf([SIZES.DEFAULT, SIZES.LARGE]),
-  /**
-   * Type of switch: 'toggle' (default), 'select', 'single'
-   */
+
+  /** Type of switch: 'toggle' (default), 'select', 'single' */
   type: PropTypes.oneOf([TYPES.TOGGLE, TYPES.SELECT, TYPES.SINGLE]),
-  /**
-   * Is Input disabled?
-   */
+
+  /** Is Input disabled? */
   disabled: PropTypes.bool,
-  /**
-   * Left label to be printed
-   */
+
+  /** Left label to be printed */
   labelLeft: PropTypes.string,
-  /**
-   * Right label to be printed
-   */
+
+  /** Right label to be printed */
   labelRight: PropTypes.string,
-  /**
-   * Form element name
-   */
+
+  /** Form element name */
   name: PropTypes.string.isRequired,
-  /**
-   * The label itself. Proxy from label
-   */
+
+  /** The label itself. Proxy from label */
   label: PropTypes.string.isRequired,
-  /**
-   * The optional label text. Proxy from label
-   */
+
+  /** The optional label text. Proxy from label */
   labelOptionalText: PropTypes.string,
-  /**
-   * Callback to be called when switch. Flag whenever switch is active or not sent
-   */
+
+  /** Callback to be called when switch. Flag whenever switch is active or not sent */
   onToggle: PropTypes.func.isRequired,
-  /**
-   * Whether switch is checked. Controlled state component. Don't combine with initialValue prop!
-   */
+
+  /** Whether switch is checked. Controlled state component. Don't combine with initialValue prop! */
   value: PropTypes.bool
 }
 

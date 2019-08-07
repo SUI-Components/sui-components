86c72e3cdec46452926d9369e42658f43bc9ecd6
midudev <miduga@gmail.com>
2019-08-07 12:11:54 +0200

    refactor(atom/upload): Handle linter errors

--- components/atom/upload/src/index.js
+++ components/atom/upload/src/index.js
@@ -54,10 +54,9 @@ class AtomUpload extends PureComponent {
         <span className={classNameIcon}>{IconStatus}</span>
         <div className={CLASS_BLOCK_TEXT}>
           <h4 className={CLASS_BLOCK_TEXT_MAIN}>{textStatus}</h4>
-          {status === STATUSES.ACTIVE &&
-            textExplanation && (
-              <p className={CLASS_BLOCK_TEXT_SECONDARY}>{textExplanation}</p>
-            )}
+          {status === STATUSES.ACTIVE && textExplanation && (
+            <p className={CLASS_BLOCK_TEXT_SECONDARY}>{textExplanation}</p>
+          )}
         </div>
       </div>
     )

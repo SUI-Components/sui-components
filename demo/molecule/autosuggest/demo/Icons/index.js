72e60bbcd72cc4147084d2b01f5f6265b2841531
JuanMa Garrido <juanma.garrido@gmail.com>
2019-09-03 15:49:38 +0200

    feat(Root): added select to demo

--- demo/molecule/autosuggest/demo/Icons/index.js
+++ demo/molecule/autosuggest/demo/Icons/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const IconClose = () => (
-  <svg viewBox="0 0 24 24">
+  <svg>
     <path
-      id="a"
+      id="IconClose"
       d="M13.42 12l4.79-4.8a1 1 0 0 0-1.41-1.41L12 10.58 7.21 5.79A1 1 0 0 0 5.8 7.2l4.79 4.8-4.79 4.79a1 1 0 1 0 1.41 1.41L12 13.41l4.8 4.79a1 1 0 0 0 1.41-1.41L13.42 12z"
     />
   </svg>
@@ -13,17 +13,17 @@ const IconArrowDown = () => (
   <svg>
     <defs>
       <path
-        id="a"
+        id="IconArrowDown"
         d="M14.5 18.5a1 1 0 0 1-.71-.29l-4.08-4.08a3 3 0 0 1 0-4.24l4.09-4.1a1 1 0 0 1 1.41 1.41l-4.09 4.1a1 1 0 0 0 0 1.41l4.08 4.08a1 1 0 0 1-.71 1.71h.01z"
       />
     </defs>
-    <g fillRule="evenodd">
+    <g fill="none" fillRule="evenodd">
       <path d="M0 0h24v24H0z" />
       <use
         fill="#666"
         fillRule="nonzero"
         transform="matrix(0 -1 -1 0 24.189 24.189)"
-        xlinkHref="#a"
+        xlinkHref="#IconArrowDown"
       />
     </g>
   </svg>
--- demo/molecule/autosuggest/demo/components/ComboCountries.js
+++ demo/molecule/autosuggest/demo/components/ComboCountries.js
@@ -3,32 +3,13 @@ import React, {useState} from 'react'
 import axios from 'axios'
 import {withStateValue} from '@s-ui/hoc'
 
-import MoleculeSelect from '../../../../../components/molecule/select/src'
+import MoleculeSelect from '@s-ui/react-molecule-select'
 import MoleculeAutosuggest from '../../../../../components/molecule/autosuggest/src'
 
 import MoleculeDropdownOption from '@s-ui/react-molecule-dropdown-option'
 
 import regions from '../data/regions.json'
-
-const IconArrowDown = () => (
-  <svg>
-    <defs>
-      <path
-        id="a"
-        d="M14.5 18.5a1 1 0 0 1-.71-.29l-4.08-4.08a3 3 0 0 1 0-4.24l4.09-4.1a1 1 0 0 1 1.41 1.41l-4.09 4.1a1 1 0 0 0 0 1.41l4.08 4.08a1 1 0 0 1-.71 1.71h.01z"
-      />
-    </defs>
-    <g fill="none" fillRule="evenodd">
-      <path d="M0 0h24v24H0z" />
-      <use
-        fill="#666"
-        fillRule="nonzero"
-        transform="matrix(0 -1 -1 0 24.189 24.189)"
-        xlinkHref="#a"
-      />
-    </g>
-  </svg>
-)
+import {IconArrowDown} from '../Icons/'
 
 const MoleculeSelectWithState = withStateValue(MoleculeSelect)
 const MoleculeAutosuggestWithState = withStateValue(MoleculeAutosuggest)

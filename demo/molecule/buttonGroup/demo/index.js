ce325e5aa2ddcf3202c7f211c6783e49b1acc0da
Julian Gernun <julian.gernun@schibsted.com>
2019-09-30 12:08:43 +0200

    feat(molecule/buttonGroup): remove type prop
    BREAKING CHANGE:
    remove type prop

--- demo/molecule/buttonGroup/demo/index.js
+++ demo/molecule/buttonGroup/demo/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types, no-unused-vars, no-console */
 
-import React from 'react'
+import React, {useState} from 'react'
 
 import MoleculeButtonGroup from '../../../../components/molecule/buttonGroup/src'
 import AtomButtom, {

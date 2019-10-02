ccb820fcc90b90c3cb6de4b075c6d696f4c28c73
JuanMa Garrido <juanma.garrido@gmail.com>
2019-10-02 10:56:45 +0200

    feat(atom/radioButton): styles

--- components/atom/radioButton/src/index.js
+++ components/atom/radioButton/src/index.js
@@ -1,13 +1,74 @@
-import React from 'react'
-// import PropTypes from 'prop-types'
+import React, {Fragment} from 'react'
+import PropTypes from 'prop-types'
+import cx from 'classnames'
 
-export default function AtomRadioButton() {
+const BASE_CLASS = 'sui-AtomRadioButton'
+
+const ERROR_STATES = {
+  ERROR: 'error',
+  SUCCESS: 'success'
+}
+
+const getErrorStateClass = errorState => {
+  if (errorState) return `${BASE_CLASS}--${ERROR_STATES.ERROR}`
+  if (errorState === false) return `${BASE_CLASS}--${ERROR_STATES.SUCCESS}`
+  return ''
+}
+
+const AtomRadioButton = ({
+  id,
+  disabled,
+  checked,
+  onChange: onChangeFromProps,
+  errorState,
+  value
+}) => {
+  const handleChange = ev => {
+    const {checked} = ev.target
+    if (!disabled) onChangeFromProps(ev, {value: checked})
+  }
+
+  const className = cx(BASE_CLASS, getErrorStateClass(errorState))
+
   return (
-    <div className="react-AtomRadioButton">
-      <h1>AtomRadioButton</h1>
-    </div>
+    <Fragment>
+      <input
+        className={className}
+        value={value}
+        type="radio"
+        id={id}
+        disabled={disabled}
+        checked={checked}
+        onChange={handleChange}
+      />
+    </Fragment>
   )
 }
 
 AtomRadioButton.displayName = 'AtomRadioButton'
-AtomRadioButton.propTypes = {}
+
+AtomRadioButton.defaultProps = {
+  checked: false
+}
+
+AtomRadioButton.propTypes = {
+  /* The DOM id global attribute. */
+  id: PropTypes.string.isRequired,
+
+  /* This Boolean attribute prevents the user from interacting with the input */
+  disabled: PropTypes.bool,
+
+  /* Mark the input as selected */
+  checked: PropTypes.bool,
+
+  /* onChange callback */
+  onChange: PropTypes.func.isRequired,
+
+  /* Will set a red/green border if set to true/false */
+  errorState: PropTypes.bool,
+
+  /* Value assigned to the radio button */
+  value: PropTypes.string
+}
+
+export default AtomRadioButton

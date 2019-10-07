8fe5df7368267e69b35265d545ccfc2f50b35787
JuanMa Garrido <juanma.garrido@gmail.com>
2019-10-07 13:27:48 +0200

    feat(molecule/radioButtonGroup): molecule radio button group

---
+++ components/molecule/radioButtonGroup/README.md
@@ -0,0 +1,23 @@
+# MoleculeRadioButtonGroup
+
+> Description
+
+<!-- ![](./assets/preview.png) -->
+
+## Installation
+
+```sh
+$ npm install @s-ui/react-molecule-radio-button-group --save
+```
+
+## Usage
+
+### Basic usage
+```js
+import MoleculeRadioButtonGroup from '@s-ui/react-molecule-radio-button-group'
+
+return (<MoleculeRadioButtonGroup />)
+```
+
+
+> **Find full description and more examples in the [demo page](#).**
---
+++ demo/molecule/radioButtonGroup/demo/index.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-console */
+import React from 'react'
+
+import AtomRadioButton from '../../../../components/atom/radioButton/src'
+import AtomRadioButtonField from '../../../../components/molecule/radioButtonField/src'
+import AtomRadioButtonGroup from '../../../../components/molecule/radioButtonGroup/src'
+
+import './index.scss'
+
+const BASE_CLASS_DEMO = `DemoAtomRadioButton`
+const CLASS_SECTION = `${BASE_CLASS_DEMO}-section`
+
+const Demo = () => {
+  return (
+    <div className={BASE_CLASS_DEMO}>
+      <h1>AtomRadioButton</h1>
+      <h2>Use Cases</h2>
+      <div className={CLASS_SECTION}>
+        <h3>Basic</h3>
+
+        <AtomRadioButtonGroup
+          onChange={(ev, {name, value}) => {
+            console.log({[name]: value})
+          }}
+          name="favorite-beatle"
+          value="john"
+        >
+          <AtomRadioButton value="john" />
+          <AtomRadioButton value="paul" />
+          <AtomRadioButton value="george" />
+          <AtomRadioButton value="ringo" />
+          <AtomRadioButton value="martin" disabled />
+        </AtomRadioButtonGroup>
+      </div>
+
+      <div className={CLASS_SECTION}>
+        <h3>Basic</h3>
+
+        <AtomRadioButtonGroup
+          onChange={(ev, {name, value}) => {
+            console.log({[name]: value})
+          }}
+          name="field-favorite-beatle"
+          value="john"
+        >
+          <AtomRadioButtonField id="john" value="john" label="John" />
+          <AtomRadioButtonField id="paul" value="paul" label="Paul" />
+          <AtomRadioButtonField id="george" value="george" label="George" />
+        </AtomRadioButtonGroup>
+      </div>
+    </div>
+  )
+}
+
+export default Demo

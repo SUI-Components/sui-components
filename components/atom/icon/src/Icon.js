2e1d9b2c77cbe0885b8898289ef86ce1e65ec183
midudev <miduga@gmail.com>
2019-10-31 14:46:05 +0100

    feat(atom/icon): Create Lazy and normal Icon

---
+++ components/atom/icon/src/Icon.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const IMG_ROLE = 'img'
+
+export default function AtomIcon({className, children, outerRef}) {
+  return (
+    <span className={className} role={IMG_ROLE} ref={outerRef}>
+      {children}
+    </span>
+  )
+}
+
+AtomIcon.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.element,
+  outerRef: PropTypes.func
+}

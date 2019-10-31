7a2eb73535bc72c6d366f3fcc4c2499be16a2cdd
midudev <miduga@gmail.com>
2019-10-31 08:17:07 +0100

    feat(atom/icon): add Sui Atom Icon component

---
+++ components/atom/icon/README.md
@@ -0,0 +1,23 @@
+# AtomIcon
+
+> Description
+
+<!-- ![](./assets/preview.png) -->
+
+## Installation
+
+```sh
+$ npm install @s-ui/react-atom-icon --save
+```
+
+## Usage
+
+### Basic usage
+```js
+import AtomIcon from '@s-ui/react-atom-icon'
+
+return (<AtomIcon />)
+```
+
+
+> **Find full description and more examples in the [demo page](#).**
---
+++ components/atom/icon/src/index.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import cx from 'classnames'
+
+const BASE_CLASS = 'sui-AtomIcon'
+export const ATOM_ICON_COLORS = {
+  accent: 'accent',
+  alert: 'alert',
+  currentColor: 'currentColor',
+  error: 'error',
+  primary: 'primary',
+  success: 'success'
+}
+export const ATOM_ICON_SIZES = {
+  small: 'small',
+  medium: 'medium',
+  large: 'large',
+  extraLarge: 'extraLarge'
+}
+
+export default function AtomIcon({
+  children,
+  color = ATOM_ICON_COLORS.currentColor,
+  size = ATOM_ICON_SIZES.small
+}) {
+  const className = cx(
+    BASE_CLASS,
+    `${BASE_CLASS}--${size}`,
+    color && `${BASE_CLASS}--${color}`
+  )
+
+  return <span className={className}>{children}</span>
+}
+
+AtomIcon.displayName = 'AtomIcon'
+AtomIcon.propTypes = {
+  /**
+   * Determine color of the icon
+   * Besides the primary color types, you could use currentColor to inherit the color from the parent.
+   * (default: ATOM_ICON_COLORS.currentColor)
+   */
+  color: PropTypes.oneOf(Object.keys(ATOM_ICON_COLORS)),
+  /**
+   * The children must be a SVG that follows the definition stated on the UXDEF.md.
+   */
+  children: PropTypes.element,
+  /**
+   * Determine the size of the icon. (default: ATOM_ICON_SIZES.medium)
+   */
+  size: PropTypes.oneOf(Object.keys(ATOM_ICON_SIZES))
+}

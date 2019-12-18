73a0fb3fad1564958be15de72d11433975b01fb6
Aitor Rodríguez Martínez <aitor.rodriguez990@gmail.com>
2019-12-18 08:57:57 +0100

    fix(organism/nestedCheckboxes): fix and little refactoring

--- components/organism/nestedCheckboxes/src/index.js
+++ components/organism/nestedCheckboxes/src/index.js
@@ -1,24 +1,23 @@
 import React, {useState} from 'react'
 import PropTypes from 'prop-types'
+import cx from 'classnames'
 
 import MoleculeCheckboxField from '@s-ui/react-molecule-checkbox-field'
 
 const baseClass = 'sui-OrganismNestedCheckboxes'
 
-const noop = () => {}
-
 const OrganismNestedCheckboxes = ({
   id,
-  labelParent = '',
-  onChangeParent = noop,
+  checkedIcon: CheckedIcon,
+  intermediateIcon: IntermediateIcon,
+  hideItemsIcon: HideItemsIcon,
   items = [],
   join = false,
-  onChangeItem = noop,
-  fullCheckedStyledIcon: FullCheckedStyledIcon,
-  halfCheckedStyledIcon: HalfCheckedStyledIcon,
+  labelParent = '',
+  onChangeItem = () => {},
+  onChangeParent = () => {},
   showItems: showItemsProp = true,
-  showItemsIcon: ShowItemsIcon,
-  hideItemsIcon: HideItemsIcon
+  showItemsIcon: ShowItemsIcon
 }) => {
   const [showItems, setShowItems] = useState(showItemsProp)
   const showToggleIcons = ShowItemsIcon !== null
@@ -33,13 +32,13 @@ const OrganismNestedCheckboxes = ({
     <ul className={baseClass}>
       <li>
         <MoleculeCheckboxField
-          checked={isParentFullChecked}
-          intermediate={isParentHalfChecked}
           id={id}
+          checked={isParentFullChecked}
+          checkedIcon={CheckedIcon}
+          intermediate={isParentHalfChecked}
+          intermediateIcon={IntermediateIcon}
           label={labelParent}
           onChange={() => onChangeParent(isParentFullOrHalfChecked)}
-          checkedIcon={FullCheckedStyledIcon}
-          intermediateIcon={HalfCheckedStyledIcon}
           toggleIcon={showItems ? HideItemsIcon : ShowItemsIcon}
           toggleIconOnChange={() => setShowItems(!showItems)}
         />
@@ -48,17 +47,19 @@ const OrganismNestedCheckboxes = ({
             {items.map(item => {
               const {id: childId, checked} = item
 
+              const listItemClass = `${baseClass}-listItem`
+              const listItemClassModifier = cx(listItemClass, {
+                [`${listItemClass}--joined`]: join
+              })
+
               return (
-                <li
-                  key={childId}
-                  className={`${baseClass}-listItem ${join ? 'is-joined' : ''}`}
-                >
+                <li key={childId} className={listItemClassModifier}>
                   <MoleculeCheckboxField
-                    controlledChecked={checked}
                     id={childId}
+                    checked={checked}
+                    checkedIcon={CheckedIcon}
+                    intermediateIcon={IntermediateIcon}
                     onChange={e => onChangeItem(e.target)}
-                    checkedIcon={FullCheckedStyledIcon}
-                    intermediateIcon={HalfCheckedStyledIcon}
                     {...item}
                   />
                 </li>
@@ -77,11 +78,14 @@ OrganismNestedCheckboxes.propTypes = {
   /* Used for the label and element identifier */
   id: PropTypes.string.isRequired,
 
-  /* Text to be displayed as label on parent checkbox */
-  labelParent: PropTypes.string,
+  /* Icon to show on items and on parent when all items are checked */
+  checkedIcon: PropTypes.elementType,
 
-  /* onChange callback for parent */
-  onChangeParent: PropTypes.func,
+  /* Icon to show on parent when some items are checked */
+  intermediateIcon: PropTypes.elementType,
+
+  /* Hide items icon */
+  hideItemsIcon: PropTypes.elementType,
 
   /* Shape for items */
   items: PropTypes.arrayOf(
@@ -95,23 +99,20 @@ OrganismNestedCheckboxes.propTypes = {
   /* Remove default padding on items */
   join: PropTypes.bool,
 
+  /* Text to be displayed as label on parent checkbox */
+  labelParent: PropTypes.string,
+
+  /* onChange callback for parent */
+  onChangeParent: PropTypes.func,
+
   /* Default visibility for items */
   showItems: PropTypes.bool,
 
   /* Show items icon */
   showItemsIcon: PropTypes.elementType,
 
-  /* Hide items icon */
-  hideItemsIcon: PropTypes.elementType,
-
   /* onChange callback for items */
-  onChangeItem: PropTypes.func,
-
-  /* Icon to show on items and on parent when all items are checked */
-  fullCheckedStyledIcon: PropTypes.elementType,
-
-  /* Icon to show on parent when some items are checked */
-  halfCheckedStyledIcon: PropTypes.elementType
+  onChangeItem: PropTypes.func
 }
 
 export default OrganismNestedCheckboxes

91918d3554768c51b9fd65e37c56525bcdb71e5a
Daniel Muñoz <daniel.munozr@schibsted.com>
2020-01-08 10:31:46 +0100

    feat(examples): create demo

---
+++ demo/molecule/selectPopover/demo/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import MoleculeSelectPopover from '../../../../components/molecule/selectPopover/src'
+import {IconArrowDown} from './Icons'
+
+const Demo = () => {
+  return (
+    <MoleculeSelectPopover defaultText="foo" iconArrowDown={IconArrowDown} />
+  )
+}
+
+export default Demo

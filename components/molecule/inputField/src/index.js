06b2d63fcc37088f5f8ab554f5c26efbf6b32d76
Luis Garrido <luis.garrido@schibsted.com>
2020-01-30 13:31:15 +0100

    feat(molecule/inputField): add new alert text type

--- components/molecule/inputField/src/index.js
+++ components/molecule/inputField/src/index.js
@@ -2,11 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import MoleculeField from '@s-ui/react-molecule-field'
-import AtomInput from '@s-ui/react-atom-input'
+import AtomInput, {inputStates} from '@s-ui/react-atom-input'
 
 const getErrorState = (success, error) => {
   if (success) return false
   if (error) return true
+}
+
+const getState = (success, error, alert) => {
+  if (success) return inputStates.SUCCESS
+  if (error) return inputStates.ERROR
+  if (alert) return inputStates.ALERT
 }
 
 const MoleculeInputField = ({
@@ -14,23 +20,32 @@ const MoleculeInputField = ({
   label,
   successText,
   errorText,
+  alertText,
   helpText,
   inline,
   onChange,
   ...props
 }) => {
   const errorState = getErrorState(successText, errorText)
+  const inputState = getState(successText, errorState, alertText)
+
   return (
     <MoleculeField
       name={id}
       label={label}
       successText={successText}
       errorText={errorText}
+      alertText={alertText}
       helpText={helpText}
       inline={inline}
       onChange={onChange}
     >
-      <AtomInput id={id} errorState={errorState} {...props} />
+      <AtomInput
+        id={id}
+        errorState={errorState}
+        state={inputState}
+        {...props}
+      />
     </MoleculeField>
   )
 }
@@ -52,6 +67,9 @@ MoleculeInputField.propTypes = {
 
   /** Error message to display when error state  */
   errorText: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
+
+  /** Alert message to display when alert state  */
+  alertText: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
 
   /** Help Text to display */
   helpText: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),

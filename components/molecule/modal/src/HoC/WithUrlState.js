326b3601fe4b73e9c7b395a2e958de95b1b27ff7
Rafael Moral Pareja <rafamp89@gmail.com>
2020-01-30 08:41:30 +0100

    fix(molecule/modal): moving functions inside effects

--- components/molecule/modal/src/HoC/WithUrlState.js
+++ components/molecule/modal/src/HoC/WithUrlState.js
@@ -1,12 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import PropTypes from 'prop-types'
 
-const getUrl = ({hash: hashName} = {}) => {
-  const location = window.location
-  const hash = hashName ? `#${hashName}` : ''
-  return `${location.origin}${location.pathname}${location.search}${hash}`
-}
-
 const checkHash = hash => window.location.hash.includes(hash)
 
 const emptyFn = () => {}
@@ -26,33 +20,39 @@ export default BaseComponent => {
     const {isOpen} = rest
     const [isPopStateChange, setIsPopStateChange] = useState(checkHash(hash))
 
-    const _handlePopState = ev => {
-      const hasHash = checkHash(hash)
-      setIsPopStateChange(true)
-
-      if (hasHash) openModalTrigger()
-      else onClose()
-    }
-
-    const _handleStateChange = () => {
-      if (isPopStateChange) return setIsPopStateChange(false)
-
-      const nextUrl = isOpen ? getUrl({hash}) : getUrl()
-      window.history.pushState({hash}, '', nextUrl)
-    }
-
     useEffect(() => {
+      const getUrl = ({hash: hashName} = {}) => {
+        const location = window.location
+        const hash = hashName ? `#${hashName}` : ''
+        return `${location.origin}${location.pathname}${location.search}${hash}`
+      }
+
+      const _handleStateChange = () => {
+        if (isPopStateChange) return setIsPopStateChange(false)
+
+        const nextUrl = isOpen ? getUrl({hash}) : getUrl()
+        window.history.pushState({hash}, '', nextUrl)
+      }
+
       _handleStateChange()
     }, [hash, isOpen]) // eslint-disable-line
 
     useEffect(() => {
-      if (isPopStateChange) openModalTrigger()
+      const _handlePopState = ev => {
+        const hasHash = checkHash(hash)
+        setIsPopStateChange(true)
+
+        if (hasHash) openModalTrigger()
+        else onClose()
+      }
+
+      if (checkHash(hash)) openModalTrigger()
 
       window.onpopstate = _handlePopState
       return () => {
         window.onpopstate = emptyFn
       }
-    }, []) // eslint-disable-line
+    }, [hash]) // eslint-disable-line
 
     return <BaseComponent {...rest} onClose={onClose} />
   }

3e5a3b0d1199fdd5a3045e32465ecc929764d1f5
Alejandro Ferrante <alejandro.ferrante@adevinta.com>
2020-02-26 12:39:45 +0100

    fix(atom/slider): remove console log

--- components/atom/slider/src/Label.js
+++ components/atom/slider/src/Label.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Label = ({value, formatter}) => {
-  console.log('render label', value)
   return <div className="sui-AtomSlider-label">{formatter(value)}</div>
 }
 

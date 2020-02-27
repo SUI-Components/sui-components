74c4a933e31fca3aecb69c377f0b16919cbd3f0b
David Martin Cerda <david.martin@scmspain.com>
2020-02-27 09:35:15 +0100

    feat(Root): add test

--- test/molecule/buttonGroupField/index.js
+++ test/molecule/buttonGroupField/index.js
@@ -1,14 +1,61 @@
+/* global MoleculeButtonGroupField */
+
 import React from 'react'
+import {render, getByText} from '@testing-library/react'
 
 import chai, {expect} from 'chai'
 import chaiDOM from 'chai-dom'
-import {render} from '@testing-library/react'
 
 chai.use(chaiDOM)
 
-describe('MoleculeButtonGroupField', () => {
-  it('Render', () => {
-    render(<MoleculeButtonGroupField />)
-    expect(true).to.be.eql(false)
+const moleculeButtonGroupTestId = 'testId'
+const label = 'Test Label'
+const help = 'Test description here'
+const success = 'Test Everything ok!'
+const error = 'Test All wrong!'
+const alert = 'Test Alert!!!'
+
+const createComponent = props => {
+  return (
+    <MoleculeButtonGroupField
+      id={moleculeButtonGroupTestId}
+      label={label}
+      {...props}
+    >
+      <>
+        <div>BUTTON 1</div>
+        <div>BUTTON 2</div>
+      </>
+    </MoleculeButtonGroupField>
+  )
+}
+
+describe('SUI - MoleculeButtonGroupField', () => {
+  it('User can see a label before button group', () => {
+    const component = createComponent({helpTest: help})
+    const {container} = render(component)
+    const expectedLabel = getByText(container, label)
+    expect(expectedLabel).to.be.exist
+  })
+
+  it('User can see successHelpText after button group', () => {
+    const component = createComponent({successText: success})
+    const {container} = render(component)
+    const expectedSuccessText = getByText(container, success)
+    expect(expectedSuccessText).to.be.exist
+  })
+
+  it('User can see errorHelpText after button group', () => {
+    const component = createComponent({errorText: error})
+    const {container} = render(component)
+    const expectedErrorText = getByText(container, error)
+    expect(expectedErrorText).to.be.exist
+  })
+
+  it('User can see alertHelpText after button group', () => {
+    const component = createComponent({alertText: alert})
+    const {container} = render(component)
+    const expectedAlertText = getByText(container, alert)
+    expect(expectedAlertText).to.be.exist
   })
 })

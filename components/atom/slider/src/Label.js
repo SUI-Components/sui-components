2b53c337b297eccd689f141353e9c8a7b108a8a1
andresin87 <andresin87@gmail.com>
2020-04-15 13:19:01 +0200

    fix(atom/slider): resulting slider behavior

--- components/atom/slider/src/Label.js
+++ components/atom/slider/src/Label.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Label = ({value, formatter}) => {
-  return (
-    <div className="sui-AtomSlider-labelContainer">
-      <div className="sui-AtomSlider-labelContainer--inner">
-        <div className="sui-AtomSlider-label" style={{left: `${value}%`}}>
-          {formatter(value)}
-        </div>
+const Label = ({value, formatter, percentage}) => (
+  <div className="sui-AtomSlider-labelContainer">
+    <div className="sui-AtomSlider-labelContainer--inner">
+      <div className="sui-AtomSlider-label" style={{left: `${percentage}%`}}>
+        {formatter(value)}
       </div>
     </div>
-  )
-}
+  </div>
+)
 
 Label.propTypes = {
+  percentage: PropTypes.number,
   value: PropTypes.string,
   formatter: PropTypes.func
 }

56966e890b3df6add39a5d870d4540b303595d2a
andresin87 <andresin87@gmail.com>
2020-06-30 11:14:34 +0200

    feat(atom/button): button link @mixin

--- components/atom/button/src/index.js
+++ components/atom/button/src/index.js
@@ -59,7 +59,9 @@ const getPropsWithDefaultValues = props => {
   let {color, design, type} = props
   // if color or design are defined, use them with the passed or default value
   if (color || design) {
-    color = color || 'primary'
+    if (design !== DESIGNS.LINK) {
+      color = color || 'primary'
+    }
     design = design || 'solid'
   } else {
     type = type || 'primary'

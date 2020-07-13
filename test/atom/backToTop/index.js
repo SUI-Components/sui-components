840ce403bacf1335f24c5bbc668655fc0477b836
andresin87 <andresin87@gmail.com>
2020-07-13 12:08:01 +0200

    test(Root): atom/backtoTop add tests

--- test/atom/backToTop/index.js
+++ test/atom/backToTop/index.js
@@ -3,20 +3,52 @@
  * */
 
 /* eslint react/jsx-no-undef:0 */
+/* eslint no-undef:0 */
 
-// import React from 'react'
-// import {render} from '@testing-library/react'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 import chai, {expect} from 'chai'
 import chaiDOM from 'chai-dom'
+import {render} from '@testing-library/react'
 
 chai.use(chaiDOM)
 
+const setupBuilder = Component => props => {
+  const container = document.createElement('div')
+  container.setAttribute('id', 'test-container')
+  const utils = render(<Component {...props} />, {
+    container: document.body.appendChild(container)
+  })
+  return utils
+}
+
 describe('atom/backToTop', () => {
-  it.skip('Render', () => {
-    // Example TO BE DELETED!!!!
-    // const {getByRole} = render(<AtomButton>HOLA</AtomButton>)
-    // expect(getByRole('button')).to.have.text('HOLA')
-    expect(true).to.be.eql(false)
+  const Component = AtomBackToTop
+  const setup = setupBuilder(Component)
+
+  it('should render without crashing', () => {
+    // Given
+    const props = {}
+
+    // When
+    const component = <Component {...props} />
+
+    // Then
+    const div = document.createElement('div')
+    ReactDOM.render(component, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  it('should NOT render null', () => {
+    // Given
+    const props = {}
+
+    // When
+    const {container} = setup(props)
+
+    // Then
+    expect(container.innerHTML).to.be.a('string')
+    expect(container.innerHTML).to.not.have.lengthOf(0)
   })
 })

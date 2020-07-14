0773da7368e5d918cc36530de44002825269cd2e
andresin87 <andresin87@gmail.com>
2020-07-14 12:55:27 +0200

    test(Root): add MoleculeRadioButtonField tests

--- test/molecule/radioButtonField/index.js
+++ test/molecule/radioButtonField/index.js
@@ -1,12 +1,16 @@
-/* global MoleculeRadioButtonField */
+/*
+ * Remember: YOUR COMPONENT IS DEFINED GLOBALLY
+ * */
 
 /* eslint react/jsx-no-undef:0 */
+/* eslint no-undef:0 */
 
 import React from 'react'
-import {render} from '@testing-library/react'
+import ReactDOM from 'react-dom'
 
 import chai, {expect} from 'chai'
 import chaiDOM from 'chai-dom'
+import {render} from '@testing-library/react'
 
 chai.use(chaiDOM)
 
@@ -19,17 +23,44 @@ const setupBuilder = Component => props => {
   return utils
 }
 
-const setup = setupBuilder(MoleculeRadioButtonField)
-
-chai.use(chaiDOM)
-
 describe('molecule/radioButtonField', () => {
+  const Component = MoleculeRadioButtonField
+  const setup = setupBuilder(Component)
+
+  it('should render without crashing', () => {
+    // Given
+    const props = {}
+
+    // When
+    const component = <Component {...props} />
+
+    // Then
+    const div = document.createElement('div')
+    ReactDOM.render(component, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  it('should NOT render null', () => {
+    // Given
+    const props = {}
+
+    // When
+    const {container} = setup(props)
+
+    // Then
+    expect(container.innerHTML).to.be.a('string')
+    expect(container.innerHTML).to.not.have.lengthOf(0)
+  })
+
   describe('props', () => {
     describe('label & nodeLabel', () => {
       it('should render the empty component if there is not label or nodeLabel props', async () => {
         // Given
+        const props = {}
+
         // When
-        const {container} = setup()
+        const {container} = setup(props)
+
         // Then
         expect(container).to.be.not.undefined
       })
@@ -39,8 +70,10 @@ describe('molecule/radioButtonField', () => {
         const props = {
           label: 'label'
         }
+
         // When
         const {container, getByText} = setup(props)
+
         // Then
         const labelElement = getByText(props.label)
         expect(container).to.be.not.undefined
@@ -48,12 +81,14 @@ describe('molecule/radioButtonField', () => {
         expect(labelElement.innerText).to.equal(props.label)
         expect(labelElement.classList.contains('sui-AtomLabel')).to.be.true
       })
+
       it.skip('should render the component if there is nodeLabel', async () => {
         // Given
         const text = 'nodeLabel'
         const props = {
           nodeLabel: <div className="testNodeLabel">{text}</div>
         }
+
         // When
         const {container, getByText} = setup(props)
 
@@ -64,6 +99,7 @@ describe('molecule/radioButtonField', () => {
         expect(labelElement.innerText).to.equal(text)
         expect(labelElement.classList.contains('testNodeLabel')).to.be.true
       })
+
       it('should render the component with label value if there is label and nodeLabel props', async () => {
         // Given
         const text = 'label'
@@ -71,6 +107,7 @@ describe('molecule/radioButtonField', () => {
           label: text,
           nodeLabel: <div className="testNodeLabel">{text}</div>
         }
+
         // When
         const {container, getByText} = setup(props)
 

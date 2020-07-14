c24148cb9fadb62225f7fe229435939a97f60589
andresin87 <andresin87@gmail.com>
2020-07-14 12:45:06 +0200

    test(Root): add MoleculePhotoUploader tests

--- test/molecule/photoUploader/index.js
+++ test/molecule/photoUploader/index.js
@@ -1,4 +1,12 @@
+/*
+ * Remember: YOUR COMPONENT IS DEFINED GLOBALLY
+ * */
+
+/* eslint react/jsx-no-undef:0 */
+/* eslint no-undef:0 */
+
 import React from 'react'
+import ReactDOM from 'react-dom'
 
 import chai, {expect} from 'chai'
 import chaiDOM from 'chai-dom'
@@ -6,9 +14,55 @@ import {render} from '@testing-library/react'
 
 chai.use(chaiDOM)
 
-describe('MoleculePhotoUploader', () => {
-  it.skip('Render', () => {
-    render(<MoleculePhotoUploader />)
+const setupBuilder = Component => props => {
+  const container = document.createElement('div')
+  container.setAttribute('id', 'test-container')
+  const utils = render(<Component {...props} />, {
+    container: document.body.appendChild(container)
+  })
+  return utils
+}
+
+describe('molecule/photoUploader', () => {
+  const Component = MoleculePhotoUploader
+  const setup = setupBuilder(Component)
+
+  it('should render without crashing', () => {
+    // Given
+    const props = {}
+
+    // When
+    const component = <Component {...props} />
+
+    // Then
+    const div = document.createElement('div')
+    ReactDOM.render(component, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  it('should NOT render null', () => {
+    // Given
+    const props = {}
+
+    // When
+    const {container} = setup(props)
+
+    // Then
+    expect(container.innerHTML).to.be.a('string')
+    expect(container.innerHTML).to.not.have.lengthOf(0)
+  })
+
+  it.skip('example', () => {
+    // Example TO BE DELETED!!!!
+
+    // Given
+    // const props = {}
+
+    // When
+    // const {getByRole} = setup(props)
+
+    // Then
+    // expect(getByRole('button')).to.have.text('HOLA')
     expect(true).to.be.eql(false)
   })
 })

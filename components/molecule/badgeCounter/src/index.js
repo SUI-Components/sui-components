6df6cf614a55414e8552584cca8d153c2918a378
Aitor Rodríguez Martínez <aitor.rodriguez990@gmail.com>
2020-10-16 13:07:24 +0200

    feat(molecule/badgeCounter): add Large size to molecule badgeConter

--- components/molecule/badgeCounter/src/index.js
+++ components/molecule/badgeCounter/src/index.js
@@ -8,12 +8,15 @@ const CLASS_WITH_CHILDREN = `${BASE_CLASS}--withChildren`
 const CLASS_BULLET = `${BASE_CLASS}-bullet`
 const CLASS_SMALL = `${CLASS_BULLET}-small`
 const CLASS_MEDIUM = `${CLASS_BULLET}-medium`
+const CLASS_LARGE = `${CLASS_BULLET}-large`
 
 const CLASS_MEDIUM_THREE_CHARS = `${CLASS_MEDIUM}--threeCharacters`
+const CLASS_LARGE_THREE_CHARS = `${CLASS_LARGE}--threeCharacters`
 
 const SIZES = {
   SMALL: 'small',
-  MEDIUM: 'medium'
+  MEDIUM: 'medium',
+  LARGE: 'large'
 }
 
 const VARIANTS = {
@@ -25,11 +28,11 @@ const MoleculeBadgeCounter = ({children, label = '', size, variant}) => {
   const hasLabel = Boolean(label)
   const hasChildren = Boolean(children)
 
-  const CLASS_SIZE =
-    size === SIZES.MEDIUM || hasLabel ? CLASS_MEDIUM : CLASS_SMALL
-  const CLASS_LENGTH_LABEL =
-    hasLabel && (label + '').length >= 3 ? CLASS_MEDIUM_THREE_CHARS : ''
-  const CLASS_VARIANT = variant ? `${CLASS_SIZE}-${variant}` : ''
+  const CLASS_SIZE = getClassSize({size, hasLabel})
+
+  const CLASS_LENGTH_LABEL = getClassLengthLabel({hasLabel, label, size})
+
+  const CLASS_VARIANT = variant ? `${CLASS_BULLET}-${variant}` : ''
 
   const className = cx(BASE_CLASS, {
     [CLASS_WITH_CHILDREN]: hasChildren
@@ -46,12 +49,30 @@ const MoleculeBadgeCounter = ({children, label = '', size, variant}) => {
       [CLASS_WITH_CHILDREN]: Boolean(children)
     }
   )
+
   return (
     <span className={className}>
       <span className={classNameBullet}>{processedLabel}</span>
       <span>{children}</span>
     </span>
   )
+}
+
+const getClassSize = ({size, hasLabel}) => {
+  if ((hasLabel && (size === SIZES.SMALL || !size)) || size === SIZES.MEDIUM) {
+    return CLASS_MEDIUM
+  } else if (size === SIZES.LARGE) {
+    return CLASS_LARGE
+  }
+
+  return CLASS_SMALL
+}
+
+const getClassLengthLabel = ({hasLabel, label, size}) => {
+  if (!hasLabel || (label + '').length < 3) return ''
+
+  if (size === SIZES.MEDIUM) return CLASS_MEDIUM_THREE_CHARS
+  return CLASS_LARGE_THREE_CHARS
 }
 
 MoleculeBadgeCounter.displayName = 'MoleculeBadgeCounter'
@@ -63,7 +84,7 @@ MoleculeBadgeCounter.propTypes = {
   /** Number to be displayed inside the bullet */
   label: PropTypes.number,
 
-  /** Size (small or medium) */
+  /** Size (small, medium or large) */
   size: PropTypes.oneOf(Object.values(SIZES)),
 
   /** Variant (dot or exclamation) */

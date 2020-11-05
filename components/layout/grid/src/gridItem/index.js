d210f8573807b3ba17aa41050a2572f8437ed11a
Oriol Puig <oriol.puig90@gmail.com>
2020-11-05 18:21:13 +0100

    feat(layout/grid): add offset support

--- components/layout/grid/src/gridItem/index.js
+++ components/layout/grid/src/gridItem/index.js
@@ -6,25 +6,35 @@ import {baseClass, CELL_NUMBERS} from '../settings'
 
 export default function LayoutGridItem({
   children,
-  xxsOffset,
-  xxs,
-  xs,
-  s,
-  m,
   l,
   lOffset,
+  m,
+  mOffset,
+  s,
+  sOffset,
   xl,
-  xxl
+  xlOffset,
+  xs,
+  xsOffset,
+  xxl,
+  xxlOffset,
+  xxs,
+  xxsOffset
 }) {
   const classNames = cx(
     `${baseClass}-item`,
     l && `${baseClass}-item--l-${l}`,
     lOffset && `${baseClass}-item--lOffset-${lOffset}`,
     m && `${baseClass}-item--m-${m}`,
+    mOffset && `${baseClass}-item--mOffset-${mOffset}`,
     s && `${baseClass}-item--s-${s}`,
+    sOffset && `${baseClass}-item--sOffset-${sOffset}`,
     xl && `${baseClass}-item--xl-${xl}`,
+    xlOffset && `${baseClass}-item--xlOffset-${xlOffset}`,
     xs && `${baseClass}-item--xs-${xs}`,
+    xsOffset && `${baseClass}-item--xsOffset-${xsOffset}`,
     xxl && `${baseClass}-item--xxl-${xxl}`,
+    xxlOffset && `${baseClass}-item--xxlOffset-${xxlOffset}`,
     xxs && `${baseClass}-item--xxs-${xxs}`,
     xxsOffset && `${baseClass}-item--xxsOffset-${xxsOffset}`
   )
@@ -36,13 +46,18 @@ LayoutGridItem.displayName = 'sui-LayoutGrid-Item'
 
 LayoutGridItem.propTypes = {
   children: PropTypes.node.isRequired,
-  xxsOffset: PropTypes.oneOf(CELL_NUMBERS),
-  xxs: PropTypes.oneOf(CELL_NUMBERS),
-  xs: PropTypes.oneOf(CELL_NUMBERS),
-  s: PropTypes.oneOf(CELL_NUMBERS),
-  m: PropTypes.oneOf(CELL_NUMBERS),
   l: PropTypes.oneOf(CELL_NUMBERS),
   lOffset: PropTypes.oneOf(CELL_NUMBERS),
+  m: PropTypes.oneOf(CELL_NUMBERS),
+  mOffset: PropTypes.oneOf(CELL_NUMBERS),
+  s: PropTypes.oneOf(CELL_NUMBERS),
+  sOffset: PropTypes.oneOf(CELL_NUMBERS),
   xl: PropTypes.oneOf(CELL_NUMBERS),
-  xxl: PropTypes.oneOf(CELL_NUMBERS)
+  xlOffset: PropTypes.oneOf(CELL_NUMBERS),
+  xs: PropTypes.oneOf(CELL_NUMBERS),
+  xsOffset: PropTypes.oneOf(CELL_NUMBERS),
+  xxl: PropTypes.oneOf(CELL_NUMBERS),
+  xxlOffset: PropTypes.oneOf(CELL_NUMBERS),
+  xxs: PropTypes.oneOf(CELL_NUMBERS),
+  xxsOffset: PropTypes.oneOf(CELL_NUMBERS)
 }

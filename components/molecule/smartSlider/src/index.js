2710c65c080f4c78ca0486659ab2d76de14721e3
jordi.munoz@adevinta.com <jordi.munoz@adevinta.com>
2020-11-13 09:44:41 +0100

    feat(molecule/smartSlider): first commit

---
+++ components/molecule/smartSlider/src/index.js
@@ -0,0 +1,12 @@
+// import PropTypes from 'prop-types'
+
+export default function MoleculeSmartSlider() {
+  return (
+    <div className="react-MoleculeSmartSlider">
+      <h1>MoleculeSmartSlider</h1>
+    </div>
+  )
+}
+
+MoleculeSmartSlider.displayName = 'MoleculeSmartSlider'
+MoleculeSmartSlider.propTypes = {}
---
+++ test/molecule/smartSlider/index.js
@@ -0,0 +1,57 @@
+/*
+ * Remember: YOUR COMPONENT IS DEFINED GLOBALLY
+ * */
+
+/* eslint react/jsx-no-undef:0 */
+/* eslint no-undef:0 */
+
+import ReactDOM from 'react-dom'
+
+import chai, {expect} from 'chai'
+import chaiDOM from 'chai-dom'
+
+chai.use(chaiDOM)
+
+describe('MoleculeSmartSlider', () => {
+  const Component = MoleculeSmartSlider
+  const setup = setupEnvironment(Component)
+
+  it('should render without crashing', () => {
+    // Given
+    const props = {}
+
+    // When
+    const component = <Component {...props} />
+
+    // Then
+    const div = document.createElement('div')
+    ReactDOM.render(component, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  it('should NOT render null', () => {
+    // Given
+    const props = {}
+
+    // When
+    const {container} = setup(props)
+
+    // Then
+    expect(container.innerHTML).to.be.a('string')
+    expect(container.innerHTML).to.not.have.lengthOf(0)
+  })
+
+  it('example to be deleted', () => {
+    // Example TO BE DELETED!!!!
+
+    // Given
+    // const props = {}
+
+    // When
+    // const {getByRole} = setup(props)
+
+    // Then
+    // expect(getByRole('button')).to.have.text('HOLA')
+    expect(true).to.be.eql(false)
+  })
+})

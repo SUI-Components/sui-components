2afdf9b2d49a3cfccc1447dd7de9e2cedf6845f1
“andresin87” <andres.lucas@adevinta.com>
2020-12-21 14:27:18 +0100

    fix(Root): test actionButton

--- test/atom/actionButton/index.js
+++ test/atom/actionButton/index.js
@@ -14,16 +14,14 @@ chai.use(chaiDOM)
 
 describe('atom/actionButton', () => {
   const Component = AtomActionButton
-  const Icon = () => (
-    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-      <path d="m6.3506 6.3506 2.649 2.649h-6v-6l1.938 1.938c1.842-1.849 4.347-2.938 7.062-2.938 5.515 0 10 4.486 10 10h-2c0-4.411-3.588-8-8-8-2.172 0-4.176.872-5.649 2.351zm11.2988 11.2988-2.649-2.649h6v6l-1.938-1.939c-1.842 1.85-4.347 2.939-7.062 2.939-5.515 0-10-4.486-10-10h2c0 4.411 3.588 8 8 8 2.172 0 4.176-.872 5.649-2.351z" />
-    </svg>
-  )
+  const Icon = () => null
   const setup = setupEnvironment(Component)
 
   it('should render without crashing', () => {
     // Given
-    const props = {}
+    const props = {
+      icon: <Icon />,
+    }
 
     // When
     const component = <Component {...props} />
@@ -36,7 +34,9 @@ describe('atom/actionButton', () => {
 
   it('should NOT render null', () => {
     // Given
-    const props = {}
+    const props = {
+      icon: <Icon />,
+    }
 
     // When
     const {container} = setup(props)

56fbeb7fbb10a160cb6d40ad9c6cdec3fbd6b9f4
“andresin87” <andres.lucas@adevinta.com>
2021-01-29 13:20:51 +0100

    docs(Root): add atom-icon to modal demo

--- demo/molecule/modal/demo/helperComponents.js
+++ demo/molecule/modal/demo/helperComponents.js
@@ -1,12 +1,19 @@
 /* eslint react/prop-types: 0 */
 /* eslint react/no-multi-comp: 0 */
+
+import AtomIcon, {
+  ATOM_ICON_SIZES
+} from '@s-ui/react-atom-icon'
+
 const IconClose = () => (
-  <svg viewBox="0 0 24 24">
-    <path
-      id="a"
-      d="M13.42 12l4.79-4.8a1 1 0 0 0-1.41-1.41L12 10.58 7.21 5.79A1 1 0 0 0 5.8 7.2l4.79 4.8-4.79 4.79a1 1 0 1 0 1.41 1.41L12 13.41l4.8 4.79a1 1 0 0 0 1.41-1.41L13.42 12z"
-    />
-  </svg>
+  <AtomIcon size={ATOM_ICON_SIZES.medium}>
+    <svg viewBox="0 0 24 24">
+      <path
+        id="a"
+        d="M13.42 12l4.79-4.8a1 1 0 0 0-1.41-1.41L12 10.58 7.21 5.79A1 1 0 0 0 5.8 7.2l4.79 4.8-4.79 4.79a1 1 0 1 0 1.41 1.41L12 13.41l4.8 4.79a1 1 0 0 0 1.41-1.41L13.42 12z"
+      />
+    </svg>
+  </AtomIcon>
 )
 
 const ContentWithCloseButton = ({children, onClose}) => (

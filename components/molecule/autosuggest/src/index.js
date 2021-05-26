7219434ef7b36d4158eaa4b325b91001552be58a
“andresin87” <andres.lucas@adevinta.com>
2021-05-26 15:51:19 +0200

    refactor(molecule/autosuggest): remove nulled useRef param

--- components/molecule/autosuggest/src/index.js
+++ components/molecule/autosuggest/src/index.js
@@ -59,7 +59,7 @@ const MoleculeAutosuggest = ({
   state,
   ...restProps
 }) => {
-  const innerRefMoleculeAutosuggest = useRef(null)
+  const innerRefMoleculeAutosuggest = useRef(useRef(null))
   const refMoleculeAutosuggest = useMergeRefs(
     ...[innerRefMoleculeAutosuggest, refMoleculeAutosuggestFromProps].filter(
       isValidRef
@@ -67,7 +67,7 @@ const MoleculeAutosuggest = ({
   )
 
   const refsMoleculeAutosuggestOptions = useRef([])
-  const innerRefMoleculeAutosuggestInput = useRef()
+  const innerRefMoleculeAutosuggestInput = useRef(null)
   const refMoleculeAutosuggestInput = useMergeRefs(
     ...[
       innerRefMoleculeAutosuggestInput,

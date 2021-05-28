0a88ebaf2b0727ebf48bec7971c14c5f9697fb7f
Marco Antonio Ghiani <marcoantonio.ghiani01@gmail.com>
2021-05-28 11:57:54 +0200

    feat(molecule/dropdownList): forward ref

--- components/molecule/dropdownList/src/index.js
+++ components/molecule/dropdownList/src/index.js
@@ -1,5 +1,13 @@
-import {Children, cloneElement, useState, useEffect, useRef} from 'react'
+import {
+  Children,
+  forwardRef,
+  cloneElement,
+  useState,
+  useEffect,
+  useRef
+} from 'react'
 import useDebounce from '@s-ui/react-hooks/lib/useDebounce'
+import useMergeRefs from '@s-ui/react-hooks/lib/useMergeRefs'
 import isEqual from 'lodash.isequal'
 import PropTypes from 'prop-types'
 import cx from 'classnames'
@@ -14,97 +22,96 @@ const SIZES = {
   LARGE: 'large'
 }
 
-const MoleculeDropdownList = ({
-  children,
-  onSelect,
-  alwaysRender,
-  size,
-  value,
-  visible,
-  ...props
-}) => {
-  const refDropdownList = useRef()
-  const [typedWord, setTypedWord] = useState('')
-  const debouncedTypedWord = useDebounce(typedWord, DEBOUNCE_TIME)
+const MoleculeDropdownList = forwardRef(
+  (
+    {children, onSelect, alwaysRender, size, value, visible, ...props},
+    forwardedRef
+  ) => {
+    const refDropdownList = useRef()
+    const ref = useMergeRefs(refDropdownList, forwardedRef)
 
-  const extendedChildren = Children.toArray(children)
-    .filter(Boolean)
-    .map((child, index) => {
-      const {value: valueChild} = child.props
-      let selected = false
-      if (Array.isArray(value)) {
-        selected = value.some(innerValue => isEqual(valueChild, innerValue))
-      } else {
-        selected = isEqual(value, valueChild)
-      }
-      return cloneElement(child, {
-        ...props,
-        index,
-        onSelect,
-        selected
+    const [typedWord, setTypedWord] = useState('')
+    const debouncedTypedWord = useDebounce(typedWord, DEBOUNCE_TIME)
+
+    const extendedChildren = Children.toArray(children)
+      .filter(Boolean)
+      .map((child, index) => {
+        const {value: valueChild} = child.props
+        let selected = false
+        if (Array.isArray(value)) {
+          selected = value.some(innerValue => isEqual(valueChild, innerValue))
+        } else {
+          selected = isEqual(value, valueChild)
+        }
+        return cloneElement(child, {
+          ...props,
+          index,
+          onSelect,
+          selected
+        })
       })
+
+    const classNames = cx(BASE_CLASS, `${BASE_CLASS}--${size}`, {
+      [CLASS_HIDDEN]: !visible
     })
 
-  const classNames = cx(BASE_CLASS, `${BASE_CLASS}--${size}`, {
-    [CLASS_HIDDEN]: !visible
-  })
-
-  const getFocusedOptionIndex = options => {
-    const currentElementFocused = document.activeElement
-    return Array.from(options).reduce((focusedOptionIndex, option, index) => {
-      if (option === currentElementFocused) focusedOptionIndex = index
-      return focusedOptionIndex
-    }, 0)
-  }
-
-  const handleKeyDown = ev => {
-    const {key} = ev
-    const {
-      current: {children: options}
-    } = refDropdownList
-    const numOptions = options.length
-    const index = getFocusedOptionIndex(options)
-    if (key === 'ArrowDown' || key === 'ArrowUp') {
-      if (index >= 0 || index <= numOptions) {
-        if (key === 'ArrowDown' && index < numOptions - 1)
-          options[index + 1].focus()
-        if (key === 'ArrowUp' && index > 0) options[index - 1].focus()
-      }
-    } else {
-      setTypedWord(v => v + key.toLowerCase())
-      const word = typedWord + key.toLowerCase()
-      const optionToFocusOn =
-        Array.from(options).find(
-          (option, i) =>
-            i >= index && option.innerText.toLowerCase().indexOf(word) === 0
-        ) ||
-        Array.from(options).find(
-          option => option.innerText.toLowerCase().indexOf(word) === 0
-        )
-      optionToFocusOn && optionToFocusOn.focus()
+    const getFocusedOptionIndex = options => {
+      const currentElementFocused = document.activeElement
+      return Array.from(options).reduce((focusedOptionIndex, option, index) => {
+        if (option === currentElementFocused) focusedOptionIndex = index
+        return focusedOptionIndex
+      }, 0)
     }
-    ev.preventDefault()
-    ev.stopPropagation()
+
+    const handleKeyDown = ev => {
+      const {key} = ev
+      const {
+        current: {children: options}
+      } = refDropdownList
+      const numOptions = options.length
+      const index = getFocusedOptionIndex(options)
+      if (key === 'ArrowDown' || key === 'ArrowUp') {
+        if (index >= 0 || index <= numOptions) {
+          if (key === 'ArrowDown' && index < numOptions - 1)
+            options[index + 1].focus()
+          if (key === 'ArrowUp' && index > 0) options[index - 1].focus()
+        }
+      } else {
+        setTypedWord(v => v + key.toLowerCase())
+        const word = typedWord + key.toLowerCase()
+        const optionToFocusOn =
+          Array.from(options).find(
+            (option, i) =>
+              i >= index && option.innerText.toLowerCase().indexOf(word) === 0
+          ) ||
+          Array.from(options).find(
+            option => option.innerText.toLowerCase().indexOf(word) === 0
+          )
+        optionToFocusOn && optionToFocusOn.focus()
+      }
+      ev.preventDefault()
+      ev.stopPropagation()
+    }
+
+    // When DEBOUNCE_TIME reset typed word
+    useEffect(() => {
+      setTypedWord('')
+    }, [debouncedTypedWord])
+
+    if (!visible && !alwaysRender) return null
+
+    return (
+      <ul
+        ref={ref}
+        tabIndex={0}
+        onKeyDown={handleKeyDown}
+        className={classNames}
+      >
+        {extendedChildren}
+      </ul>
+    )
   }
-
-  // When DEBOUNCE_TIME reset typed word
-  useEffect(() => {
-    setTypedWord('')
-  }, [debouncedTypedWord])
-
-  if (!visible && !alwaysRender) return null
-
-  return (
-    <ul
-      ref={refDropdownList}
-      tabIndex={0}
-      onKeyDown={handleKeyDown}
-      className={classNames}
-    >
-      {extendedChildren}
-    </ul>
-  )
-}
+)
 
 MoleculeDropdownList.displayName = 'MoleculeDropdownList'
 

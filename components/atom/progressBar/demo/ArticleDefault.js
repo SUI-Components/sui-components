473037ab9a5b2a188c6f8fde77118828631ced1e
“andresin87” <andres.lucas@adevinta.com>
2021-05-31 15:23:17 +0200

    feat(components/atom/progressBar): PoC

--- components/atom/progressBar/demo/ArticleDefault.js
+++ components/atom/progressBar/demo/ArticleDefault.js
@@ -1,13 +1,19 @@
-import {Article, H2} from '@s-ui/documentation-library'
+import PropTypes from 'prop-types'
+import {Article, H2, Paragraph} from '@s-ui/documentation-library'
 import AtomProgressBar from 'components/atom/progressBar/src'
 
 const ArticlePosition = ({className}) => {
   return (
     <Article className={className}>
       <H2>Default</H2>
+      <Paragraph>Paragraph</Paragraph>
       <AtomProgressBar />
     </Article>
   )
+}
+
+ArticlePosition.propTypes = {
+  className: PropTypes.string
 }
 
 export default ArticlePosition

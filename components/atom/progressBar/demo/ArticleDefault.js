dc9de1c66d1e4e4a8306469032d656aa4c04874c
“andresin87” <andres.lucas@adevinta.com>
2021-05-31 15:06:12 +0200

    feat(components/atom/progressBar): PoC

--- components/atom/progressBar/demo/ArticleDefault.js
+++ components/atom/progressBar/demo/ArticleDefault.js
@@ -1,13 +1,13 @@
-import { Article, H2 } from '@s-ui/documentation-library'
+import {Article, H2} from '@s-ui/documentation-library'
 import AtomProgressBar from 'components/atom/progressBar/src'
 
 const ArticlePosition = ({className}) => {
   return (
-  <Article className={className}>
-    <H2>Default</H2>
-    <AtomProgressBar />
-  </Article>
+    <Article className={className}>
+      <H2>Default</H2>
+      <AtomProgressBar />
+    </Article>
   )
 }
 
-export default ArticlePosition;
+export default ArticlePosition
--- components/atom/progressBar/demo/index.js
+++ components/atom/progressBar/demo/index.js
@@ -6,15 +6,18 @@ const BASE_CLASS_DEMO = `DemoAtomProgressBar`
 const CLASS_SECTION = `${BASE_CLASS_DEMO}-section`
 
 const Demo = () => {
-  return (<div className="sui-StudioPreview">
-    <div className="sui-StudioPreview-content sui-StudioDemo-preview">
-      <H1>Progress Bar</H1>
-      <Paragraph>
-        A Progress bar is a visual representation of a specified wait time or the current status of a process.
-      </Paragraph>
-      <ArticleDefault className={CLASS_SECTION}/>
+  return (
+    <div className="sui-StudioPreview">
+      <div className="sui-StudioPreview-content sui-StudioDemo-preview">
+        <H1>Progress Bar</H1>
+        <Paragraph>
+          A Progress bar is a visual representation of a specified wait time or
+          the current status of a process.
+        </Paragraph>
+        <ArticleDefault className={CLASS_SECTION} />
+      </div>
     </div>
-  </div>);
+  )
 }
 
-export default Demo;
+export default Demo

a8dcdc6a04e091ded3c6d209887270af57968a9c
Daniel Muñoz <daniel.munozr@adevinta.com>
2021-06-01 14:25:50 +0200

    feat(components/molecule/imageEditor): create crop component

--- components/molecule/imageEditor/demo/index.js
+++ components/molecule/imageEditor/demo/index.js
@@ -1,2 +1,18 @@
+import {useState} from 'react'
 import MoleculeImageEditor from 'components/molecule/imageEditor/src'
-export default () => <MoleculeImageEditor />
+
+const DEMO_IMAGE =
+  'https://lp-cms-production.imgix.net/features/2019/06/panda-d55d15231c4f.jpg?auto=compress&fit=crop&fm=auto&sharp=10&vib=20&w=1200&h=800'
+
+export default () => {
+  const [croppedImage, setCroppedImage] = useState()
+
+  return (
+    <div>
+      <h1>Component</h1>
+      <MoleculeImageEditor onChange={setCroppedImage} image={DEMO_IMAGE} />
+      <h1>Result</h1>
+      <img src={croppedImage} style={{width: '300px'}} />
+    </div>
+  )
+}
--- components/molecule/imageEditor/src/index.js
+++ components/molecule/imageEditor/src/index.js
@@ -1,12 +1,72 @@
-// import PropTypes from 'prop-types'
+import {useState, useCallback} from 'react'
+import PropTypes from 'prop-types'
+import Cropper from 'react-easy-crop'
+import AtomSlider from '@s-ui/react-atom-slider'
 
-export default function MoleculeImageEditor() {
+import getCroppedImg from './utils/cropImage'
+
+const baseClass = 'react-MoleculeImageEditor'
+const DEFAULT_ASPECT = 4 / 3
+export default function MoleculeImageEditor({
+  aspect = DEFAULT_ASPECT,
+  image,
+  onChange
+}) {
+  const [crop, setCrop] = useState({x: 0, y: 0})
+  const [rotation, setRotation] = useState(0)
+  const [zoom, setZoom] = useState(0)
+
+  const getRotationDegrees = rotation => (rotation * 360) / 100
+
+  const onCropComplete = useCallback(
+    async (croppedArea, croppedAreaPixels) => {
+      const rotationDegrees = getRotationDegrees(rotation)
+      const croppedImage = await getCroppedImg(
+        image,
+        croppedAreaPixels,
+        rotationDegrees
+      )
+      onChange(croppedImage)
+    },
+    [rotation, image, onChange]
+  )
+
   return (
-    <div className="react-MoleculeImageEditor">
-      <h1>MoleculeImageEditor</h1>
+    <div className={baseClass}>
+      <div className={`${baseClass}-crop`}>
+        <Cropper
+          image={image}
+          crop={crop}
+          zoom={1 + zoom / 100}
+          rotation={getRotationDegrees(rotation)}
+          aspect={aspect}
+          onCropChange={setCrop}
+          onCropComplete={onCropComplete}
+          onRotationChange={setRotation}
+          onZoomChange={zoom => setZoom((zoom - 1) * 100)}
+        />
+      </div>
+      <div className={`${baseClass}-slider`}>
+        <AtomSlider
+          onChange={(event, {value}) => setZoom(value)}
+          value={zoom}
+          hideMarks
+        />
+      </div>
+      <div className={`${baseClass}-slider`}>
+        <AtomSlider
+          onChange={(event, {value}) => setRotation(value)}
+          value={rotation}
+          hideMarks
+        />
+      </div>
     </div>
   )
 }
 
 MoleculeImageEditor.displayName = 'MoleculeImageEditor'
-MoleculeImageEditor.propTypes = {}
+MoleculeImageEditor.propTypes = {
+  aspect: PropTypes.number,
+  image: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired
+}

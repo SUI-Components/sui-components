990742a259ad71591d48b49c407f1cf30b4ba895
“andresin87” <andres.lucas@adevinta.com>
2021-06-03 11:26:33 +0200

    chore(Root): PoC

--- components/atom/popover/demo/ArticleDefault.js
+++ components/atom/popover/demo/ArticleDefault.js
@@ -21,35 +21,10 @@ const ArticleDefault = ({className}) => {
       <Grid cols={6} gutter={[8, 8]}>
         <Cell span={3}>
           <H4>Uncontrolled component</H4>
-          <Paragraph>
-            The Popover is normally used as uncontrolled component. User can use
-            it wrapping the component desired to be targeted and deciding the
-            default onLoading configuration of the element under the{' '}
-            <Code>defaultIsVisible</Code> prop. By default it is false.
-          </Paragraph>
-          <Paragraph>
-            If the Popover is not visible and the use clicks on the target area,
-            the popover will toggle its internal visible state showing an
-            element positioned on top of the other elements of the document.
-          </Paragraph>
-          <Paragraph>
-            When the popover is visible, it there is any click or tap event on
-            any part of the document not contained in the popover content, it
-            will immediately toggle its inner visible state becoming invisible.
-          </Paragraph>
         </Cell>
 
         <Cell span={3}>
           <H4>Controlled component</H4>
-          <Paragraph>
-            The popover can be controlled using the <Code>isVisible</Code> prop.
-            It is a boolean prop and its value is undefined.
-          </Paragraph>
-          <Paragraph>
-            Acting as a controlled component, the behavior of its visibility
-            state might be fully controlled by an outer component defining its
-            function handler (<Code>onClose</Code>, <Code>onOpen</Code>) props.
-          </Paragraph>
         </Cell>
         <Cell span={3}>
           <Separator style={{display: 'flex', justifyContent: 'space-between'}}>

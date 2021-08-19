af0293f0f5346d4c63ab5eb32e37914089eee2e3
Marco Antonio Ghiani <marco.ghiani@adevinta.com>
2021-08-19 09:49:17 +0200

    docs(components/atom/tag/demo): update demo and readme

--- components/atom/tag/README.md
+++ components/atom/tag/README.md
@@ -116,12 +116,14 @@ $atom-tag-types: (
 
 ### Value
 
-Use the prop value if you want to use this the value on you onClick handler
+Use the prop value if you want to use this value on your onClick/onClose handler.
 
 ```js
 <AtomTag
   label="Tag with value"
   onClick={(_, {value}) => alert('click! this is my value', value)}
+  onClose={(_, {value}) => alert('click! this is my value', value)}
+  iconClose={<IconClose />}
   value={'test}
 />
 ```
--- components/atom/tag/demo/index.js
+++ components/atom/tag/demo/index.js
@@ -32,6 +32,10 @@ const closeIcon = (
   </AtomIcon>
 )
 
+const handleClose = (_event, data) => {
+  window.alert(`Click on close icon for tag with data: ${JSON.stringify(data)}`)
+}
+
 const BASE_CLASS_DEMO = `DemoAtomTag`
 const CLASS_SECTION = `${BASE_CLASS_DEMO}-section`
 
@@ -298,7 +302,7 @@ export default () => (
         closeIcon={closeIcon}
         label="Close Tag"
         value="Close Tag"
-        onClose={() => window.alert('close!')}
+        onClose={handleClose}
         responsive
       />
     </Article>
@@ -314,7 +318,7 @@ export default () => (
           closeIcon={closeIcon}
           icon={icon}
           label="Icon & Close Tag"
-          onClose={() => window.alert('close!')}
+          onClose={handleClose}
           responsive
           size={atomTagSizes.SMALL}
         />
@@ -330,14 +334,14 @@ export default () => (
           closeIcon={closeIcon}
           icon={icon}
           label="Icon & Close Tag"
-          onClose={() => window.alert('close!')}
+          onClose={handleClose}
           responsive
         />
         <AtomTag
           closeIcon={closeIcon}
           icon={icon}
           label="Icon & Close Tag"
-          onClose={() => window.alert('close!')}
+          onClose={handleClose}
           responsive
           size={atomTagSizes.LARGE}
         />

a42253b3a5fd8edc28c92b5b9f21f71fcc09e173
Albert Font <albert.font.fernandez@gmail.com>
2021-08-26 14:20:38 +0200

    feat(Root): Deleted molecule for PR

--- components/molecule/validationCode/src/index.js
+++
@@ -1,5 +0,0 @@
-import ValidationCode from './validationCode'
-import {pinInputStatus} from '@s-ui/react-atom-pin-input'
-
-export default ValidationCode
-export {pinInputStatus as validationCodeStatus}

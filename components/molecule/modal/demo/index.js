44fcf06903c23e43a0082642252680ba6ce6c4cc
“andresin87” <andres.lucas@adevinta.com>
2021-09-02 09:37:08 +0200

    docs(components/molecule/modal/demo): demo modal

--- components/molecule/modal/demo/index.js
+++ components/molecule/modal/demo/index.js
@@ -1,6 +1,6 @@
 /* eslint react/prop-types: 0 */
 import {H1, H2, Paragraph} from '@s-ui/documentation-library'
-// import ScrollModal from './ScrollModal'
+import ScrollModal from './ScrollModal'
 // import ContentlessModal from './ContentlessModal'
 // import SizeModal from './SizeModal'
 // import NoScrollModal from './NoScrollModal'
@@ -13,13 +13,13 @@ import {H1, H2, Paragraph} from '@s-ui/documentation-library'
 // import WithUrlStateModal from './WithUrlStateModal'
 // import WithoutAnimationModal from './WithoutAnimationModal'
 
-// const fieldStyle = {
-//   border: '1px solid rgb(204, 204, 204)',
-//   background: 'rgb(255, 255, 255)',
-//   marginTop: '10px',
-//   padding: '10px',
-//   maxWidth: '600px'
-// }
+const fieldStyle = {
+  border: '1px solid rgb(204, 204, 204)',
+  background: 'rgb(255, 255, 255)',
+  marginTop: '10px',
+  padding: '10px',
+  maxWidth: '600px'
+}
 
 const Demo = () => (
   <div>
@@ -27,6 +27,9 @@ const Demo = () => (
       <H1>Modal</H1>
       <Paragraph>dfghjkl</Paragraph>
       <H2>Scroll Modal</H2>
+      <div style={fieldStyle}>
+        <ScrollModal />
+      </div>
     </div>
   </div>
 )

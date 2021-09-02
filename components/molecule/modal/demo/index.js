5be4d65ff03f8bcbf881161fe010393b1ed655fd
“andresin87” <andres.lucas@adevinta.com>
2021-09-02 09:22:01 +0200

    docs(components/molecule/modal/demo): fix demo WIP

--- components/molecule/modal/demo/index.js
+++ components/molecule/modal/demo/index.js
@@ -27,61 +27,6 @@ const Demo = () => (
       <H1>Modal</H1>
       <Paragraph>dfghjkl</Paragraph>
       <H2>Scroll Modal</H2>
-      {/*<div style={fieldStyle}>*/}
-      {/*  <ScrollModal />*/}
-      {/*</div>*/}
-      {/*<h2>WithoutAnimationModal</h2>*/}
-      {/*<div style={fieldStyle}>*/}
-      {/*  <WithoutAnimationModal />*/}
-      {/*</div>*/}
-      {/*<h2>No Scroll Modal</h2>*/}
-      {/*<div style={fieldStyle}>*/}
-      {/*  <NoScrollModal />*/}
-      {/*</div>*/}
-      {/*<h2>No header Modal</h2>*/}
-      {/*<div style={fieldStyle}>*/}
-      {/*  <NoHeaderModal />*/}
-      {/*</div>*/}
-      {/*<h2>Without indentation</h2>*/}
-      {/*<div style={fieldStyle}>*/}
-      {/*  <WithoutIndentationModal />*/}
-      {/*</div>*/}
-      {/*<h2>Modal with max-width</h2>*/}
-      {/*<div style={fieldStyle}>*/}
-      {/*  <NoFullScreenModal />*/}
-      {/*</div>*/}
-      {/*<h2>No header and no close</h2>*/}
-      {/*<div style={fieldStyle}>*/}
-      {/*  <p>*/}
-      {/*    In order to add a button in the content to close the modal you need to*/}
-      {/*    provide a React component with a property named <code>'onClose'</code>*/}
-      {/*  </p>*/}
-      {/*  <NoHeaderNoCloseButtonModal />*/}
-      {/*</div>*/}
-      {/*<h2>Mobile fit content Modal</h2>*/}
-      {/*<div style={fieldStyle}>*/}
-      {/*  <MobileFitContentModal />*/}
-      {/*</div>*/}
-      {/*<h2>Modal with scrollable children</h2>*/}
-      {/*<div style={fieldStyle}>*/}
-      {/*  <ScrollableChildrenModal />*/}
-      {/*</div>*/}
-      {/*<h2>Modal with url state</h2>*/}
-      {/*<div style={fieldStyle}>*/}
-      {/*  <p>*/}
-      {/*    Given a defined hash it will look for url changes in order to open /*/}
-      {/*    close himself*/}
-      {/*  </p>*/}
-      {/*  <WithUrlStateModal />*/}
-      {/*</div>*/}
-      {/*<h2>Contentless Modal</h2>*/}
-      {/*<div style={fieldStyle}>*/}
-      {/*  <ContentlessModal />*/}
-      {/*</div>*/}
-      {/*<h2>Size Modal</h2>*/}
-      {/*<div style={fieldStyle}>*/}
-      {/*  <SizeModal />*/}
-      {/*</div>*/}
     </div>
   </div>
 )

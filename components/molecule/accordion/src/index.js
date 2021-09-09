2dc382693e88806df6106b3ad96f45329701ca1b
Nacho Torrella <ignacio.torrella@schibsted.com>
2021-09-09 12:03:37 +0200

    fix(components/molecule/accordion): fix default opened tabs state

--- components/molecule/accordion/src/index.js
+++ components/molecule/accordion/src/index.js
@@ -15,7 +15,7 @@ const MoleculeAccordion = ({
     defaultOpenedTabs.includes(index)
   )
 
-  const [openTabs, setOpenTabs] = useState(initialOpenTabs)
+  const [openTabs, setOpenTabs] = useState([])
 
   const _handleOnToggle = index => event => {
     let newOpenTabs = []
@@ -31,17 +31,21 @@ const MoleculeAccordion = ({
 
   return (
     <div className={BASE_CLASS}>
-      {children.map((child, index) => (
-        <Tab
-          isOpen={!!openTabs[index]}
-          key={index}
-          onToggle={_handleOnToggle(index)}
-          title={child.props.label}
-          {...tabProps}
-        >
-          {child.props.children}
-        </Tab>
-      ))}
+      {children.map((child, index) => {
+        const isDefaultOpenedTab = initialOpenTabs[index]
+        const isCurrentOpenedTab = openTabs[index]
+        return (
+          <Tab
+            isOpen={isDefaultOpenedTab || isCurrentOpenedTab}
+            key={index}
+            onToggle={_handleOnToggle(index)}
+            title={child.props.label}
+            {...tabProps}
+          >
+            {child.props.children}
+          </Tab>
+        )
+      })}
     </div>
   )
 }

9270627fb46cd76be39144605a626d166900ebe9
“andresin87” <andres.lucas@adevinta.com>
2021-09-27 13:32:20 +0200

    feat(components/molecule/autosuggest): remove withOpenToogle HoC

---
+++ components/molecule/autosuggest/src/config.js
@@ -0,0 +1,24 @@
+export const BASE_CLASS = `sui-MoleculeAutosuggest`
+export const CLASS_FOCUS = `${BASE_CLASS}--focus`
+export const CLASS_DISABLED = `${BASE_CLASS}--disabled`
+
+export const AUTOSUGGEST_STATES = {
+  ERROR: 'error',
+  SUCCESS: 'success',
+  ALERT: 'alert'
+}
+
+export const CLOSE_KEYS_LIST = ['Escape']
+export const SELECT_KEYS_LIST = [' ', 'Enter']
+
+export const getIsTypeableKey = key => {
+  const keysEdit = [
+    'Backspace',
+    'Enter',
+    'Meta',
+    'Shift',
+    'ArrowLeft',
+    'ArrowRight'
+  ]
+  return key.length === 1 || keysEdit.includes(key)
+}

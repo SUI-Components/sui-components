7c20d78ef74649c4df61f4a0c38a7d6861fd7712
“andresin87” <andres.lucas@adevinta.com>
2021-09-28 17:41:53 +0200

    feat(components/atom/input): remove HoC

--- components/atom/input/src/Input/index.js
+++ components/atom/input/src/Input/index.js
@@ -1,7 +1,62 @@
-import withAddons from './HOC/withAddons'
-import withIcons from './HOC/withIcons'
-import withButton from './HOC/withButton'
+import {forwardRef} from 'react'
+import PropTypes from 'prop-types'
+
+import InputButton from './Wrappers/Button/InputButton'
+import InputAddons from './Wrappers/Addons/InputAddons'
+import InputIcons from './Wrappers/Icons/InputIcons'
+
 import Input, {inputStates, inputSizes} from './Component'
 
-export default withButton(withAddons(withIcons(Input)))
-export {inputSizes, inputStates}
+const BaseInput = forwardRef(
+  (
+    {
+      button,
+      leftAddon,
+      rightAddon,
+      leftIcon,
+      rightIcon,
+      children,
+      onClickLeftIcon,
+      onClickRightIcon,
+      ...inputProps
+    },
+    forwardedRef
+  ) => {
+    return (
+      <InputButton button={button}>
+        <InputAddons leftAddon={leftAddon} rightAddon={rightAddon}>
+          <InputIcons
+            leftIcon={leftIcon}
+            rightIcon={rightIcon}
+            onClickLeftIcon={onClickLeftIcon}
+            onClickRightIcon={onClickRightIcon}
+          >
+            <Input ref={forwardedRef} {...inputProps} />
+          </InputIcons>
+        </InputAddons>
+      </InputButton>
+    )
+  }
+)
+
+BaseInput.propTypes = {
+  /** button html element */
+  button: PropTypes.node,
+  /* inner react node element */
+  children: PropTypes.node,
+  /* Left addon component, text,... */
+  leftAddon: PropTypes.any,
+  /* Right addon component, text,... */
+  rightAddon: PropTypes.any,
+  /* Left icon component */
+  leftIcon: PropTypes.node,
+  /* Left icon component */
+  rightIcon: PropTypes.node,
+  /* Left icon click callback */
+  onClickLeftIcon: PropTypes.func,
+  /* Right icon click callback */
+  onClickRightIcon: PropTypes.func
+}
+
+export default BaseInput
+export {inputSizes, inputStates, BaseInput}

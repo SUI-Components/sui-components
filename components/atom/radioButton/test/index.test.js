9534d3a3e7989781436f5d0b0218652fd29b14bb
“andresin87” <andres.lucas@adevinta.com>
2021-09-30 12:30:18 +0200

    test(components/atom/radioButton): add classNames agrement test

--- components/atom/radioButton/test/index.test.js
+++ components/atom/radioButton/test/index.test.js
@@ -40,18 +40,4 @@ describe('atom/radioButton', () => {
     expect(container.innerHTML).to.be.a('string')
     expect(container.innerHTML).to.not.have.lengthOf(0)
   })
-
-  it.skip('example', () => {
-    // Example TO BE DELETED!!!!
-
-    // Given
-    // const props = {}
-
-    // When
-    // const {getByRole} = setup(props)
-
-    // Then
-    // expect(getByRole('button')).to.have.text('HOLA')
-    expect(true).to.be.eql(false)
-  })
 })

79d183c8197a4013a9bd42ee8d9391b0bf35073e
Andrés <andresin87@gmail.com>
2021-10-08 09:24:19 +0200

    Update index.js

--- components/molecule/accordion/src/index.js
+++ components/molecule/accordion/src/index.js
@@ -37,7 +37,6 @@ const MoleculeAccordion = ({
           key={index}
           onToggle={_handleOnToggle(index)}
           title={child.props.label}
-          customTitle={child.props.customTitle}
           {...tabProps}
         >
           {child.props.children}

060fad373d2bce377a1a03722ce6be30b63e10bb
Oriol Puig <oriol.puig90@google.com>
2021-10-11 16:43:33 +0200

    feat(components/atom/textarea): new resize prop available!

---
+++ components/atom/textarea/src/settings.js
@@ -0,0 +1,28 @@
+export const BASE_CLASS = 'sui-AtomTextarea'
+
+export const TEXTAREA_SIZES = {
+  SHORT: 'short',
+  LONG: 'long'
+}
+
+export const DEFAULT_PROPS = {
+  size: TEXTAREA_SIZES.SHORT,
+  onChange: () => {},
+  onBlur: () => {}
+}
+
+export const TEXTAREA_RESIZES = {
+  AUTO: 'auto',
+  BOTH: 'both',
+  HORIZONTAL: 'horizontal',
+  INHERIT: 'inherit',
+  INITIAL: 'intiial',
+  NONE: 'none',
+  VERTICAL: 'vertical'
+}
+
+export const TEXTAREA_STATES = {
+  ERROR: 'error',
+  SUCCESS: 'success',
+  ALERT: 'alert'
+}

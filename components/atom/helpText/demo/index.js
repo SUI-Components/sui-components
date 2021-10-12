eafdd697e6925276ad9aac1db4371df035f66e4e
azahara <azaharamaga@gmail.com>
2021-10-12 18:44:30 +0200

    refactor(components/atom/helpText/demo): use SUI input instead of input from Docs

--- components/atom/helpText/demo/index.js
+++ components/atom/helpText/demo/index.js
@@ -1,14 +1,8 @@
 /* eslint-disable no-console */
 import AtomHelpText from '@s-ui/react-atom-help-text'
+import AtomInput from '@s-ui/react-atom-input'
 
-import {
-  H1,
-  H2,
-  Paragraph,
-  Article,
-  Code,
-  Input
-} from '@s-ui/documentation-library'
+import {H1, H2, Paragraph, Article, Code} from '@s-ui/documentation-library'
 
 import './index.scss'
 
@@ -25,7 +19,7 @@ const Demo = () => {
         <Paragraph>
           The prop <Code>text</Code> is used to set the text included in.
         </Paragraph>
-        <Input />
+        <AtomInput type="text" />
         <AtomHelpText text="hello help text" />
       </Article>
     </div>

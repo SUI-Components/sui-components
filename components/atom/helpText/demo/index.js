c15e744ab2f60525502db410fed33d1f5a760477
azahara <azaharamaga@gmail.com>
2021-10-12 19:02:06 +0200

    docs(components/atom/helpText/demo): add textarea example to helpText demo

--- components/atom/helpText/demo/index.js
+++ components/atom/helpText/demo/index.js
@@ -1,10 +1,30 @@
 /* eslint-disable no-console */
 import AtomHelpText from '@s-ui/react-atom-help-text'
 import AtomInput from '@s-ui/react-atom-input'
+import AtomTextarea from '@s-ui/react-atom-textarea'
 
 import {H1, H2, Paragraph, Article, Code} from '@s-ui/documentation-library'
 
 import './index.scss'
+
+const InputDemo = () => (
+  <Article>
+    <H2>Text</H2>
+    <Paragraph>
+      The prop <Code>text</Code> is used to set the text included in.
+    </Paragraph>
+    <AtomInput type="text" />
+    <AtomHelpText text="Add something here to help your users" />
+  </Article>
+)
+
+const TextareaDemo = () => (
+  <Article>
+    <H2>Text-area</H2>
+    <AtomTextarea placeholder="Write something cool here..." />
+    <AtomHelpText text="Additional information for your textarea input" />
+  </Article>
+)
 
 const Demo = () => {
   return (
@@ -14,14 +34,9 @@ const Demo = () => {
         Help Text is a feedback that the system gives users to make them clearly
         understand which information is required
       </Paragraph>
-      <Article>
-        <H2>Text</H2>
-        <Paragraph>
-          The prop <Code>text</Code> is used to set the text included in.
-        </Paragraph>
-        <AtomInput type="text" />
-        <AtomHelpText text="hello help text" />
-      </Article>
+      <InputDemo />
+      <br />
+      <TextareaDemo />
     </div>
   )
 }

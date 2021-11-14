afa986d1a950ff6884b031323aae488135ef946d
andresin87 <andresin87@gmail.com>
2021-11-14 17:02:46 +0100

    chore(Root): wip

--- scripts/github/exportCoverageFromMarkdown.js
+++ scripts/github/exportCoverageFromMarkdown.js
@@ -18,25 +18,29 @@ module.exports = async function exportCoverageFromMarkdown(
   {github, context, core, exec},
   markdownFilePath
 ) {
-  core.debug('Loading markdown coverage badges')
+  core.info('Loading markdown coverage badges')
   let markdownContent
   try {
     markdownContent = fs.readFileSync(markdownFilePath, {
       flag: 'r',
       encoding: 'utf8'
     })
+    core.info('Markdown loaded')
   } catch (error) {
     core.error(
       `Error ${error}, ${markdownFilePath} markdown file does not exist`
     )
   }
 
+  core.info('Loading badges')
   const matches = markdownContent.matchAll(regex.shield)
 
+  matches.length && core.info('Badges loaded')
   for (const match of matches) {
     const {alt, filename, optionalPart} = match.groups
     const [type, pct] = (filename.match(regex.pct) || [''])[0].split('-')
     if (type) {
+      core.info(`coverage_${type}_master: ${parseFloat(pct)}`)
       core.setOutput(`coverage_${type}_master`, parseFloat(pct))
     }
   }

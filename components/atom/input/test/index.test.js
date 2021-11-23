4e6271af614fa2ebd94ed5e3e806eabc855b8dfe
andresin87 <andresin87@gmail.com>
2021-11-23 14:44:28 +0100

    test(components/atom/input): add tests

--- components/atom/input/test/index.test.js
+++ components/atom/input/test/index.test.js
@@ -88,6 +88,102 @@ describe(json.name, () => {
     })
   })
 
+  describe(`${Component.displayName} ${pkg.inputTypes.MASK}`, () => {
+    it('should render without crashing', () => {
+      // Given
+      const props = {
+        type: pkg.inputTypes.MASK
+      }
+
+      // When
+      const component = <Component {...props} />
+
+      // Then
+      const div = document.createElement('div')
+      ReactDOM.render(component, div)
+      ReactDOM.unmountComponentAtNode(div)
+    })
+
+    it('should NOT render null', () => {
+      // Given
+      const props = {
+        type: pkg.inputTypes.MASK
+      }
+
+      // When
+      const {container} = setup(props)
+
+      // Then
+      expect(container.innerHTML).to.be.a('string')
+      expect(container.innerHTML).to.not.have.lengthOf(0)
+    })
+
+    it('should NOT extend classNames', () => {
+      // Given
+      const props = {
+        className: 'extended-classNames',
+        type: pkg.inputTypes.MASK
+      }
+      const findSentence = str => string =>
+        string.match(new RegExp(`S*${str}S*`))
+
+      // When
+      const {container} = setup(props)
+      const findClassName = findSentence(props.className)
+
+      // Then
+      expect(findClassName(container.innerHTML)).to.be.null
+    })
+  })
+
+  describe(`${Component.displayName} ${pkg.inputTypes.SUI_PASSWORD}`, () => {
+    it('should render without crashing', () => {
+      // Given
+      const props = {
+        type: pkg.inputTypes.SUI_PASSWORD
+      }
+
+      // When
+      const component = <Component {...props} />
+
+      // Then
+      const div = document.createElement('div')
+      ReactDOM.render(component, div)
+      ReactDOM.unmountComponentAtNode(div)
+    })
+
+    it('should NOT render null', () => {
+      // Given
+      const props = {
+        type: pkg.inputTypes.SUI_PASSWORD
+      }
+
+      // When
+      const {container} = setup(props)
+
+      // Then
+      expect(container.innerHTML).to.be.a('string')
+      expect(container.innerHTML).to.not.have.lengthOf(0)
+    })
+
+    it('should NOT extend classNames', () => {
+      // Given
+      const props = {
+        className: 'extended-classNames',
+        type: pkg.inputTypes.MASK
+      }
+      const findSentence = str => string =>
+        string.match(new RegExp(`S*${str}S*`))
+
+      // When
+      const {container} = setup(props)
+      const findClassName = findSentence(props.className)
+
+      // Then
+      expect(findClassName(container.innerHTML)).to.be.null
+    })
+  })
+
   describe('inputTypes', () => {
     it('value must be an object enum', () => {
       // Given

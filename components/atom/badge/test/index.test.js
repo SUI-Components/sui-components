8e784c82efc3bdab7133ab6a42cfe8ab64d715fa
andresin87 <andresin87@gmail.com>
2021-11-23 10:05:11 +0100

    test(components/atom/badge): add tests

--- components/atom/badge/test/index.test.js
+++ components/atom/badge/test/index.test.js
@@ -10,51 +10,202 @@ import ReactDOM from 'react-dom'
 import chai, {expect} from 'chai'
 import chaiDOM from 'chai-dom'
 
+import * as pkg from '../src'
+
+import json from '../package.json'
+
 chai.use(chaiDOM)
 
-describe('atom/badge', () => {
-  const Component = AtomBadge
+describe(json.name, () => {
+  const {default: Component} = pkg
   const setup = setupEnvironment(Component)
 
-  it('should render without crashing', () => {
+  it('library should include defined exported elements', () => {
     // Given
-    const props = {
-      label: 'label'
-    }
+    const library = pkg
+    const libraryExportedMembers = [
+      'atomBadgeDesigns',
+      'atomBadgeTypes',
+      'atomBadgeSizes',
+      'default'
+    ]
 
     // When
-    const component = <Component {...props} />
+    const {
+      atomBadgeDesigns,
+      atomBadgeTypes,
+      atomBadgeSizes,
+      default: AtomBadge,
+      ...others
+    } = library
 
     // Then
-    const div = document.createElement('div')
-    ReactDOM.render(component, div)
-    ReactDOM.unmountComponentAtNode(div)
+    expect(Object.keys(library).length).to.equal(libraryExportedMembers.length)
+    expect(Object.keys(library)).to.have.members(libraryExportedMembers)
+    expect(Object.keys(others).length).to.equal(0)
   })
 
-  it('should NOT render null', () => {
-    // Given
-    const props = {
-      label: 'label'
-    }
+  describe(Component.displayName, () => {
+    it('should render without crashing', () => {
+      // Given
+      const props = {
+        label: 'label'
+      }
 
-    // When
-    const {container} = setup(props)
+      // When
+      const component = <Component {...props} />
 
-    // Then
-    expect(container.innerHTML).to.be.a('string')
-    expect(container.innerHTML).to.not.have.lengthOf(0)
+      // Then
+      const div = document.createElement('div')
+      ReactDOM.render(component, div)
+      ReactDOM.unmountComponentAtNode(div)
+    })
+
+    it('should NOT render null', () => {
+      // Given
+      const props = {
+        label: 'label'
+      }
+
+      // When
+      const {container} = setup(props)
+
+      // Then
+      expect(container.innerHTML).to.be.a('string')
+      expect(container.innerHTML).to.not.have.lengthOf(0)
+    })
+
+    it('should NOT extend classNames', () => {
+      // Given
+      const props = {className: 'extended-classNames', label: 'label'}
+      const findSentence = str => string =>
+        string.match(new RegExp(`S*${str}S*`))
+
+      // When
+      const {container} = setup(props)
+      const findClassName = findSentence(props.className)
+
+      // Then
+      expect(findClassName(container.innerHTML)).to.be.null
+    })
   })
 
-  it('should NOT extend classNames', () => {
-    // Given
-    const props = {className: 'extended-classNames', label: 'label'}
-    const findSentence = str => string => string.match(new RegExp(`S*${str}S*`))
+  describe('atomBadgeDesigns', () => {
+    it('value must be an object enum', () => {
+      // Given
+      const library = pkg
 
-    // When
-    const {container} = setup(props)
-    const findClassName = findSentence(props.className)
+      // When
+      const {atomBadgeDesigns: actual} = library
 
-    // Then
-    expect(findClassName(container.innerHTML)).to.be.null
+      // Then
+      expect(actual).to.be.an('object')
+    })
+
+    it('value must be a defined string-key pair filled', () => {
+      // Given
+      const library = pkg
+      const expected = {
+        SOLID: 'solid',
+        SOFT: 'soft'
+      }
+
+      // When
+      const {atomBadgeDesigns: actual} = library
+      const {SOLID, SOFT, ...others} = actual
+
+      // Then
+      expect(Object.keys(others).length).to.equal(0)
+      expect(Object.keys(actual)).to.have.members(Object.keys(expected))
+      Object.entries(expected).forEach(([expectedKey, expectedValue]) => {
+        expect(Object.keys(actual).includes(expectedKey)).to.be.true
+        expect(actual[expectedKey]).to.equal(expectedValue)
+      })
+    })
+  })
+
+  describe('atomBadgeTypes', () => {
+    it('value must be an object enum', () => {
+      // Given
+      const library = pkg
+
+      // When
+      const {atomBadgeTypes: actual} = library
+
+      // Then
+      expect(actual).to.be.an('object')
+    })
+
+    it('value must be a defined string-key pair filled', () => {
+      // Given
+      const library = pkg
+      const expected = {
+        SUCCESS: 'success',
+        ERROR: 'error',
+        INFO: 'info',
+        ALERT: 'alert',
+        NEW: 'new',
+        NEUTRAL: 'neutral',
+        PRIMARY: 'primary',
+        SECONDARY: 'secondary'
+      }
+
+      // When
+      const {atomBadgeTypes: actual} = library
+      const {
+        SUCCESS,
+        ERROR,
+        INFO,
+        ALERT,
+        NEW,
+        NEUTRAL,
+        PRIMARY,
+        SECONDARY,
+        ...others
+      } = actual
+
+      // Then
+      expect(Object.keys(others).length).to.equal(0)
+      expect(Object.keys(actual)).to.have.members(Object.keys(expected))
+      Object.entries(expected).forEach(([expectedKey, expectedValue]) => {
+        expect(Object.keys(actual).includes(expectedKey)).to.be.true
+        expect(actual[expectedKey]).to.equal(expectedValue)
+      })
+    })
+  })
+
+  describe('atomBadgeSizes', () => {
+    it('value must be an object enum', () => {
+      // Given
+      const library = pkg
+
+      // When
+      const {atomBadgeSizes: actual} = library
+
+      // Then
+      expect(actual).to.be.an('object')
+    })
+
+    it('value must be a defined string-key pair filled', () => {
+      // Given
+      const library = pkg
+      const expected = {
+        LARGE: 'large',
+        MEDIUM: 'medium',
+        SMALL: 'small'
+      }
+
+      // When
+      const {atomBadgeSizes: actual} = library
+      const {LARGE, MEDIUM, SMALL, ...others} = actual
+
+      // Then
+      expect(Object.keys(others).length).to.equal(0)
+      expect(Object.keys(actual)).to.have.members(Object.keys(expected))
+      Object.entries(expected).forEach(([expectedKey, expectedValue]) => {
+        expect(Object.keys(actual).includes(expectedKey)).to.be.true
+        expect(actual[expectedKey]).to.equal(expectedValue)
+      })
+    })
   })
 })

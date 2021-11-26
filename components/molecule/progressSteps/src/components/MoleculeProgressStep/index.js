c01c6862ab232ca620d8ee69eec244743c7a8ccf
andresin87 <andresin87@gmail.com>
2021-11-26 18:09:05 +0100

    feat(components/molecule/progressSteps): wip

--- components/molecule/progressSteps/src/components/MoleculeProgressStep/index.js
+++ components/molecule/progressSteps/src/components/MoleculeProgressStep/index.js
@@ -1,37 +1,16 @@
+import {useMemo} from 'react'
 import cx from 'classnames'
 import PropTypes from 'prop-types'
 
-const BASE_CLASS = `sui-MoleculeProgressSteps`
-
-const CLASS_STEPS = `${BASE_CLASS}-path`
-
-const CLASS_BAR = `${CLASS_STEPS}-bar`
-const CLASS_STEP = `${CLASS_STEPS}-step`
-
-const CLASS_STEP_NUMBER = `${CLASS_STEP}-number`
-const CLASS_STEP_ICON = `${CLASS_STEP}-icon`
-
-const CLASS_STEP_DESCRIPTION = `${CLASS_STEP}-description`
-
-/* status */
-const CLASS_BAR_VISITED = `${CLASS_BAR}--visited`
-const CLASS_STEP_ACTIVE = `${CLASS_STEP}--active`
-const CLASS_STEP_VISITED = `${CLASS_STEP}--visited`
-
-const STATUSES = {
-  VISITED: 'VISITED',
-  NORMAL: 'NORMAL',
-  ACTIVE: 'ACTIVE'
-}
-
-const isVisited = status => status === STATUSES.VISITED
-const isActive = status => status === STATUSES.ACTIVE
-
-const getStatusClass = status => {
-  if (isVisited(status)) return [CLASS_STEP_VISITED, CLASS_BAR_VISITED]
-  if (isActive(status)) return [CLASS_STEP_ACTIVE, '']
-  return ['', '']
-}
+import {
+  CLASS_BAR,
+  CLASS_STEP,
+  CLASS_STEP_NUMBER,
+  CLASS_STEP_ICON,
+  CLASS_STEP_DESCRIPTION,
+  STATUSES,
+  getStatusClass
+} from './config'
 
 const MoleculeProgressStep = ({
   status,
@@ -39,10 +18,22 @@ const MoleculeProgressStep = ({
   label,
   numStep,
   lastStep,
-  compressed
+  compressed,
+  isProgressBarAutoSizedLength = true,
+  progressBarJustifyContent
 }) => {
-  const [CLASS_STEP_STATUS, CLASS_BAR_STATUS] = getStatusClass(status)
-  const bar = <hr className={cx(CLASS_BAR, CLASS_BAR_STATUS)} />
+  const [CLASS_STEP_STATUS, CLASS_BAR_STATUS] = useMemo(
+    getStatusClass.bind(this, status),
+    [status]
+  )
+  const bar = (
+    <hr
+      className={cx(CLASS_BAR, CLASS_BAR_STATUS, {
+        [`${CLASS_BAR}--grow`]: isProgressBarAutoSizedLength,
+        [`${CLASS_BAR}--justifyContent-${progressBarJustifyContent}`]: progressBarJustifyContent
+      })}
+    />
+  )
   return (
     <>
       {!compressed && (

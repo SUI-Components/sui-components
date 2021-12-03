b791f102dcd271f6a00279f104ac68c70483de07
andresin87 <andresin87@gmail.com>
2021-12-03 15:34:15 +0100

    test(components/molecule/quickAction): add tests

---
+++ components/molecule/quickAction/src/config.js
@@ -0,0 +1,9 @@
+export const CLASS = 'sui-MoleculeQuickAction'
+
+export const SIZES = {
+  SMALL: 'small',
+  MEDIUM: 'medium',
+  LARGE: 'large'
+}
+
+export const getClassName = variant => `${CLASS}--${variant}`

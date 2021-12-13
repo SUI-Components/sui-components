4daa37da9df4edb3b7a0640f296f7cd66054f209
andresin87 <andresin87@gmail.com>
2021-12-13 15:16:05 +0100

    test(components/molecule/dropdownList): refactor

--- components/molecule/dropdownList/test/index.test.js
+++ components/molecule/dropdownList/test/index.test.js
@@ -10,168 +10,288 @@ import ReactDOM from 'react-dom'
 
 import chai, {expect} from 'chai'
 import chaiDOM from 'chai-dom'
+import MoleculeDropdownOption from '@s-ui/react-molecule-dropdown-option'
+
+import * as pkg from '../src'
+
+import json from '../package.json'
 
 chai.use(chaiDOM)
 
-describe('molecule/dropdownList', () => {
-  const Component = MoleculeDropdownList
+describe(json.name, () => {
+  const {default: Component} = pkg
   const setup = setupEnvironment(Component)
-  const defaultProps = {
-    children: (
-      <>
-        {Array(5)
-          .fill()
-          .map((value, index) => (
-            <MoleculeDropdownOption value={index.toString()} key={index}>
-              {index}
-            </MoleculeDropdownOption>
-          ))}
-      </>
-    )
-  }
 
-  it('should render without crashing', () => {
+  it('library should include defined exported elements', () => {
     // Given
-    const props = {}
+    const library = pkg
+    const libraryExportedMembers = [
+      'moleculeDropdownListDesigns',
+      'moleculeDropdownListSizes',
+      'default'
+    ]
 
     // When
-    const component = <Component {...props} />
+    const {
+      moleculeDropdownListDesigns,
+      moleculeDropdownListSizes,
+      default: MoleculeDropDownList,
+      ...others
+    } = library
 
     // Then
-    const div = document.createElement('div')
-    ReactDOM.render(component, div)
-    ReactDOM.unmountComponentAtNode(div)
+    expect(Object.keys(library).length).to.equal(libraryExportedMembers.length)
+    expect(Object.keys(library)).to.have.members(libraryExportedMembers)
+    expect(Object.keys(others).length).to.equal(0)
   })
 
-  it('should NOT render null', () => {
-    // Given
-    const props = {}
+  describe(Component.displayName, () => {
+    it('should render without crashing', () => {
+      // Given
+      const props = {}
 
-    // When
-    const {container} = setup(props)
+      // When
+      const component = <Component {...props} />
 
-    // Then
-    expect(container.innerHTML).to.be.a('string')
-    expect(container.innerHTML).to.not.have.lengthOf(0)
-  })
+      // Then
+      const div = document.createElement('div')
+      ReactDOM.render(component, div)
+      ReactDOM.unmountComponentAtNode(div)
+    })
 
-  it('should NOT extend classNames', () => {
-    // Given
-    const props = {
-      className: 'extended-classNames'
-    }
-    const findSentence = str => string => string.match(new RegExp(`S*${str}S*`))
+    it('should NOT render null', () => {
+      // Given
+      const props = {}
 
-    // When
-    const {container} = setup(props)
-    const findClassName = findSentence(props.className)
+      // When
+      const {container} = setup(props)
 
-    // Then
-    expect(findClassName(container.innerHTML)).to.be.null
-  })
+      // Then
+      expect(container.innerHTML).to.be.a('string')
+      expect(container.innerHTML).to.not.have.lengthOf(0)
+    })
 
-  it('should forward a given ref to the `ul` element', () => {
-    // Given
-    const props = {}
-    const ref = createRef()
+    it('should NOT extend classNames', () => {
+      // Given
+      const props = {
+        className: 'extended-classNames'
+      }
+      const findSentence = str => string =>
+        string.match(new RegExp(`S*${str}S*`))
 
-    // When
-    const component = <Component {...props} ref={ref} />
-    const div = document.createElement('div')
-    ReactDOM.render(component, div)
+      // When
+      const {container} = setup(props)
+      const findClassName = findSentence(props.className)
 
-    // Then
-    expect(ref.current).to.not.equal(undefined)
-    expect(ref.current.nodeName).to.equal('UL')
-  })
+      // Then
+      expect(findClassName(container.innerHTML)).to.be.null
+    })
 
-  describe('props', () => {
-    describe('visible', () => {
-      it('should render the children if visible is not defined', async () => {
-        // Given
-        const props = {}
+    it('should forward a given ref to the `ul` element', () => {
+      // Given
+      const props = {}
+      const ref = createRef()
 
-        // When
-        const {container, getByText} = setup({
-          ...defaultProps,
-          ...props
+      // When
+      const component = <Component {...props} ref={ref} />
+      const div = document.createElement('div')
+      ReactDOM.render(component, div)
+
+      // Then
+      expect(ref.current).to.not.equal(undefined)
+      expect(ref.current.nodeName).to.equal('UL')
+    })
+
+    it("element role must be 'listbox' not visible by default", () => {
+      // Given
+      const props = {
+        children: (
+          <>
+            {Array(5)
+              .fill()
+              .map((value, index) => (
+                <MoleculeDropdownOption value={index.toString()} key={index}>
+                  {index}
+                </MoleculeDropdownOption>
+              ))}
+          </>
+        )
+      }
+
+      // When
+      const {getByRole} = setup(props)
+
+      // Then
+      expect(() => getByRole('listbox', {hidden: true})).to.not.throw()
+    })
+
+    it("element role must be 'listbox' visible when visible prop is true", () => {
+      // Given
+      const props = {
+        children: (
+          <>
+            {Array(5)
+              .fill()
+              .map((value, index) => (
+                <MoleculeDropdownOption value={index.toString()} key={index}>
+                  {index}
+                </MoleculeDropdownOption>
+              ))}
+          </>
+        ),
+        visible: true
+      }
+
+      // When
+      const {getByRole} = setup(props)
+
+      // Then
+      expect(() => getByRole('listbox')).to.not.throw()
+    })
+
+    describe('props', () => {
+      describe('visible', () => {
+        it('should not render the children if visible is not defined', async () => {
+          // Given
+          const props = {
+            children: (
+              <>
+                {Array(5)
+                  .fill()
+                  .map((value, index) => (
+                    <MoleculeDropdownOption
+                      value={index.toString()}
+                      key={index}
+                    >
+                      {index}
+                    </MoleculeDropdownOption>
+                  ))}
+              </>
+            )
+          }
+
+          // When
+          const {container, getByText} = setup(...props)
+
+          // Then
+          expect(container).to.be.not.undefined
+          expect(() => getByText('1', {visible: true})).to.throw()
         })
 
-        // Then
-        const element = getByText('1')
-        expect(container).to.be.not.undefined
-        expect(element).to.be.not.undefined
-      })
+        it('should render the children if it is visible', async () => {
+          // Given
+          const props = {
+            visible: true,
+            children: (
+              <>
+                {Array(5)
+                  .fill()
+                  .map((value, index) => (
+                    <MoleculeDropdownOption
+                      value={index.toString()}
+                      key={index}
+                    >
+                      {index}
+                    </MoleculeDropdownOption>
+                  ))}
+              </>
+            )
+          }
 
-      it('should render the children if it is visible', async () => {
-        // Given
-        const props = {
-          visible: true
-        }
+          // When
+          const {container, getByText} = setup(props)
 
-        // When
-        const {container, getByText} = setup({
-          ...defaultProps,
-          ...props
+          // Then
+          const element = getByText('1')
+          expect(container).to.be.not.undefined
+          expect(element).to.be.not.undefined
         })
 
-        // Then
-        const element = getByText('1')
-        expect(container).to.be.not.undefined
-        expect(element).to.be.not.undefined
-      })
+        it('should not render the children if it is not visible', async () => {
+          // Given
+          const props = {
+            visible: false,
+            children: (
+              <>
+                {Array(5)
+                  .fill()
+                  .map((value, index) => (
+                    <MoleculeDropdownOption
+                      value={index.toString()}
+                      key={index}
+                    >
+                      {index}
+                    </MoleculeDropdownOption>
+                  ))}
+              </>
+            )
+          }
 
-      it('should render the children if it is not visible', async () => {
-        // Given
-        const props = {
-          visible: false
-        }
+          // When
+          const {container} = setup(props)
 
-        // When
-        const {container} = setup({
-          ...defaultProps,
-          ...props
+          // Then
+          expect(container).to.be.not.undefined
+          expect(container.children.length).to.be.equal(1)
         })
 
-        // Then
-        expect(container).to.be.not.undefined
-        expect(container.children.length).to.be.equal(1)
-      })
+        it('should render the children if it is not visible but alwaysRender is enabled', async () => {
+          // Given
+          const props = {
+            children: (
+              <>
+                {Array(5)
+                  .fill()
+                  .map((value, index) => (
+                    <MoleculeDropdownOption
+                      value={index.toString()}
+                      key={index}
+                    >
+                      {index}
+                    </MoleculeDropdownOption>
+                  ))}
+              </>
+            ),
+            alwaysRender: true,
+            visible: false
+          }
 
-      it('should render the children if it is not visible but alwaysRender is enabled', async () => {
-        // Given
-        const props = {
-          alwaysRender: true,
-          visible: false
-        }
+          // When
+          const {container} = setup(props)
 
-        // When
-        const {container} = setup({
-          ...defaultProps,
-          ...props
+          // Then
+          expect(container).to.be.not.undefined
+          expect(container.children.length).to.be.equal(1)
         })
 
-        // Then
-        expect(container).to.be.not.undefined
-        expect(container.children.length).to.be.equal(1)
-      })
+        it('should NOT render the children if it is not visible and alwaysRender is disabled', async () => {
+          // Given
+          const props = {
+            children: (
+              <>
+                {Array(5)
+                  .fill()
+                  .map((value, index) => (
+                    <MoleculeDropdownOption
+                      value={index.toString()}
+                      key={index}
+                    >
+                      {index}
+                    </MoleculeDropdownOption>
+                  ))}
+              </>
+            ),
+            alwaysRender: false,
+            visible: false
+          }
 
-      it('should NOT render the children if it is not visible and alwaysRender is disabled', async () => {
-        // Given
-        const props = {
-          alwaysRender: false,
-          visible: false
-        }
+          // When
+          const {container} = setup(props)
 
-        // When
-        const {container} = setup({
-          ...defaultProps,
-          ...props
+          // Then
+          expect(container).to.be.not.undefined
+          expect(container.children.length).to.be.equal(0)
         })
-
-        // Then
-        expect(container).to.be.not.undefined
-        expect(container.children.length).to.be.equal(0)
       })
     })
   })

4ae63ea8de5b5f5745436c55ef1d6cb376d5d5bb
andresin87 <andresin87@gmail.com>
2021-12-14 10:13:14 +0100

    test(components/atom/checkbox): refactor

---
+++ components/atom/checkbox/src/config.js
@@ -0,0 +1,9 @@
+export const BASE_CLASS = 'sui-AtomCheckbox'
+
+export const CHECKBOX_STATUS = {
+  ERROR: 'error',
+  SUCCESS: 'success',
+  ALERT: 'alert'
+}
+
+export const CHECKBOX_SIZES = {SMALL: 'small', MEDIUM: 'medium'}

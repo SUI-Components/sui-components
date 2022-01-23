ab98aac21db4097613c4dfe2e4050cc4021731cd
andresin87 <andresin87@gmail.com>
2022-01-23 23:20:18 +0100

    refactor(components/molecule/dataCounter): add import file extensions

--- components/molecule/dataCounter/src/config.js
+++ components/molecule/dataCounter/src/config.js
@@ -1,5 +1,18 @@
+import {atomButtonSizes} from '@s-ui/react-atom-button'
+import {inputSizes} from '@s-ui/react-atom-input'
+
+export const BUTTON_TYPE = 'secondary'
+
+export const BASE_CLASS = `sui-MoleculeDataCounter`
+export const CLASS_INPUT_CONTAINER = `${BASE_CLASS}-container`
+
 export const ACTIONS = {
   CHANGE: 'change',
   LESS: 'less',
   MORE: 'more'
+}
+
+export const sizeConversor = {
+  [inputSizes.SMALL]: atomButtonSizes.SMALL,
+  [inputSizes.LARGE]: atomButtonSizes.LARGE
 }

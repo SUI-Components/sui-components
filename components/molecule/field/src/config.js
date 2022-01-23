ceb2c4ad8fb89d9c2b29797fe7225d0d45fd162b
andresin87 <andresin87@gmail.com>
2022-01-23 23:03:46 +0100

    refactor(components/molecule/field): add import file statements

---
+++ components/molecule/field/src/config.js
@@ -0,0 +1,8 @@
+export const BASE_CLASS = 'sui-MoleculeField'
+export const CLASS_INLINE = `${BASE_CLASS}--inline`
+export const CLASS_AUTO_HIDE = `${BASE_CLASS}--autohide`
+export const CLASS_FULLWIDTH = `${BASE_CLASS}--fullWidth`
+export const CLASS_INLINE_REVERSE = `${CLASS_INLINE}-reverse`
+export const CLASS_NODE_LABEL_CONTAINER = `${BASE_CLASS}-nodeLabelContainer`
+export const CLASS_INPUT_CONTAINER = `${BASE_CLASS}-inputContainer`
+export const CLASS_LABEL_CONTAINER = `${BASE_CLASS}-labelContainer`

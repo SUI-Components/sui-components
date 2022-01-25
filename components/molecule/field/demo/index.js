630a092bf85912c1128b873de1587bf88c8d1fef
andresin87 <andresin87@gmail.com>
2022-01-25 09:37:00 +0100

    refactor(components/molecule/field): remove playgotund

---
+++ components/molecule/field/demo/index.js
@@ -0,0 +1,23 @@
+import {H1, H2, Paragraph, Code, Anchor} from '@s-ui/documentation-library'
+
+const Demo = () => (
+  <div className="sui-StudioPreview">
+    <H1>Field</H1>
+    <H2>
+      With <Code>AtomInput</Code>
+    </H2>
+    <Paragraph>
+      Check the <Code>MoleculeInputField</Code>{' '}
+      <Anchor href="/workbench/molecule/inputField/demo">demo</Anchor>
+    </Paragraph>
+    <H2>
+      With <Code>AtomTextarea</Code>
+    </H2>
+    <Paragraph>
+      Check the <Code>MoleculeTextareaField</Code>{' '}
+      <Anchor href="/workbench/molecule/textareaField/demo">demo</Anchor>
+    </Paragraph>
+  </div>
+)
+
+export default Demo

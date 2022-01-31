f04dd2e227c9082834cb7a4e71a6d3b20115629b
Beatriz Iglesias Pino <beatriz.iglesias@adevinta.com>
2022-01-31 12:45:16 +0100

    fix(components/molecule/checkboxField): add disabled prop to atom checkbox

--- components/molecule/checkboxField/src/index.js
+++ components/molecule/checkboxField/src/index.js
@@ -37,7 +37,7 @@ const MoleculeCheckboxField = ({
         isAligned
         disabled={disabled}
       >
-        <AtomCheckbox id={id} {...props} />
+        <AtomCheckbox id={id} {...props} disabled={disabled} />
         {ToggleIcon && (
           <span
             className={`${BASE_CLASS}-toggleIcon`}

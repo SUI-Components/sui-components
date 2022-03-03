aaab0e3d0f6750a3d3a3f06a1a57c99746a0b264
andresin87 <andresin87@gmail.com>
2022-03-03 15:29:18 +0100

    feat(components/molecule/dropdownOption): add fwdRef and stateFull selected

---
+++ components/molecule/dropdownOption/demo/ArticleTextWrap.js
@@ -0,0 +1,58 @@
+import {useState} from 'react'
+import PropTypes from 'prop-types'
+
+import {
+  Article,
+  H2,
+  Paragraph,
+  RadioButtonGroup,
+  RadioButton,
+  Code
+} from '@s-ui/documentation-library'
+
+import MoleculeDropdownOption, {
+  moleculeDropdownOptionTextWrapStyles
+} from '../src/index.js'
+import LoremIpsum from './LoremIpsum.js'
+import {CLASS_DEMO_OPTION} from './config.js'
+
+const ArticleTextWrap = ({className}) => {
+  const [textWrap, setTextWrap] = useState()
+  return (
+    <Article className={className}>
+      <H2>Text Wrap</H2>
+      <Paragraph>
+        Each dropDownOption children content can be ellipsed controlled under
+        the <Code>textWrap</Code> (enum) prop
+      </Paragraph>
+      <RadioButtonGroup
+        value={textWrap}
+        onChange={(event, value) => setTextWrap(value)}
+      >
+        {[
+          ['undefined', undefined],
+          ...Object.entries(moleculeDropdownOptionTextWrapStyles)
+        ].map(([, textWrapStylesValue]) => (
+          <RadioButton
+            value={textWrapStylesValue}
+            label={`${textWrapStylesValue}`}
+            checked={textWrap === textWrapStylesValue}
+          />
+        ))}
+      </RadioButtonGroup>
+      <br />
+      <br />
+      <div className={CLASS_DEMO_OPTION}>
+        <MoleculeDropdownOption textWrap={textWrap}>
+          <LoremIpsum units="words" count={200} format="plain" />
+        </MoleculeDropdownOption>
+      </div>
+    </Article>
+  )
+}
+
+ArticleTextWrap.displayName = 'ArticleTextWrap'
+
+ArticleTextWrap.propTypes = {className: PropTypes.string}
+
+export default ArticleTextWrap

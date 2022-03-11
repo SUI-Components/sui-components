dae9fbf1fbaf41260e5b464508506a616e4e8c9d
andresin87 <andresin87@gmail.com>
2022-03-11 09:56:53 +0100

    docs(components/molecule/dropdownList/demo): remove imports

--- components/molecule/dropdownList/demo/index.js
+++ components/molecule/dropdownList/demo/index.js
@@ -1,28 +1,12 @@
-/* eslint-disable react/prop-types, no-unused-vars, no-console */
-import MoleculeDropdownOption from '@s-ui/react-molecule-dropdown-option'
-
 import {H1, Paragraph} from '@s-ui/documentation-library'
 
-import withStateSingle from './hoc/withStateSingle.js'
-import withStateMulti from './hoc/withStateMulti.js'
-
-import {countries} from './data/index.js'
-import {CLASS_DEMO_SECTION, CLASS_DEMO_LIST} from './config.js'
+import {CLASS_DEMO_SECTION} from './config.js'
 import ArticleDefault from './ArticleDefault.js'
 import ArticleDesign from './ArticleDesign.js'
 import ArticleSize from './ArticleSize.js'
 import ArticleCheckbox from './ArticleCheckbox.js'
 
 import './index.scss'
-
-const isOpen = true
-//
-// const MoleculeDropdownOptionListWithStateSingle = withStateSingle(
-//   MoleculeDropdownList
-// )
-// const MoleculeDropdownOptionListWithStateMulti = withStateMulti(
-//   MoleculeDropdownList
-// )
 
 const Demo = () => (
   <div className="sui-StudioPreview">

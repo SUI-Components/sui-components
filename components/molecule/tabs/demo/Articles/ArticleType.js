110103c297d9672babd82a8a34cc279583fedff5
andresin87 <andresin87@gmail.com>
2022-03-19 18:31:00 +0100

    fix(components/molecule/tabs): fix classes, mod demo and unnecesary propsTypes declaration and defau

---
+++ components/molecule/tabs/demo/Articles/ArticleType.js
@@ -0,0 +1,99 @@
+import {useState} from 'react'
+import PropTypes from 'prop-types'
+
+import {
+  Article,
+  H2,
+  H4,
+  Paragraph,
+  RadioButtonGroup,
+  RadioButton,
+  Grid,
+  Cell,
+  Label,
+  Code,
+  UnorderedList,
+  ListItem
+} from '@s-ui/documentation-library'
+
+import MoleculeTabs, {
+  MoleculeTab,
+  moleculeTabsTypes
+} from 'components/molecule/tabs/src/index.js'
+import Content from '../components/Content.js'
+import {CLASS_DEMO_CONTENT_TAB} from '../config.js'
+
+const ArticleDefault = ({className}) => {
+  const [type, setType] = useState(moleculeTabsTypes.VERTICAL)
+  return (
+    <Article className={className}>
+      <H2>Type</H2>
+      <Paragraph>
+        Using the <Code>type</Code> (enum) prop you can set it to any of the
+        predefined types. The default type values are exported on an enum called{' '}
+        <Code>moleculeTabsTypes</Code>:
+      </Paragraph>
+      <UnorderedList>
+        {Object.entries(moleculeTabsTypes).map(
+          ([moleculeTabsTypeKey, moleculeTabsTypeValue]) => (
+            <ListItem key={moleculeTabsTypeKey}>
+              <Code>moleculeTabsTypes.{moleculeTabsTypeKey}</Code>: "
+              {moleculeTabsTypeValue}"
+            </ListItem>
+          )
+        )}
+      </UnorderedList>
+      <Grid cols={1} gutter={[8, 8]}>
+        <Cell>
+          <Label>type</Label>
+        </Cell>
+        <Cell>
+          <RadioButtonGroup
+            value={type}
+            onChange={(event, value) => setType(value)}
+          >
+            {[
+              ['undefined', undefined],
+              ...Object.entries(moleculeTabsTypes)
+            ].map(([moleculeTabsTypeKey, moleculeTabsTypeValue]) => (
+              <RadioButton
+                key={`${moleculeTabsTypeKey}`}
+                label={`${moleculeTabsTypeKey}`}
+                value={moleculeTabsTypeValue}
+                checked={moleculeTabsTypeValue === type}
+              />
+            ))}
+          </RadioButtonGroup>
+        </Cell>
+        <Cell>
+          <MoleculeTabs type={type}>
+            {Array(5)
+              .fill(true)
+              .map((v, index) => (
+                <MoleculeTab
+                  key={index + 1}
+                  label={<H4 style={{padding: '0 8px'}}>Label {index + 1}</H4>}
+                  numTab={index + 1}
+                  active={index + 1 === 1}
+                >
+                  <Content
+                    title="Title"
+                    number={index + 1}
+                    className={CLASS_DEMO_CONTENT_TAB}
+                  />
+                </MoleculeTab>
+              ))}
+          </MoleculeTabs>
+        </Cell>
+      </Grid>
+    </Article>
+  )
+}
+
+ArticleDefault.displayName = 'ArticleDefault'
+
+ArticleDefault.propTypes = {
+  className: PropTypes.string
+}
+
+export default ArticleDefault

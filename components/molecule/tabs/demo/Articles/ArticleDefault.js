56488809574915b9673c60a581a4789b74da789e
andresin87 <andresin87@gmail.com>
2022-03-19 18:31:00 +0100

    fix(components/molecule/tabs): fix classes, mod demo and unnecesary propsTypes declaration and defau

---
+++ components/molecule/tabs/demo/Articles/ArticleDefault.js
@@ -0,0 +1,57 @@
+import PropTypes from 'prop-types'
+
+import {Article, H2, H4, Paragraph, Code} from '@s-ui/documentation-library'
+
+import MoleculeTabs, {MoleculeTab} from 'components/molecule/tabs/src/index.js'
+import Content from '../components/Content.js'
+import {CLASS_DEMO_CONTENT_TAB} from '../config.js'
+
+const ArticleType = ({className}) => {
+  return (
+    <Article className={className}>
+      <H2>Default</H2>
+      <Paragraph>
+        By default it is presented as an horizontal basic mode. The{' '}
+        <Code>MoleculeTabs</Code> element act as a wrapper of other different{' '}
+        <Code>MoleculeTab</Code> child elements. You can define wich one will be
+        the desired <Code>active</Code> (boolean) prop will be the predefined.
+        in case of defining more than 1, the active set tab will be the last
+        active defined. You can also use <Code>disable</Code> (boolean) prop to
+        make unreachable any of that tabs.
+      </Paragraph>
+      <MoleculeTabs>
+        {Array(5)
+          .fill(true)
+          .map((v, index) => (
+            <MoleculeTab
+              key={index + 1}
+              label={<H4 style={{padding: '0 8px'}}>Label {index + 1}</H4>}
+              numTab={index + 1}
+              active={index + 1 === 1}
+              disabled={index + 1 === 4}
+            >
+              <Content
+                title="Title"
+                number={index + 1}
+                className={CLASS_DEMO_CONTENT_TAB}
+                style={{
+                  borderTop: 0,
+                  borderLeft: '1px solid #e6e6e6',
+                  borderRight: '1px solid #e6e6e6',
+                  borderBottom: '1px solid #e6e6e6'
+                }}
+              />
+            </MoleculeTab>
+          ))}
+      </MoleculeTabs>
+    </Article>
+  )
+}
+
+ArticleType.displayName = 'ArticleType'
+
+ArticleType.propTypes = {
+  className: PropTypes.string
+}
+
+export default ArticleType
---
+++ components/molecule/tabs/demo/components/Content.js
@@ -0,0 +1,36 @@
+import {useMemo} from 'react'
+import PropTypes from 'prop-types'
+
+import {H3, H4, Article, Paragraph} from '@s-ui/documentation-library'
+
+import LoremIpsum from './LoremIpsum.js'
+
+const Content = ({className, title, number, style}) => {
+  const paragraphNumber = useMemo(() => Math.trunc(1 + Math.random() * 3), [])
+  return (
+    <Article className={className} style={style}>
+      <H3>{title}</H3>
+      <H4>Tab nº{number}</H4>
+      {Array(paragraphNumber)
+        .fill(true)
+        .map((v, index) => (
+          <Paragraph key={index} className="demo-content-paragraph">
+            <LoremIpsum
+              units="words"
+              count={Math.trunc(200 / paragraphNumber)}
+              format="plain"
+            />
+          </Paragraph>
+        ))}
+    </Article>
+  )
+}
+
+Content.propTypes = {
+  className: PropTypes.string,
+  title: PropTypes.string,
+  number: PropTypes.number,
+  style: PropTypes.object
+}
+
+export default Content

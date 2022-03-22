9a6fe66c95f00c52a0a535d74d818b9a38d39f4a
andresin87 <andresin87@gmail.com>
2022-03-22 11:30:32 +0100

    test(components/atom/upload): refactor

--- components/atom/upload/test/index.test.js
+++ components/atom/upload/test/index.test.js
@@ -10,51 +10,108 @@ import ReactDOM from 'react-dom'
 import chai, {expect} from 'chai'
 import chaiDOM from 'chai-dom'
 
+import * as pkg from '../src/index.js'
+
+import json from '../package.json'
+
 chai.use(chaiDOM)
 
-describe('atom/upload', () => {
-  const Component = AtomUpload
+describe(json.name, () => {
+  const {default: Component} = pkg
   const setup = setupEnvironment(Component)
 
-  it('should render without crashing', () => {
+  it('library should include defined exported elements', () => {
     // Given
-    const props = {
-      status: 'active'
-    }
+    const library = pkg
+    const libraryExportedMembers = ['uploadStatuses', 'default']
 
     // When
-    const component = <Component {...props} />
+    const {uploadStatuses, default: AtomUpload, ...others} = library
 
     // Then
-    const div = document.createElement('div')
-    ReactDOM.render(component, div)
-    ReactDOM.unmountComponentAtNode(div)
+    expect(Object.keys(library).length).to.equal(libraryExportedMembers.length)
+    expect(Object.keys(library)).to.have.members(libraryExportedMembers)
+    expect(Object.keys(others).length).to.equal(0)
   })
 
-  it('should render null', () => {
-    // Given
-    const props = {
-      status: 'active'
-    }
+  describe(Component.displayName, () => {
+    it('should render without crashing', () => {
+      // Given
+      const props = {
+        status: 'active'
+      }
 
-    // When
-    const {container} = setup(props)
+      // When
+      const component = <Component {...props} />
 
-    // Then
-    expect(container.innerHTML).to.be.a('string')
-    expect(container.innerHTML).to.have.lengthOf(0)
+      // Then
+      const div = document.createElement('div')
+      ReactDOM.render(component, div)
+      ReactDOM.unmountComponentAtNode(div)
+    })
+
+    it('should render null', () => {
+      // Given
+      const props = {
+        status: 'active'
+      }
+
+      // When
+      const {container} = setup(props)
+
+      // Then
+      expect(container.innerHTML).to.be.a('string')
+      expect(container.innerHTML).to.have.lengthOf(0)
+    })
+
+    it('should NOT extend classNames', () => {
+      // Given
+      const props = {className: 'extended-classNames'}
+      const findSentence = str => string =>
+        string.match(new RegExp(`S*${str}S*`))
+
+      // When
+      const {container} = setup(props)
+      const findClassName = findSentence(props.className)
+
+      // Then
+      expect(findClassName(container.innerHTML)).to.be.null
+    })
   })
 
-  it('should NOT extend classNames', () => {
-    // Given
-    const props = {className: 'extended-classNames'}
-    const findSentence = str => string => string.match(new RegExp(`S*${str}S*`))
+  describe('uploadStatuses', () => {
+    it('value must be an object enum', () => {
+      // Given
+      const library = pkg
 
-    // When
-    const {container} = setup(props)
-    const findClassName = findSentence(props.className)
+      // When
+      const {uploadStatuses: actual} = library
 
-    // Then
-    expect(findClassName(container.innerHTML)).to.be.null
+      // Then
+      expect(actual).to.be.an('object')
+    })
+
+    it('value must be a defined string-key pair filled', () => {
+      // Given
+      const library = pkg
+      const expected = {
+        ACTIVE: 'active',
+        UPLOAD: 'upload',
+        SUCCESS: 'success',
+        ERROR: 'error'
+      }
+
+      // When
+      const {uploadStatuses: actual} = library
+      const {ACTIVE, UPLOAD, SUCCESS, ERROR, ...others} = actual
+
+      // Then
+      expect(Object.keys(others).length).to.equal(0)
+      expect(Object.keys(actual)).to.have.members(Object.keys(expected))
+      Object.entries(expected).forEach(([expectedKey, expectedValue]) => {
+        expect(Object.keys(actual).includes(expectedKey)).to.be.true
+        expect(actual[expectedKey]).to.equal(expectedValue)
+      })
+    })
   })
 })

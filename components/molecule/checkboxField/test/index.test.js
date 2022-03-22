8209da76faabad2202b9436698564c3cc9293008
andresin87 <andresin87@gmail.com>
2022-03-22 16:34:54 +0100

    test(components/molecule/checkboxField): refactor

--- components/molecule/checkboxField/test/index.test.js
+++ components/molecule/checkboxField/test/index.test.js
@@ -10,116 +10,137 @@ import ReactDOM from 'react-dom'
 import chai, {expect} from 'chai'
 import chaiDOM from 'chai-dom'
 
+import * as pkg from '../src/index.js'
+
+import json from '../package.json'
+
 chai.use(chaiDOM)
 
-describe('molecule/checkboxField', () => {
-  const Component = MoleculeCheckboxField
+describe(json.name, () => {
+  const {default: Component} = pkg
   const setup = setupEnvironment(Component)
 
-  it('should render without crashing', () => {
+  it('library should include defined exported elements', () => {
     // Given
-    const props = {}
+    const library = pkg
+    const libraryExportedMembers = ['default']
 
     // When
-    const component = <Component {...props} />
+    const {default: MoleculeCheckboxField, ...others} = library
 
     // Then
-    const div = document.createElement('div')
-    ReactDOM.render(component, div)
-    ReactDOM.unmountComponentAtNode(div)
+    expect(Object.keys(library).length).to.equal(libraryExportedMembers.length)
+    expect(Object.keys(library)).to.have.members(libraryExportedMembers)
+    expect(Object.keys(others).length).to.equal(0)
   })
 
-  it('should NOT render null', () => {
-    // Given
-    const props = {}
+  describe(Component.displayName, () => {
+    it('should render without crashing', () => {
+      // Given
+      const props = {}
 
-    // When
-    const {container} = setup(props)
+      // When
+      const component = <Component {...props} />
 
-    // Then
-    expect(container.innerHTML).to.be.a('string')
-    expect(container.innerHTML).to.not.have.lengthOf(0)
-  })
+      // Then
+      const div = document.createElement('div')
+      ReactDOM.render(component, div)
+      ReactDOM.unmountComponentAtNode(div)
+    })
 
-  it('should NOT extend classNames', () => {
-    // Given
-    const props = {
-      className: 'extended-classNames'
-    }
-    const findSentence = str => string => string.match(new RegExp(`S*${str}S*`))
+    it('should NOT render null', () => {
+      // Given
+      const props = {}
 
-    // When
-    const {container} = setup(props)
-    const findClassName = findSentence(props.className)
+      // When
+      const {container} = setup(props)
 
-    // Then
-    expect(findClassName(container.innerHTML)).to.be.null
-  })
+      // Then
+      expect(container.innerHTML).to.be.a('string')
+      expect(container.innerHTML).to.not.have.lengthOf(0)
+    })
 
-  describe('props', () => {
-    describe('label & nodeLabel', () => {
-      it('should render the empty component if there is not label or nodeLabel props', async () => {
-        // Given
-        const props = {}
+    it('should NOT extend classNames', () => {
+      // Given
+      const props = {
+        className: 'extended-classNames'
+      }
+      const findSentence = str => string =>
+        string.match(new RegExp(`S*${str}S*`))
 
-        // When
-        const {container} = setup(props)
+      // When
+      const {container} = setup(props)
+      const findClassName = findSentence(props.className)
 
-        // Then
-        expect(container).to.be.not.undefined
-      })
+      // Then
+      expect(findClassName(container.innerHTML)).to.be.null
+    })
 
-      it('should render the component if there is label', async () => {
-        // Given
-        const props = {
-          label: 'label'
-        }
+    describe('props', () => {
+      describe('label & nodeLabel', () => {
+        it('should render the empty component if there is not label or nodeLabel props', async () => {
+          // Given
+          const props = {}
 
-        // When
-        const {container, getByText} = setup(props)
+          // When
+          const {container} = setup(props)
 
-        // Then
-        const labelElement = getByText(props.label)
-        expect(container).to.be.not.undefined
-        expect(labelElement).to.be.not.undefined
-        expect(labelElement.innerText).to.equal(props.label)
-        expect(labelElement.classList.contains('sui-AtomLabel')).to.be.true
-      })
-      it('should render the component if there is nodeLabel', async () => {
-        // Given
-        const text = 'nodeLabel'
-        const props = {
-          nodeLabel: <div className="testNodeLabel">{text}</div>
-        }
+          // Then
+          expect(container).to.be.not.undefined
+        })
 
-        // When
-        const {container, getByText} = setup(props)
+        it('should render the component if there is label', async () => {
+          // Given
+          const props = {
+            label: 'label'
+          }
 
-        // Then
-        const labelElement = getByText(text)
-        expect(container).to.be.not.undefined
-        expect(labelElement).to.be.not.undefined
-        expect(labelElement.innerText).to.equal(text)
-        expect(labelElement.classList.contains('testNodeLabel')).to.be.true
-      })
-      it('should render the component with label value if there is label and nodeLabel props', async () => {
-        // Given
-        const text = 'label'
-        const props = {
-          label: text,
-          nodeLabel: <div className="testNodeLabel">{text}</div>
-        }
+          // When
+          const {container, getByText} = setup(props)
 
-        // When
-        const {container, getByText} = setup(props)
+          // Then
+          const labelElement = getByText(props.label)
+          expect(container).to.be.not.undefined
+          expect(labelElement).to.be.not.undefined
+          expect(labelElement.innerText).to.equal(props.label)
+          expect(labelElement.classList.contains('sui-AtomLabel')).to.be.true
+        })
+        it('should render the component if there is nodeLabel', async () => {
+          // Given
+          const text = 'nodeLabel'
+          const props = {
+            nodeLabel: <div className="testNodeLabel">{text}</div>
+          }
 
-        // Then
-        const labelElement = getByText(text)
-        expect(container).to.be.not.undefined
-        expect(labelElement).to.be.not.undefined
-        expect(labelElement.innerText).to.equal(props.label)
-        expect(labelElement.classList.contains('sui-AtomLabel')).to.be.true
-        expect(labelElement.classList.contains('testNodeLabel')).to.be.false
+          // When
+          const {container, getByText} = setup(props)
+
+          // Then
+          const labelElement = getByText(text)
+          expect(container).to.be.not.undefined
+          expect(labelElement).to.be.not.undefined
+          expect(labelElement.innerText).to.equal(text)
+          expect(labelElement.classList.contains('testNodeLabel')).to.be.true
+        })
+        it('should render the component with label value if there is label and nodeLabel props', async () => {
+          // Given
+          const text = 'label'
+          const props = {
+            label: text,
+            nodeLabel: <div className="testNodeLabel">{text}</div>
+          }
+
+          // When
+          const {container, getByText} = setup(props)
+
+          // Then
+          const labelElement = getByText(text)
+          expect(container).to.be.not.undefined
+          expect(labelElement).to.be.not.undefined
+          expect(labelElement.innerText).to.equal(props.label)
+          expect(labelElement.classList.contains('sui-AtomLabel')).to.be.true
+          expect(labelElement.classList.contains('testNodeLabel')).to.be.false
+        })
       })
     })
   })

b2ecf8c24c57ba8b0960997ba2615e168aad2d29
andresin87 <andresin87@gmail.com>
2022-03-22 11:45:33 +0100

    test(components/molecule/accordion): refactor

--- components/molecule/accordion/test/index.test.js
+++ components/molecule/accordion/test/index.test.js
@@ -12,103 +12,124 @@ import chaiDOM from 'chai-dom'
 import sinon from 'sinon'
 import userEvents from '@testing-library/user-event'
 
+import * as pkg from '../src/index.js'
+
+import json from '../package.json'
+
 chai.use(chaiDOM)
 
-describe('molecule/accordion', () => {
-  const Component = MoleculeAccordion
+describe(json.name, () => {
+  const {default: Component} = pkg
   const setup = setupEnvironment(Component)
 
-  it('should render without crashing', () => {
+  it('library should include defined exported elements', () => {
     // Given
-    const props = {
-      children: [
-        <div key={0} label="label 1">
-          element 1
-        </div>,
-        <div key={1} label="label 2">
-          element 2
-        </div>
-      ],
-      icon: <svg />
-    }
+    const library = pkg
+    const libraryExportedMembers = ['default']
 
     // When
-    const component = <Component {...props} />
+    const {default: AtomAccordion, ...others} = library
 
     // Then
-    const div = document.createElement('div')
-    ReactDOM.render(component, div)
-    ReactDOM.unmountComponentAtNode(div)
+    expect(Object.keys(library).length).to.equal(libraryExportedMembers.length)
+    expect(Object.keys(library)).to.have.members(libraryExportedMembers)
+    expect(Object.keys(others).length).to.equal(0)
   })
 
-  it('should NOT render null', () => {
-    // Given
-    const props = {
-      children: [
-        <div key={0} label="label 1">
-          element 1
-        </div>,
-        <div key={1} label="label 2">
-          element 2
-        </div>
-      ],
-      icon: <svg />
-    }
+  describe(Component.displayName, () => {
+    it('should render without crashing', () => {
+      // Given
+      const props = {
+        children: [
+          <div key={0} label="label 1">
+            element 1
+          </div>,
+          <div key={1} label="label 2">
+            element 2
+          </div>
+        ],
+        icon: <svg />
+      }
 
-    // When
-    const {container} = setup(props)
+      // When
+      const component = <Component {...props} />
 
-    // Then
-    expect(container.innerHTML).to.be.a('string')
-    expect(container.innerHTML).to.not.have.lengthOf(0)
-  })
+      // Then
+      const div = document.createElement('div')
+      ReactDOM.render(component, div)
+      ReactDOM.unmountComponentAtNode(div)
+    })
 
-  it('should NOT extend classNames', () => {
-    // Given
-    const props = {
-      className: 'extended-classNames',
-      children: [
-        <div key={0} label="label 1">
-          element 1
-        </div>,
-        <div key={1} label="label 2">
-          element 2
-        </div>
-      ],
-      icon: <svg />
-    }
-    const findSentence = str => string => string.match(new RegExp(`S*${str}S*`))
+    it('should NOT render null', () => {
+      // Given
+      const props = {
+        children: [
+          <div key={0} label="label 1">
+            element 1
+          </div>,
+          <div key={1} label="label 2">
+            element 2
+          </div>
+        ],
+        icon: <svg />
+      }
 
-    // When
-    const {container} = setup(props)
-    const findClassName = findSentence(props.className)
+      // When
+      const {container} = setup(props)
 
-    // Then
-    expect(findClassName(container.innerHTML)).to.be.null
-  })
+      // Then
+      expect(container.innerHTML).to.be.a('string')
+      expect(container.innerHTML).to.not.have.lengthOf(0)
+    })
 
-  it('should trigger onToggleTab when tab is clicked', () => {
-    // Given
-    const spy = sinon.spy()
-    const props = {
-      children: [
-        <div key={0} label="label 1">
-          element 1
-        </div>,
-        <div key={1} label="label 2">
-          element 2
-        </div>
-      ],
-      icon: <svg />,
-      onToggleTab: spy
-    }
-    const {getByText} = setup(props)
+    it('should NOT extend classNames', () => {
+      // Given
+      const props = {
+        className: 'extended-classNames',
+        children: [
+          <div key={0} label="label 1">
+            element 1
+          </div>,
+          <div key={1} label="label 2">
+            element 2
+          </div>
+        ],
+        icon: <svg />
+      }
+      const findSentence = str => string =>
+        string.match(new RegExp(`S*${str}S*`))
 
-    // When
-    const tab = getByText('label 1')
-    userEvents.click(tab)
+      // When
+      const {container} = setup(props)
+      const findClassName = findSentence(props.className)
 
-    // Then
-    sinon.assert.called(spy)
+      // Then
+      expect(findClassName(container.innerHTML)).to.be.null
+    })
+
+    it('should trigger onToggleTab when tab is clicked', () => {
+      // Given
+      const spy = sinon.spy()
+      const props = {
+        children: [
+          <div key={0} label="label 1">
+            element 1
+          </div>,
+          <div key={1} label="label 2">
+            element 2
+          </div>
+        ],
+        icon: <svg />,
+        onToggleTab: spy
+      }
+      const {getByText} = setup(props)
+
+      // When
+      const tab = getByText('label 1')
+      userEvents.click(tab)
+
+      // Then
+      sinon.assert.called(spy)
+    })
   })
 })

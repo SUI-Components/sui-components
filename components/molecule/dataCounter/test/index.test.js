8e91fd2c8e3b905c14b873a5fcc67f993d20b31e
andresin87 <andresin87@gmail.com>
2022-03-22 17:05:18 +0100

    test(components/molecule/dataCounter): refactor

--- components/molecule/dataCounter/test/index.test.js
+++ components/molecule/dataCounter/test/index.test.js
@@ -10,63 +10,123 @@ import ReactDOM from 'react-dom'
 import chai, {expect} from 'chai'
 import chaiDOM from 'chai-dom'
 
+import * as pkg from '../src/index.js'
+
+import json from '../package.json'
+
 chai.use(chaiDOM)
 
-describe('molecule/dataCounter', () => {
-  const Component = MoleculeDataCounter
+describe(json.name, () => {
+  const {default: Component} = pkg
   const setup = setupEnvironment(Component)
 
-  it('should render without crashing', () => {
+  it('library should include defined exported elements', () => {
     // Given
-    const props = {
-      charsSize: 10,
-      label: 'label',
-      minValueHelpText: 'minValueHelpText',
-      minValueErrorText: 'minValueErrorText',
-      maxValueHelpText: 'maxValueHelpText',
-      maxValueErrorText: 'maxValueErrorText'
-    }
+    const library = pkg
+    const libraryExportedMembers = ['moleculeDataCounterSizes', 'default']
 
     // When
-    const component = <Component {...props} />
+    const {
+      moleculeDataCounterSizes,
+      default: MoleculeDataCounter,
+      ...others
+    } = library
 
     // Then
-    const div = document.createElement('div')
-    ReactDOM.render(component, div)
-    ReactDOM.unmountComponentAtNode(div)
+    expect(Object.keys(library).length).to.equal(libraryExportedMembers.length)
+    expect(Object.keys(library)).to.have.members(libraryExportedMembers)
+    expect(Object.keys(others).length).to.equal(0)
   })
 
-  it('should NOT render null', () => {
-    // Given
-    const props = {
-      charsSize: 10,
-      label: 'label',
-      minValueHelpText: 'minValueHelpText',
-      minValueErrorText: 'minValueErrorText',
-      maxValueHelpText: 'maxValueHelpText',
-      maxValueErrorText: 'maxValueErrorText'
-    }
+  describe(Component.displayName, () => {
+    it('should render without crashing', () => {
+      // Given
+      const props = {
+        charsSize: 10,
+        label: 'label',
+        minValueHelpText: 'minValueHelpText',
+        minValueErrorText: 'minValueErrorText',
+        maxValueHelpText: 'maxValueHelpText',
+        maxValueErrorText: 'maxValueErrorText'
+      }
 
-    // When
-    const {container} = setup(props)
+      // When
+      const component = <Component {...props} />
 
-    // Then
-    expect(container.innerHTML).to.be.a('string')
-    expect(container.innerHTML).to.not.have.lengthOf(0)
+      // Then
+      const div = document.createElement('div')
+      ReactDOM.render(component, div)
+      ReactDOM.unmountComponentAtNode(div)
+    })
+
+    it('should NOT render null', () => {
+      // Given
+      const props = {
+        charsSize: 10,
+        label: 'label',
+        minValueHelpText: 'minValueHelpText',
+        minValueErrorText: 'minValueErrorText',
+        maxValueHelpText: 'maxValueHelpText',
+        maxValueErrorText: 'maxValueErrorText'
+      }
+
+      // When
+      const {container} = setup(props)
+
+      // Then
+      expect(container.innerHTML).to.be.a('string')
+      expect(container.innerHTML).to.not.have.lengthOf(0)
+    })
+
+    it('should NOT extend classNames', () => {
+      // Given
+      const props = {
+        className: 'extended-classNames'
+      }
+      const findSentence = str => string =>
+        string.match(new RegExp(`S*${str}S*`))
+
+      // When
+      const {container} = setup(props)
+      const findClassName = findSentence(props.className)
+
+      // Then
+      expect(findClassName(container.innerHTML)).to.be.null
+    })
   })
 
-  it('should NOT extend classNames', () => {
-    // Given
-    const props = {
-      className: 'extended-classNames'
-    }
-    const findSentence = str => string => string.match(new RegExp(`S*${str}S*`))
+  describe('moleculeDataCounterSizes', () => {
+    it('value must be an object enum', () => {
+      // Given
+      const library = pkg
 
-    // When
-    const {container} = setup(props)
-    const findClassName = findSentence(props.className)
+      // When
+      const {moleculeDataCounterSizes: actual} = library
 
-    // Then
-    expect(findClassName(container.innerHTML)).to.be.null
+      // Then
+      expect(actual).to.be.an('object')
+    })
+
+    it('value must be a defined string-key pair filled', () => {
+      // Given
+      const library = pkg
+      const expected = {
+        SMALL: 's',
+        MEDIUM: 'm',
+        LARGE: 'l'
+      }
+
+      // When
+      const {moleculeDataCounterSizes: actual} = library
+      const {SMALL, MEDIUM, LARGE, ...others} = actual
+
+      // Then
+      expect(Object.keys(others).length).to.equal(0)
+      expect(Object.keys(actual)).to.have.members(Object.keys(expected))
+      Object.entries(expected).forEach(([expectedKey, expectedValue]) => {
+        expect(Object.keys(actual).includes(expectedKey)).to.be.true
+        expect(actual[expectedKey]).to.equal(expectedValue)
+      })
+    })
   })
 })

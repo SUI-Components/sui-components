be23b63094da754fc396863bbdd0f31b2dc042d7
andresin87 <andresin87@gmail.com>
2022-03-23 12:10:49 +0100

    test(components/organism/nestedCheckboxes): refactor

--- components/organism/nestedCheckboxes/test/index.test.js
+++ components/organism/nestedCheckboxes/test/index.test.js
@@ -10,102 +10,123 @@ import ReactDOM from 'react-dom'
 import chai, {expect} from 'chai'
 import chaiDOM from 'chai-dom'
 
+import * as pkg from '../src/index.js'
+
+import json from '../package.json'
+
 chai.use(chaiDOM)
 
-describe('organism/nestedCheckboxes', () => {
-  const Component = OrganismNestedCheckboxes
+describe(json.name, () => {
+  const {default: Component} = pkg
   const setup = setupEnvironment(Component)
 
-  it('should render without crashing', () => {
+  it('library should include defined exported elements', () => {
     // Given
-    const props = {
-      children: [
-        {id: 'nested-01', label: 'Nested 1', checked: true},
-        {id: 'nested-02', label: 'Nested 2', checked: false},
-        {id: 'nested-03', label: 'Nested 3', checked: true},
-        {id: 'nested-04', label: 'Nested 4', checked: true},
-        {id: 'nested-05', label: 'Nested 5', checked: true}
-      ].map(({checked, id, label}, index) => (
-        <MoleculeCheckboxField
-          key={id}
-          id={id}
-          checked={checked}
-          checkedIcon={() => null}
-          intermediateIcon={() => null}
-          onChange={() => null}
-          label={label}
-        />
-      ))
-    }
+    const library = pkg
+    const libraryExportedMembers = ['default']
 
     // When
-    const component = <Component {...props} />
+    const {default: OrganismNestedCheckboxes, ...others} = library
 
     // Then
-    const div = document.createElement('div')
-    ReactDOM.render(component, div)
-    ReactDOM.unmountComponentAtNode(div)
+    expect(Object.keys(library).length).to.equal(libraryExportedMembers.length)
+    expect(Object.keys(library)).to.have.members(libraryExportedMembers)
+    expect(Object.keys(others).length).to.equal(0)
   })
 
-  it('should NOT render null', () => {
-    // Given
-    const props = {
-      children: [
-        {id: 'nested-01', label: 'Nested 1', checked: true},
-        {id: 'nested-02', label: 'Nested 2', checked: false},
-        {id: 'nested-03', label: 'Nested 3', checked: true},
-        {id: 'nested-04', label: 'Nested 4', checked: true},
-        {id: 'nested-05', label: 'Nested 5', checked: true}
-      ].map(({checked, id, label}, index) => (
-        <MoleculeCheckboxField
-          key={id}
-          id={id}
-          checked={checked}
-          checkedIcon={() => null}
-          intermediateIcon={() => null}
-          onChange={() => null}
-          label={label}
-        />
-      ))
-    }
+  describe(Component.displayName, () => {
+    it('should render without crashing', () => {
+      // Given
+      const props = {
+        children: [
+          {id: 'nested-01', label: 'Nested 1', checked: true},
+          {id: 'nested-02', label: 'Nested 2', checked: false},
+          {id: 'nested-03', label: 'Nested 3', checked: true},
+          {id: 'nested-04', label: 'Nested 4', checked: true},
+          {id: 'nested-05', label: 'Nested 5', checked: true}
+        ].map(({checked, id, label}, index) => (
+          <MoleculeCheckboxField
+            key={id}
+            id={id}
+            checked={checked}
+            checkedIcon={() => null}
+            intermediateIcon={() => null}
+            onChange={() => null}
+            label={label}
+          />
+        ))
+      }
 
-    // When
-    const {container} = setup(props)
+      // When
+      const component = <Component {...props} />
 
-    // Then
-    expect(container.innerHTML).to.be.a('string')
-    expect(container.innerHTML).to.not.have.lengthOf(0)
-  })
+      // Then
+      const div = document.createElement('div')
+      ReactDOM.render(component, div)
+      ReactDOM.unmountComponentAtNode(div)
+    })
 
-  it('should NOT extend classNames', () => {
-    // Given
-    const props = {
-      children: [
-        {id: 'nested-01', label: 'Nested 1', checked: true},
-        {id: 'nested-02', label: 'Nested 2', checked: false},
-        {id: 'nested-03', label: 'Nested 3', checked: true},
-        {id: 'nested-04', label: 'Nested 4', checked: true},
-        {id: 'nested-05', label: 'Nested 5', checked: true}
-      ].map(({checked, id, label}, index) => (
-        <MoleculeCheckboxField
-          key={id}
-          id={id}
-          checked={checked}
-          checkedIcon={() => null}
-          intermediateIcon={() => null}
-          onChange={() => null}
-          label={label}
-        />
-      )),
-      className: 'extended-classNames'
-    }
-    const findSentence = str => string => string.match(new RegExp(`S*${str}S*`))
+    it('should NOT render null', () => {
+      // Given
+      const props = {
+        children: [
+          {id: 'nested-01', label: 'Nested 1', checked: true},
+          {id: 'nested-02', label: 'Nested 2', checked: false},
+          {id: 'nested-03', label: 'Nested 3', checked: true},
+          {id: 'nested-04', label: 'Nested 4', checked: true},
+          {id: 'nested-05', label: 'Nested 5', checked: true}
+        ].map(({checked, id, label}, index) => (
+          <MoleculeCheckboxField
+            key={id}
+            id={id}
+            checked={checked}
+            checkedIcon={() => null}
+            intermediateIcon={() => null}
+            onChange={() => null}
+            label={label}
+          />
+        ))
+      }
 
-    // When
-    const {container} = setup(props)
-    const findClassName = findSentence(props.className)
+      // When
+      const {container} = setup(props)
 
-    // Then
-    expect(findClassName(container.innerHTML)).to.be.null
+      // Then
+      expect(container.innerHTML).to.be.a('string')
+      expect(container.innerHTML).to.not.have.lengthOf(0)
+    })
+
+    it('should NOT extend classNames', () => {
+      // Given
+      const props = {
+        children: [
+          {id: 'nested-01', label: 'Nested 1', checked: true},
+          {id: 'nested-02', label: 'Nested 2', checked: false},
+          {id: 'nested-03', label: 'Nested 3', checked: true},
+          {id: 'nested-04', label: 'Nested 4', checked: true},
+          {id: 'nested-05', label: 'Nested 5', checked: true}
+        ].map(({checked, id, label}, index) => (
+          <MoleculeCheckboxField
+            key={id}
+            id={id}
+            checked={checked}
+            checkedIcon={() => null}
+            intermediateIcon={() => null}
+            onChange={() => null}
+            label={label}
+          />
+        )),
+        className: 'extended-classNames'
+      }
+      const findSentence = str => string =>
+        string.match(new RegExp(`S*${str}S*`))
+
+      // When
+      const {container} = setup(props)
+      const findClassName = findSentence(props.className)
+
+      // Then
+      expect(findClassName(container.innerHTML)).to.be.null
+    })
   })
 })

74be26e07477865149388031ac5dcd5d49a925aa
andresin87 <andresin87@gmail.com>
2022-03-23 12:03:50 +0100

    test(components/molecule/thumbnail): refactor

--- components/molecule/thumbnail/test/index.test.js
+++ components/molecule/thumbnail/test/index.test.js
@@ -10,57 +10,194 @@ import ReactDOM from 'react-dom'
 import chai, {expect} from 'chai'
 import chaiDOM from 'chai-dom'
 
+import * as pkg from '../src/index.js'
+
+import json from '../package.json'
+
 chai.use(chaiDOM)
 
-describe('molecule/thumbnail', () => {
-  const Component = MoleculeThumbnail
+describe(json.name, () => {
+  const {default: Component} = pkg
   const setup = setupEnvironment(Component)
 
-  it('should render without crashing', () => {
+  it('library should include defined exported elements', () => {
     // Given
-    const props = {
-      alt: 'alt',
-      src: '#'
-    }
+    const library = pkg
+    const libraryExportedMembers = [
+      'moleculeThumbnailRatio',
+      'moleculeThumbnailShape',
+      'moleculeThumbnailSize',
+      'default'
+    ]
 
     // When
-    const component = <Component {...props} />
+    const {
+      moleculeThumbnailRatio,
+      moleculeThumbnailShape,
+      moleculeThumbnailSize,
+      default: MoleculeThumbnail,
+      ...others
+    } = library
 
     // Then
-    const div = document.createElement('div')
-    ReactDOM.render(component, div)
-    ReactDOM.unmountComponentAtNode(div)
+    expect(Object.keys(library).length).to.equal(libraryExportedMembers.length)
+    expect(Object.keys(library)).to.have.members(libraryExportedMembers)
+    expect(Object.keys(others).length).to.equal(0)
   })
 
-  it('should NOT render null', () => {
-    // Given
-    const props = {
-      alt: 'alt',
-      src: '#'
-    }
+  describe(Component.displayName, () => {
+    it('should render without crashing', () => {
+      // Given
+      const props = {
+        alt: 'alt',
+        src: '#'
+      }
 
-    // When
-    const {container} = setup(props)
+      // When
+      const component = <Component {...props} />
 
-    // Then
-    expect(container.innerHTML).to.be.a('string')
-    expect(container.innerHTML).to.not.have.lengthOf(0)
+      // Then
+      const div = document.createElement('div')
+      ReactDOM.render(component, div)
+      ReactDOM.unmountComponentAtNode(div)
+    })
+
+    it('should NOT render null', () => {
+      // Given
+      const props = {
+        alt: 'alt',
+        src: '#'
+      }
+
+      // When
+      const {container} = setup(props)
+
+      // Then
+      expect(container.innerHTML).to.be.a('string')
+      expect(container.innerHTML).to.not.have.lengthOf(0)
+    })
+
+    it.skip('should NOT extend classNames', () => {
+      // Given
+      const props = {
+        alt: 'alt',
+        className: 'extended-classNames',
+        src: '#'
+      }
+      const findSentence = str => string =>
+        string.match(new RegExp(`S*${str}S*`))
+
+      // When
+      const {container} = setup(props)
+      const findClassName = findSentence(props.className)
+
+      // Then
+      expect(findClassName(container.innerHTML)).to.be.null
+    })
   })
 
-  it.skip('should NOT extend classNames', () => {
-    // Given
-    const props = {
-      alt: 'alt',
-      className: 'extended-classNames',
-      src: '#'
-    }
-    const findSentence = str => string => string.match(new RegExp(`S*${str}S*`))
+  describe('moleculeThumbnailRatio', () => {
+    it('value must be an object enum', () => {
+      // Given
+      const library = pkg
 
-    // When
-    const {container} = setup(props)
-    const findClassName = findSentence(props.className)
+      // When
+      const {moleculeThumbnailRatio: actual} = library
 
-    // Then
-    expect(findClassName(container.innerHTML)).to.be.null
+      // Then
+      expect(actual).to.be.an('object')
+    })
+
+    it('value must be a defined string-key pair filled', () => {
+      // Given
+      const library = pkg
+      const expected = {
+        '1:1': '1-1',
+        '4:3': '4-3',
+        '16:9': '16-9'
+      }
+
+      // When
+      const {moleculeThumbnailRatio: actual} = library
+      const {'1:1': one, '4:3': two, '16:9': three, ...others} = actual
+
+      // Then
+      expect(Object.keys(others).length).to.equal(0)
+      expect(Object.keys(actual)).to.have.members(Object.keys(expected))
+      Object.entries(expected).forEach(([expectedKey, expectedValue]) => {
+        expect(Object.keys(actual).includes(expectedKey)).to.be.true
+        expect(actual[expectedKey]).to.equal(expectedValue)
+      })
+    })
+  })
+
+  describe('moleculeThumbnailShape', () => {
+    it('value must be an object enum', () => {
+      // Given
+      const library = pkg
+
+      // When
+      const {moleculeThumbnailShape: actual} = library
+
+      // Then
+      expect(actual).to.be.an('object')
+    })
+
+    it('value must be a defined string-key pair filled', () => {
+      // Given
+      const library = pkg
+      const expected = {
+        SQUARED: 'squared',
+        CIRCLED: 'circled'
+      }
+
+      // When
+      const {moleculeThumbnailShape: actual} = library
+      const {SQUARED, CIRCLED, ...others} = actual
+
+      // Then
+      expect(Object.keys(others).length).to.equal(0)
+      expect(Object.keys(actual)).to.have.members(Object.keys(expected))
+      Object.entries(expected).forEach(([expectedKey, expectedValue]) => {
+        expect(Object.keys(actual).includes(expectedKey)).to.be.true
+        expect(actual[expectedKey]).to.equal(expectedValue)
+      })
+    })
+  })
+
+  describe('moleculeThumbnailSize', () => {
+    it('value must be an object enum', () => {
+      // Given
+      const library = pkg
+
+      // When
+      const {moleculeThumbnailSize: actual} = library
+
+      // Then
+      expect(actual).to.be.an('object')
+    })
+
+    it('value must be a defined string-key pair filled', () => {
+      // Given
+      const library = pkg
+      const expected = {
+        LARGE: 'large',
+        MEDIUM: 'medium',
+        SMALL: 'small',
+        XSMALL: 'xsmall'
+      }
+
+      // When
+      const {moleculeThumbnailSize: actual} = library
+      const {LARGE, MEDIUM, SMALL, XSMALL, ...others} = actual
+
+      // Then
+      expect(Object.keys(others).length).to.equal(0)
+      expect(Object.keys(actual)).to.have.members(Object.keys(expected))
+      Object.entries(expected).forEach(([expectedKey, expectedValue]) => {
+        expect(Object.keys(actual).includes(expectedKey)).to.be.true
+        expect(actual[expectedKey]).to.equal(expectedValue)
+      })
+    })
   })
 })

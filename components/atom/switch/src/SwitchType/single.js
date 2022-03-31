286573bb1c5adbe5a7debc9d3425410a86989cff
Oriol Puig <oriol.puig90@gmail.com>
2022-03-31 15:35:29 +0200

    fix(components/atom/switch): allow single switch be accessible by label

--- components/atom/switch/src/SwitchType/single.js
+++ components/atom/switch/src/SwitchType/single.js
@@ -14,7 +14,6 @@ export const SingleSwitchTypeRender = forwardRef(
     {
       disabled,
       isFitted,
-      isToggle,
       label,
       labelLeft,
       iconLeft,
@@ -81,6 +80,7 @@ export const SingleSwitchTypeRender = forwardRef(
             aria-disabled={disabled}
             disabled={disabled}
             {...(!disabled && {tabIndex: 0})}
+            {...(name && {id: name})}
           >
             <div className={cx(suitClass({element: 'icon-left'}))}>
               {iconLeft}

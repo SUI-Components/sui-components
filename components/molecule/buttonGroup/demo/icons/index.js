723b9be23fd033da1f7f669ee5419e8581d2ed90
Rafael Moral <rafamp89@gmail.com>
2022-04-05 16:01:50 +0200

    fix(components/molecule/buttonGroup/demo): resolve linting errors

--- components/molecule/buttonGroup/demo/icons/index.js
+++ components/molecule/buttonGroup/demo/icons/index.js
@@ -1,17 +1,16 @@
 /* eslint-disable react/prop-types */
 import AtomIcon from '@s-ui/react-atom-icon'
 
-const withIconSVGBounds = SVGChildren => ({
-  width = 16,
-  height = 16,
-  otherProps
-}) => (
-  <AtomIcon>
-    <svg width={width} height={height} {...otherProps}>
-      {SVGChildren}
-    </svg>
-  </AtomIcon>
-)
+const withIconSVGBounds =
+  SVGChildren =>
+  ({width = 16, height = 16, otherProps}) =>
+    (
+      <AtomIcon>
+        <svg width={width} height={height} {...otherProps}>
+          {SVGChildren}
+        </svg>
+      </AtomIcon>
+    )
 
 export const IconSquare = withIconSVGBounds(<rect width={16} height={16} />)
 export const IconTriangle = withIconSVGBounds(

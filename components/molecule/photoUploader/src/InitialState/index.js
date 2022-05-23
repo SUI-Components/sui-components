62fa1c75da49c1729feb0a47d30c95448223267b
Andres Alvarez <a.alvarez.sor@gmail.com>
2022-05-23 09:38:48 +0200

    Update components/molecule/photoUploader/src/InitialState/index.js
    Co-authored-by: Andrés <andresin87@gmail.com>

--- components/molecule/photoUploader/src/InitialState/index.js
+++ components/molecule/photoUploader/src/InitialState/index.js
@@ -1,4 +1,4 @@
-import cx from 'classname'
+import cx from 'classnames'
 import PropTypes from 'prop-types'
 import Button, {atomButtonShapes} from '@s-ui/react-atom-button'
 import AtomIcon, {ATOM_ICON_SIZES} from '@s-ui/react-atom-icon'

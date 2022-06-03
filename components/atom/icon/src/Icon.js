afea3ecc9dbddba2dd68fc9089c36d24ccfc88e1
andresin87 <andresin87@gmail.com>
2022-06-03 10:46:12 +0200

    feat(components/atom/icon): resolve conflicts

--- components/atom/icon/src/Icon.js
+++ components/atom/icon/src/Icon.js
@@ -2,23 +2,16 @@ import PropTypes from 'prop-types'
 
 import Poly from '@s-ui/react-primitive-polymorphic-element'
 
-import {IMG_ROLE} from './settings.js'
+import {getAttributes} from './settings.js'
 
-export default function AtomIcon({as, className, children, outerRef, title}) {
-  const a11yAttributes = title
-    ? {
-        role: IMG_ROLE,
-        ariaLabel: title
-      }
-    : {}
-
+const AtomIcon = ({as, className, children, outerRef, title}) => {
   return (
     <Poly
       as={as}
       className={className}
       title={title}
       ref={outerRef}
-      {...a11yAttributes}
+      {...getAttributes(title)}
     >
       {children}
     </Poly>
@@ -32,3 +25,5 @@ AtomIcon.propTypes = {
   outerRef: PropTypes.func,
   title: PropTypes.string
 }
+
+export default AtomIcon

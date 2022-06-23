9a7867d351f791a960a93b86c847f84a091cbbac
andresin87 <andresin87@gmail.com>
2022-06-23 08:50:38 +0200

    refactor(components/atom/badge): lint

--- components/atom/badge/test/index.test.js
+++ components/atom/badge/test/index.test.js
@@ -10,14 +10,13 @@ import ReactDOM from 'react-dom'
 import chai, {expect} from 'chai'
 import chaiDOM from 'chai-dom'
 
-import * as pkg from '../src/index.js'
-import {
-  truncateText,
-  shouldRenderIcon,
-  MAX_LABEL_LENGTH
-} from '../src/config.js'
-
 import json from '../package.json'
+import {
+  MAX_LABEL_LENGTH,
+  shouldRenderIcon,
+  truncateText
+} from '../src/config.js'
+import * as pkg from '../src/index.js'
 
 chai.use(chaiDOM)
 

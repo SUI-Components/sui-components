9a00828fe425e1cc56db329f116e10c72c5aac26
andresin87 <andresin87@gmail.com>
2022-06-23 09:23:31 +0200

    refactor(components/molecule/rating): lint

--- components/molecule/rating/demo/Icons/index.js
+++ components/molecule/rating/demo/Icons/index.js
@@ -1,7 +1,7 @@
 import IconStarFilled from './starFilled.js'
+import IconStarFilledCustom from './starFilledCustom.js'
 import IconStarHalfFilled from './starHalfFilled.js'
 import IconStarOutline from './starOutline.js'
-import IconStarFilledCustom from './starFilledCustom.js'
 import IconStarOutlineCustom from './starOutlineCustom.js'
 
 export {

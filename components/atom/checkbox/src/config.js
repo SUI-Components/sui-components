dae4854ed2f4d81b72f12661625838179e178735
andresin87 <andresin87@gmail.com>
2022-06-23 19:21:29 +0200

    feat(components/atom/checkbox): v2

--- components/atom/checkbox/src/config.js
+++ components/atom/checkbox/src/config.js
@@ -1,7 +1,4 @@
-import cx from 'classnames'
-
 export const BASE_CLASS = 'sui-AtomCheckbox'
-export const CLASS_ICON = 'sui-AtomCheckbox--Icon'
 
 export const CHECKBOX_STATUS = {
   ERROR: 'error',
@@ -10,65 +7,3 @@ export const CHECKBOX_STATUS = {
 }
 
 export const CHECKBOX_SIZES = {SMALL: 'small', MEDIUM: 'medium'}
-
-export const className = ({size, checked, disabled, indeterminate}) =>
-  cx(CLASS_ICON, {
-    [`${CLASS_ICON}--${size}`]: Object.values(CHECKBOX_SIZES).includes(size),
-    'is-checked': checked,
-    'is-disabled': disabled,
-    'is-indeterminate': indeterminate
-  })
-
-export const pressedValue = ({checked, indeterminate}) => {
-  if (checked) {
-    return 'true'
-  } else if (indeterminate) {
-    return 'mixed'
-  }
-  return 'false'
-}
-
-export const getIsNative = (
-  {checked, indeterminate},
-  {CheckedIcon, UncheckedIcon, IndeterminateIcon, Icon}
-) => {
-  if (checked && !CheckedIcon && !Icon) return true
-  else if (!checked && indeterminate && !IndeterminateIcon && !Icon) return true
-  else if (!checked && !indeterminate && !UncheckedIcon && !Icon) return true
-  return false
-}
-
-export const getIcon = (
-  {isNative, checked, indeterminate},
-  {CheckedIcon, UncheckedIcon, IndeterminateIcon, Icon}
-) => {
-  if (isNative) {
-    return null
-  }
-  let ResultingIcon = Icon || CheckedIcon
-  if (!checked) {
-    if (indeterminate) {
-      ResultingIcon = Icon || IndeterminateIcon
-    } else {
-      ResultingIcon = Icon || UncheckedIcon
-    }
-  }
-  return ResultingIcon
-}
-
-export const updateStatus = (
-  element,
-  {isIndeterminate = false, isChecked = false}
-) => {
-  if (!element) {
-    return
-  }
-  const {indeterminate} = element
-  if (isIndeterminate !== indeterminate) {
-    element.indeterminate = isChecked ? false : isIndeterminate
-    return
-  }
-  return undefined
-}
-
-export const isFunction = fn => typeof fn === 'function'

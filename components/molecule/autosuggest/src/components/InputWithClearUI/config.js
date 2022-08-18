011393227cea46145e2a0196c2ee5a8861de631e
Andrés Alvarez Sordo <a.alvarez.sor@gmail.com>
2022-08-18 10:44:12 +0200

    feat(components/molecule/autosuggest): clear icon as right icon

--- components/molecule/autosuggest/src/components/InputWithClearUI/config.js
+++ components/molecule/autosuggest/src/components/InputWithClearUI/config.js
@@ -1,3 +1,2 @@
 export const BASE_CLASS = `sui-MoleculeAutosuggest-input`
 export const CLASS_CONTAINER = `${BASE_CLASS}-container`
-export const CLASS_ICON_CLEAR = `${BASE_CLASS}-icon`
--- components/molecule/autosuggest/src/components/InputWithClearUI/index.js
+++ components/molecule/autosuggest/src/components/InputWithClearUI/index.js
@@ -2,29 +2,32 @@ import {Children, cloneElement} from 'react'
 
 import PropTypes from 'prop-types'
 
-import {CLASS_CONTAINER, CLASS_ICON_CLEAR} from './config.js'
+import {CLASS_CONTAINER} from './config.js'
 
 export const InputWithClearUI = props => {
-  const {onClickClear, isVisibleClear, iconClear, children} = props
+  const {onClickClear, isVisibleClear, iconClear, rightIcon, children} = props
 
   const extendedChildren = () =>
     Children.toArray(children)
       .filter(Boolean)
-      .map((child, index) => cloneElement(child, props))
+      .map((child, index) =>
+        cloneElement(child, {
+          ...props,
+          rightIcon:
+            isVisibleClear && iconClear ? (
+              <span onClick={onClickClear}>{iconClear}</span>
+            ) : (
+              rightIcon
+            )
+        })
+      )
 
-  return (
-    <div className={CLASS_CONTAINER}>
-      {extendedChildren()}
-      {!!isVisibleClear && iconClear && (
-        <span className={CLASS_ICON_CLEAR} onClick={onClickClear}>
-          {iconClear}
-        </span>
-      )}
-    </div>
-  )
+  return <div className={CLASS_CONTAINER}>{extendedChildren()}</div>
 }
 
 InputWithClearUI.propTypes = {
+  rightIcon: PropTypes.element,
+
   children: PropTypes.element,
 
   iconClear: PropTypes.node,

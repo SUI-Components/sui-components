5040a8aad30e078b35b4994551b51817038c1444
Oriol Egea <egea@oriol.im>
2022-08-25 13:54:56 +0200

    feat(components/molecule/imageEditor): Add row layout and help text to molecule image editor

--- components/molecule/imageEditor/src/index.js
+++ components/molecule/imageEditor/src/index.js
@@ -1,6 +1,7 @@
 import {useCallback, useState} from 'react'
 import Cropper from 'react-easy-crop'
 
+import cx from 'classnames'
 import PropTypes from 'prop-types'
 
 import {debounce} from '@s-ui/js/lib/function/debounce.js'
@@ -13,11 +14,13 @@ const MoleculeImageEditor = ({
   aspect = DEFAULT_ASPECT,
   cropLabelIcon,
   cropLabelText,
+  helpText,
   image,
   onChange,
   onCropping = noop,
   rotateLabelIcon,
-  rotateLabelText
+  rotateLabelText,
+  useRowLayout = false
 }) => {
   const [crop, cropSetter] = useState({x: 0, y: 0})
   const [rotation, rotationSetter] = useState(0)
@@ -46,8 +49,12 @@ const MoleculeImageEditor = ({
     [rotation, onCropping, image, onChange]
   )
 
+  const containerClass = cx(baseClass, {
+    [`${baseClass}-rowLayout`]: useRowLayout
+  })
+
   return (
-    <div className={baseClass}>
+    <div className={containerClass}>
       <div className={`${baseClass}-crop`}>
         <Cropper
           image={image}
@@ -61,43 +68,50 @@ const MoleculeImageEditor = ({
           onZoomChange={zoom => setZoom((zoom - 1) * 100)}
         />
       </div>
-      <div className={`${baseClass}-slider`}>
-        {(cropLabelIcon || cropLabelText) && (
-          <div className={`${baseClass}-label`}>
-            {cropLabelIcon && (
-              <span className={`${baseClass}-labelIcon`}>{cropLabelIcon}</span>
-            )}
-            {cropLabelText && (
-              <span className={`${baseClass}-labelText`}>{cropLabelText}</span>
-            )}
-          </div>
-        )}
-        <AtomSlider
-          onChange={(event, {value}) => setZoom(value)}
-          defaultValue={zoom}
-          hideMarks
-        />
-      </div>
-      <div className={`${baseClass}-slider`}>
-        {(rotateLabelIcon || rotateLabelText) && (
-          <div className={`${baseClass}-label`}>
-            {rotateLabelIcon && (
-              <span className={`${baseClass}-labelIcon`}>
-                {rotateLabelIcon}
-              </span>
-            )}
-            {rotateLabelText && (
-              <span className={`${baseClass}-labelText`}>
-                {rotateLabelText}
-              </span>
-            )}
-          </div>
-        )}
-        <AtomSlider
-          onChange={(event, {value}) => setRotation(value)}
-          defaultValue={rotation}
-          hideMarks
-        />
+      <div className={`${baseClass}-sliders`}>
+        <div className={`${baseClass}-slider`}>
+          {(cropLabelIcon || cropLabelText) && (
+            <div className={`${baseClass}-label`}>
+              {cropLabelIcon && (
+                <span className={`${baseClass}-labelIcon`}>
+                  {cropLabelIcon}
+                </span>
+              )}
+              {cropLabelText && (
+                <span className={`${baseClass}-labelText`}>
+                  {cropLabelText}
+                </span>
+              )}
+            </div>
+          )}
+          <AtomSlider
+            onChange={(event, {value}) => setZoom(value)}
+            defaultValue={zoom}
+            hideMarks
+          />
+        </div>
+        <div className={`${baseClass}-slider`}>
+          {(rotateLabelIcon || rotateLabelText) && (
+            <div className={`${baseClass}-label`}>
+              {rotateLabelIcon && (
+                <span className={`${baseClass}-labelIcon`}>
+                  {rotateLabelIcon}
+                </span>
+              )}
+              {rotateLabelText && (
+                <span className={`${baseClass}-labelText`}>
+                  {rotateLabelText}
+                </span>
+              )}
+            </div>
+          )}
+          <AtomSlider
+            onChange={(event, {value}) => setRotation(value)}
+            defaultValue={rotation}
+            hideMarks
+          />
+        </div>
+        {helpText && <div className={`${baseClass}-helpText`}>{helpText}</div>}
       </div>
     </div>
   )
@@ -108,11 +122,13 @@ MoleculeImageEditor.propTypes = {
   aspect: PropTypes.number,
   cropLabelIcon: PropTypes.node,
   cropLabelText: PropTypes.string,
+  helpText: PropTypes.string,
   image: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
   onCropping: PropTypes.func,
   rotateLabelIcon: PropTypes.node,
-  rotateLabelText: PropTypes.string
+  rotateLabelText: PropTypes.string,
+  useRowLayout: PropTypes.bool
 }
 
 export default MoleculeImageEditor

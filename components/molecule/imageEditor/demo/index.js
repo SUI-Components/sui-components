ea312440e9a6b170d828c8ebd8bad7e704be5d58
andresin87 <andresin87@gmail.com>
2022-08-29 12:13:55 +0200

    docs(components/molecule/imageEditor/demo): add suggestions

--- components/molecule/imageEditor/demo/index.js
+++ components/molecule/imageEditor/demo/index.js
@@ -8,8 +8,6 @@ const Demo = () => (
   <div className="sui-StudioPreview">
     <H1>Image Editor</H1>
     <Paragraph>
-      The ImageEditor component offers an interface with which an existing image
-      can be cropped, rotated, and then exported to a blob object. In practice,
       This component allows selecting an image (both from an existing public URL
       and by getting a blob object url from a local file), edit it, and then
       receive the edited image as a blob object to store it or send to any kind

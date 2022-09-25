f99c74de8cae57bbaf28cd737a53acb48e95a70c
andresin87 <andresin87@gmail.com>
2022-09-25 23:48:52 +0200

    chore(Root): wip

--- .github/SCRIPTS/hacktoberfest.js
+++ .github/SCRIPTS/hacktoberfest.js
@@ -1,38 +1,47 @@
-
-
-module.exports = async function exportCoverageFromMarkdownShields({github, context, core, exec}, senderNames = []) {
+module.exports = async function exportCoverageFromMarkdownShields(
+  {github, context, core, exec},
+  senderNames = []
+) {
   const {
     workflow,
     eventName,
     actor,
     payload: {
       action,
-      label: {
-        name: labelName
-      },
-      sender: {
-        login: sender
-      },
+      label: {name: labelName},
+      sender: {login: sender},
       repository: {
-        name,
-        owner: {
-          login: owner
-        }
+        name: repositoryName,
+        owner: {login: ownerName}
       }
     }
   } = context
 
-  console.log({workflow, eventName, actor, action, labelName, sender, owner, name})
+  console.log({
+    workflow,
+    eventName,
+    actor,
+    action,
+    labelName,
+    sender,
+    ownerName,
+    name
+  })
 
-  if(senderNames.length === 0 || senderNames.includes(sender)) {
-    // const { data } = await github.rest.projects.listForRepo({ ...repo });
+  if (senderNames.length === 0 || senderNames.includes(sender)) {
+    const {data} = await github.rest.projects.listForRepo({
+      owner: ownerName,
+      repo: repositoryName
+    })
+
+    console.log(data)
     switch (action) {
       case 'unlabeled':
-        break;
+        break
       case 'labeled':
-        break;
+        break
       default:
-        break;
+        break
     }
   }
 }

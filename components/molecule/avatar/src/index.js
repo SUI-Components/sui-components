c10fdd6f964a2f5bf9559b86e26021e04e6e50cd
Stefi Rosca <stefania.rosca2309@gmail.com>
2022-10-01 10:37:13 +0200

    refactor(components/molecule/avatar): Replace cloneElement by injector

--- components/molecule/avatar/src/index.js
+++ components/molecule/avatar/src/index.js
@@ -1,10 +1,4 @@
-import {
-  Children,
-  cloneElement,
-  forwardRef,
-  isValidElement,
-  useCallback
-} from 'react'
+import {forwardRef, useCallback} from 'react'
 
 import cx from 'classnames'
 import PropTypes from 'prop-types'
@@ -14,6 +8,7 @@ import AtomSkeleton, {
   ATOM_SKELETON_ANIMATIONS,
   ATOM_SKELETON_VARIANTS
 } from '@s-ui/react-atom-skeleton'
+import Injector from '@s-ui/react-primitive-injector'
 
 import AvatarBadge, {
   AVATAR_BADGE_PLACEMENTS,
@@ -42,15 +37,12 @@ const MoleculeAvatar = forwardRef(
       style,
       isLoading,
       imageProps = {},
-      children: childrenProp,
+      children,
       ...others
     },
     forwardedRef
   ) => {
     const className = cx(baseClassName, `${baseClassName}--${size}`)
-    const children = Children.toArray(childrenProp)
-      .filter(child => isValidElement(child))
-      .map(child => cloneElement(child, {size}))
 
     const renderContent = useCallback(() => {
       if (isLoading) {
@@ -73,7 +65,7 @@ const MoleculeAvatar = forwardRef(
           ) : (
             fallback
           )}
-          {!isLoading && children}
+          {!isLoading && <Injector size={size}>{children}</Injector>}
         </>
       )
     }, [

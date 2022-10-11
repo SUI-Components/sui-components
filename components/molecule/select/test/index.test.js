5e1c008d9682cd5fd8e616186d60ea9472aac148
alfredo.narvaez <alfredo.narvaez@adevinta.com>
2022-10-11 13:09:23 +0200

    fix(components/molecule/select): accept onblur prop
    ISSUES CLOSED: #2282

--- components/molecule/select/test/index.test.js
+++ components/molecule/select/test/index.test.js
@@ -9,6 +9,9 @@ import ReactDOM from 'react-dom'
 
 import chai, {expect} from 'chai'
 import chaiDOM from 'chai-dom'
+import sinon from 'sinon'
+
+import userEvents from '@testing-library/user-event'
 
 import json from '../package.json'
 import * as pkg from '../src/index.js'
@@ -84,6 +87,24 @@ describe(json.name, () => {
 
       // Then
       expect(findClassName(container.innerHTML)).to.be.null
+    })
+
+    it('should call onBlur callback', () => {
+      const spy = sinon.spy()
+
+      const props = {
+        onBlur: spy
+      }
+
+      // When
+      const {getByRole} = setup(props)
+      const textBox = getByRole('textbox')
+
+      userEvents.click(textBox)
+      userEvents.tab()
+
+      // Then
+      sinon.assert.calledOnce(spy)
     })
   })
 

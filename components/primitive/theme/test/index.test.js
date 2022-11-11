7980b55a21c0bd8ef7c9ff3267d3281793cf0d7b
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2022-11-11 12:03:14 +0100

    feat(components/primitive/theme): add new component for the theme

---
+++ components/primitive/theme/test/index.test.js
@@ -0,0 +1,48 @@
+/*
+ * Remember: YOUR COMPONENT IS DEFINED GLOBALLY
+ * */
+
+/* eslint react/jsx-no-undef:0 */
+/* eslint no-undef:0 */
+
+import ReactDOM from 'react-dom'
+
+import chai, {expect} from 'chai'
+import chaiDOM from 'chai-dom'
+
+import json from '../package.json'
+import * as pkg from '../src/index.js'
+
+chai.use(chaiDOM)
+
+describe(json.name, () => {
+  const {default: Component} = pkg
+  const setup = setupEnvironment(Component)
+
+  describe(Component.displayName, () => {
+    it('should render without crashing', () => {
+      // Given
+      const props = {}
+
+      // When
+      const component = <Component {...props} />
+
+      // Then
+      const div = document.createElement('div')
+      ReactDOM.render(component, div)
+      ReactDOM.unmountComponentAtNode(div)
+    })
+
+    it('should render null', () => {
+      // Given
+      const props = {}
+
+      // When
+      const {container} = setup(props)
+
+      // Then
+      expect(container.innerHTML).to.be.a('string')
+      expect(container.innerHTML).to.have.lengthOf(0)
+    })
+  })
+})

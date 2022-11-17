95fa78bce7c70836137508e0eb66fad9a4bc350c
arnau-rius <arnau.rius.contact@gmail.com>
2022-11-17 12:55:54 +0100

    feat(components/primitive/theme): add style with custom properties

--- components/primitive/theme/src/index.js
+++ components/primitive/theme/src/index.js
@@ -1,8 +1,18 @@
 import './index.scss'
-
+import Head from '@s-ui/react-head'
 // eslint-disable-next-line
-function Theme({children}) {
-  return <>{children}</>
+function Theme({children, theme}) {
+  return (
+    <>
+      <Head>
+        <style>{`
+      .sui-AtomButton {
+        --color-inverted: var(--c-primary-900);
+      }`}</style>
+      </Head>
+      {children}
+    </>
+  )
 }
 
 export default Theme

3da6d2e32c89902d021df36250c9ca2cbe166053
andresin87 <andresin87@gmail.com>
2022-11-21 09:46:56 +0100

    test(components/atom/progressBar): add progressbar tests

--- components/atom/progressBar/test/index.test.js
+++ components/atom/progressBar/test/index.test.js
@@ -84,11 +84,21 @@ describe(json.name, () => {
       expect(findClassName(container.innerHTML)).to.be.null
     })
 
-    describe('type line', () => {})
+    describe('type line', () => {
+      // Given
+      const props = {type: pkg.atomProgressBarTypes.LINE, percentage: 50}
+
+      // When
+      const {container} = setup(props)
+
+      // Then
+      expect(container.innerHTML).to.be.a('string')
+      expect(container.innerHTML).to.not.have.lengthOf(0)
+    })
     describe('type circle', () => {
       it('should NOT render null', () => {
         // Given
-        const props = {type: pkg.atomProgressBarTypes.CIRCLE}
+        const props = {type: pkg.atomProgressBarTypes.CIRCLE, percentage: 50}
 
         // When
         const {container} = setup(props)
@@ -97,6 +107,21 @@ describe(json.name, () => {
         expect(container.innerHTML).to.be.a('string')
         expect(container.innerHTML).to.not.have.lengthOf(0)
       })
+    })
+    describe('type double line', () => {
+      // Given
+      const props = {
+        type: pkg.atomProgressBarTypes.LINE_DOUBLE_BAR,
+        mainBarPercentage: 25,
+        extraBarPercentage: 50
+      }
+
+      // When
+      const {container} = setup(props)
+
+      // Then
+      expect(container.innerHTML).to.be.a('string')
+      expect(container.innerHTML).to.not.have.lengthOf(0)
     })
   })
 

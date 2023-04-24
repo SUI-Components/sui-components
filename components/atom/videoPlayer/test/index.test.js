a12f745ebe6f7d2f18bcbf13d710936def638c4d
Oriol Egea <egea@oriol.im>
2023-04-24 17:10:44 +0200

    refactor(components/atom/videoPlayer): Remove not-needed comment

--- components/atom/videoPlayer/test/index.test.js
+++ components/atom/videoPlayer/test/index.test.js
@@ -104,10 +104,6 @@ describe('AtomVideoPlayer', () => {
     })
   })
 
-  /*
-  <div style="padding:73.33% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/54289199?title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
-  */
-
   describe('VIMEO Videos', () => {
     it('should embed the vimeo video player if src is a vimeo video', () => {
       // Given

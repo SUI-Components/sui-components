6af2be0ab946ff793a2a7d5b37649cb1cb28710a
Alverd04 <albert.font.fernandez@gmail.com>
2023-05-10 13:46:54 +0200

    feat(components/atom/input): Add missing export

--- components/atom/input/src/Input/Wrappers/Button/config.js
+++ components/atom/input/src/Input/Wrappers/Button/config.js
@@ -1,12 +1,4 @@
-import cx from 'classnames'
-
 import {BASE, BASE_CLASS_ITEM} from '../../../config.js'
 
-export const BASE_CLASS_ADDON = `${BASE}--withAddon`
-export const ADDON_TYPES = {
-  LEFT: 'left',
-  RIGHT: 'right'
-}
-
-export const getClassName = ({type}) =>
-  cx(BASE_CLASS_ITEM, BASE_CLASS_ADDON, `${BASE_CLASS_ADDON}--${type}`)
+export {BASE_CLASS_ITEM}
+export const BASE_CLASS_BUTTON = `${BASE}--withButton`

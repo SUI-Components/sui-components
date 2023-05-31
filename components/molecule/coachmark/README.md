9dfe904eed1e3d52edb183d0a381597f78788f1f
xavi-murcia <xavi.murcia@adevinta.com>
2023-05-31 11:58:38 +0200

    feat(components/molecule/coachmark): create sui coachmark component skeleton

---
+++ components/molecule/coachmark/README.md
@@ -0,0 +1,32 @@
+# MoleculeCoachmark
+
+`MoleculeCoachmark` is a wrapper of the [react-joyride](https://react-joyride.com/) library in order to create smooth guided tours in the apps to explain new features to the users
+
+## Installation
+
+```sh
+$ npm install @s-ui/react-molecule-coachmark
+```
+
+## Usage
+
+### Basic usage
+
+#### Import package and use the component
+
+```js
+import MoleculeCoachmark from '@s-ui/react-molecule-coachmark'
+
+return (<MoleculeCoachmark />)
+```
+
+#### Import the styles (Sass)
+
+```css
+@import '~@s-ui/theme/lib/index';
+/* @import 'your theme'; */
+@import '~@s-ui/react-molecule-coachmark/lib/index';
+```
+
+
+> **Find full description and more examples in the [demo page](#).**
---
+++ components/molecule/coachmark/test/index.test.js
@@ -0,0 +1,56 @@
+/*
+ * Remember: YOUR COMPONENT IS DEFINED GLOBALLY
+ * */
+
+/* eslint react/jsx-no-undef:0 */
+/* eslint no-undef:0 */
+
+import ReactDOM from 'react-dom'
+
+import chai, {expect} from 'chai'
+import chaiDOM from 'chai-dom'
+import Component from '../src/index.js'
+
+chai.use(chaiDOM)
+
+describe('MoleculeCoachmark', () => {
+  const setup = setupEnvironment(Component)
+
+  it('should render without crashing', () => {
+    // Given
+    const props = {}
+
+    // When
+    const component = <Component {...props} />
+
+    // Then
+    const div = document.createElement('div')
+    ReactDOM.render(component, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  it('should NOT render null', () => {
+    // Given
+    const props = {}
+
+    // When
+    const {container} = setup(props)
+
+    // Then
+    expect(container.innerHTML).to.be.a('string')
+    expect(container.innerHTML).to.not.have.lengthOf(0)
+  })
+
+  it.skip('should NOT extend classNames', () => {
+    // Given
+    const props = {className: 'extended-classNames'}
+    const findSentence = str => string => string.match(new RegExp(`S*${str}S*`))
+
+    // When
+    const {container} = setup(props)
+    const findClassName = findSentence(props.className)
+
+    // Then
+    expect(findClassName(container.innerHTML)).to.be.null
+  })
+})

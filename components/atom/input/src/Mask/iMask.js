062936c583f0ddbf277c83f7c18e865e1790b839
Albert Font <albert.font@adevinta.com>
2023-06-14 12:03:58 +0200

    fix(components/atom/input): Change null name variable

--- components/atom/input/src/Mask/iMask.js
+++ components/atom/input/src/Mask/iMask.js
@@ -3,7 +3,9 @@ import {IMaskMixin} from 'react-imask'
 import Input from '../Input/Component/index.js'
 
 const IMask = IMaskMixin(({inputRef, value, size, ...props}) => {
-  return <Input ref={inputRef} id={name} size={size} value={value} {...props} />
+  return (
+    <Input ref={inputRef} id={value} size={size} value={value} {...props} />
+  )
 })
 
 export default IMask

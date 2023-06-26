319c277ba2fdba435cc85d740692acc9d3fe2390
xavi-murcia <xavi.murcia@adevinta.com>
2023-06-26 11:43:10 +0200

    feat(components/molecule/coachmark): return joyride with all possible props but locale and tooltip c

---
+++ components/molecule/coachmark/src/settings.js
@@ -0,0 +1,23 @@
+export const STEP_PLACEMENT_TYPES = {
+  'bottom-end': 'bottom-end',
+  'bottom-start': 'bottom-start',
+  'left-end': 'left-end',
+  'left-start': 'left-start',
+  'right-end': 'right-end',
+  'right-start': 'right-start',
+  'top-end': 'top-end',
+  'top-start': 'top-start',
+  auto: 'auto',
+  bottom: 'bottom',
+  center: 'center',
+  left: 'left',
+  right: 'right',
+  top: 'top'
+}
+
+export const STEP_BEACON_PLACEMENT_TYPES = {
+  bottom: 'bottom',
+  left: 'left',
+  right: 'right',
+  top: 'top'
+}

04e0ed6dd4f2680ab6728f8074cf02ce356d95f5
andresin87 <andresin87@gmail.com>
2023-11-23 09:29:35 +0100

    fix(components/atom/switch): lint

--- components/atom/switch/demo/articles/ArticleCircleIcons.js
+++ components/atom/switch/demo/articles/ArticleCircleIcons.js
@@ -2,15 +2,7 @@ import {Fragment} from 'react'
 
 import PropTypes from 'prop-types'
 
-import {
-  Article,
-  Cell,
-  Code,
-  Grid,
-  H2,
-  Label,
-  Paragraph
-} from '@s-ui/documentation-library'
+import {Article, Cell, Code, Grid, H2, Label, Paragraph} from '@s-ui/documentation-library'
 
 import AtomSwitch, {atomSwitchSizes, atomSwitchTypes} from '../../src/index.js'
 import {flexCenteredStyle} from '../settings.js'
@@ -19,8 +11,8 @@ const ArticleCircleIcons = ({className}) => (
   <Article className={className}>
     <H2>leftIcon and rightIcon</H2>
     <Paragraph>
-      The <Code>leftIcon</Code> and <Code>rightIcon</Code> is used to show the
-      inner circle element. Usually used for icons
+      The <Code>leftIcon</Code> and <Code>rightIcon</Code> is used to show the inner circle element. Usually used for
+      icons
     </Paragraph>
     <Grid cols={7}>
       <Cell />
@@ -39,12 +31,8 @@ const ArticleCircleIcons = ({className}) => (
             <Cell key={index} style={flexCenteredStyle} span={2}>
               <AtomSwitch
                 size={atomSwitchSize}
-                labelLeft={
-                  type !== atomSwitchTypes.SINGLE ? 'labelLeft' : undefined
-                }
-                labelRight={
-                  type !== atomSwitchTypes.SINGLE ? 'labelRight' : undefined
-                }
+                labelLeft={type !== atomSwitchTypes.SINGLE ? 'labelLeft' : undefined}
+                labelRight={type !== atomSwitchTypes.SINGLE ? 'labelRight' : undefined}
                 iconLeft="☽"
                 iconRight="☼"
                 label="label"

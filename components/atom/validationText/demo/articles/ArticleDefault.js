b47deb6a773ceccdfa143cbe5eb2c6cb6e1860d9
andresin87 <andresin87@gmail.com>
2023-11-23 09:31:57 +0100

    fix(components/atom/validationText): lint

--- components/atom/validationText/demo/articles/ArticleDefault.js
+++ components/atom/validationText/demo/articles/ArticleDefault.js
@@ -2,17 +2,7 @@ import {useState} from 'react'
 
 import PropTypes from 'prop-types'
 
-import {
-  Article,
-  Cell,
-  Code,
-  Grid,
-  H2,
-  Input,
-  Label,
-  Paragraph,
-  RadioButton
-} from '@s-ui/documentation-library'
+import {Article, Cell, Code, Grid, H2, Input, Label, Paragraph, RadioButton} from '@s-ui/documentation-library'
 
 import AtomValidationText, {AtomValidationTextTypes} from '../../src/index.js'
 import {flexCenteredStyle} from '../settings.js'
@@ -23,24 +13,14 @@ const ArticleDefault = ({className}) => {
     <Article className={className} mode={mode}>
       <H2>Type</H2>
       <Paragraph>
-        using the prop <Code>type</Code> user can inherit the helpText color
-        styler for validation text usages.
+        using the prop <Code>type</Code> user can inherit the helpText color styler for validation text usages.
       </Paragraph>
-      <RadioButton
-        onClick={() => setMode(mode === 'light' ? 'dark' : 'light')}
-        label={mode}
-      />
+      <RadioButton onClick={() => setMode(mode === 'light' ? 'dark' : 'light')} label={mode} />
       <br />
       <br />
-      <Grid
-        cols={Object.values(AtomValidationTextTypes).length}
-        gutter={[10, 10]}
-      >
+      <Grid cols={Object.values(AtomValidationTextTypes).length} gutter={[10, 10]}>
         {Object.values(AtomValidationTextTypes).map((type, index) => (
-          <Cell
-            style={{...flexCenteredStyle, justifyContent: 'flex-start'}}
-            key={index}
-          >
+          <Cell style={{...flexCenteredStyle, justifyContent: 'flex-start'}} key={index}>
             <Label>{type}</Label>
           </Cell>
         ))}

d51a4c87e1a56029eb89b21283190c037f831eb5
Oriol Egea <egea@oriol.im>
2024-02-06 14:05:57 +0100

    test(components/primitive/loadingIcon): Adjust tests

--- components/primitive/loadingIcon/test/index.test.js
+++ components/primitive/loadingIcon/test/index.test.js
@@ -9,19 +9,18 @@ import ReactDOM from 'react-dom'
 
 import chai, {expect} from 'chai'
 import chaiDOM from 'chai-dom'
-import Component from '../src/index.js'
 
 chai.use(chaiDOM)
 
 describe('PrimitiveLoadingIcon', () => {
-  const setup = setupEnvironment(Component)
+  const setup = setupEnvironment(PrimitiveLoadingIcon)
 
   it('should render without crashing', () => {
     // Given
     const props = {}
 
     // When
-    const component = <Component {...props} />
+    const component = <PrimitiveLoadingIcon {...props} />
 
     // Then
     const div = document.createElement('div')
@@ -39,18 +38,5 @@ describe('PrimitiveLoadingIcon', () => {
     // Then
     expect(container.innerHTML).to.be.a('string')
     expect(container.innerHTML).to.not.have.lengthOf(0)
-  })
-
-  it.skip('should NOT extend classNames', () => {
-    // Given
-    const props = {className: 'extended-classNames'}
-    const findSentence = str => string => string.match(new RegExp(`S*${str}S*`))
-
-    // When
-    const {container} = setup(props)
-    const findClassName = findSentence(props.className)
-
-    // Then
-    expect(findClassName(container.innerHTML)).to.be.null
   })
 })

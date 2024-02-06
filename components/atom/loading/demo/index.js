59d76ee9ed4ac89cee7e83f2ddf45303b41e0dee
Oriol Egea <egea@oriol.im>
2024-02-06 10:27:50 +0100

    feat(components/atom/loading): Add demo and parametrize css

--- components/atom/loading/demo/index.js
+++ components/atom/loading/demo/index.js
@@ -1,2 +1,13 @@
 import AtomLoading from 'components/atom/loading/src'
-export default () => <AtomLoading />
+
+import {H1, Paragraph} from '@s-ui/documentation-library'
+export default () => (
+  <div className="sui-StudioPreview">
+    <H1>AtomLoading</H1>
+    <Paragraph>
+      An animated circular loop used for giving users feedback while the content
+      of a page or section is being loaded.
+    </Paragraph>
+    <AtomLoading />
+  </div>
+)

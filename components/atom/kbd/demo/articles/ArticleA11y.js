952fdf8319c45a0820c656ca68a38134b1348162
andresin87 <andresin87@gmail.com>
2025-02-04 11:59:33 +0100

    docs(components/atom/kbd): link proper a11y section

--- components/atom/kbd/demo/articles/ArticleA11y.js
+++ components/atom/kbd/demo/articles/ArticleA11y.js
@@ -1,15 +1,21 @@
 import PropTypes from 'prop-types'
 
-import {Article, Box, H2, Paragraph} from '@s-ui/documentation-library'
+import {Anchor, Article, Box, H2, Paragraph, Strong} from '@s-ui/documentation-library'
 
 const ArticleA11y = ({className}) => {
   return (
     <Article className={className}>
-      <H2>Accessibility</H2>
+      <H2>
+        Accessibility –{' '}
+        <Anchor href="https://github.com/SUI-Components/sui-components/blob/master/contributor-docs/Accessibility%20Standards.md">
+          <Strong>Guidelines</Strong>
+        </Anchor>
+      </H2>
       <Box style={{backgroundColor: 'color-mix(in srgb, #00FF00 10%, transparent)'}}>
         <Paragraph>
-          ✅ This component has been successfully tested for WCAG 2.0 levels A and AA, WCAG 2.1 levels A and AA and for
-          common accessibility best practices.
+          ✅ This component has been successfully tested for{' '}
+          <Strong>WCAG 2.0 levels A and AA, WCAG 2.1 levels A and AA</Strong> and for common accessibility best
+          practices.
         </Paragraph>
       </Box>
     </Article>

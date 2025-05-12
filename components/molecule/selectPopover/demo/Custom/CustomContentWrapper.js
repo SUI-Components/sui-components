c3beb6d7c28d7f1af72ea3c3e0ee0323f99bbac2
andresin87 <andresin87@gmail.com>
2025-05-12 15:54:26 +0200

    fix(components/molecule/selectPopover): add css rules for proper action sscetion accomodation

---
+++ components/molecule/selectPopover/demo/Custom/CustomContentWrapper.js
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types'
+
+import MoleculeModal from '@s-ui/react-molecule-modal'
+
+const CustomContentWrapper = ({actions, content, isOpen, setIsOpen}) => {
+  return (
+    <MoleculeModal isOpen={isOpen}>
+      <MoleculeModal.Overlay />
+      <MoleculeModal.Portal>
+        <MoleculeModal.Content>
+          <MoleculeModal.Header>
+            <MoleculeModal.Title>Custom select popover</MoleculeModal.Title>
+          </MoleculeModal.Header>
+          <MoleculeModal.Body>
+            <MoleculeModal.ScrollArea>
+              <MoleculeModal.Description>choose the proper options</MoleculeModal.Description>
+            </MoleculeModal.ScrollArea>
+            {content}
+          </MoleculeModal.Body>
+          <MoleculeModal.Footer isInset>{actions}</MoleculeModal.Footer>
+          <MoleculeModal.CloseTrigger>
+            <MoleculeModal.CloseIconButton ariaLabel="close the modal" />
+          </MoleculeModal.CloseTrigger>
+        </MoleculeModal.Content>
+      </MoleculeModal.Portal>
+    </MoleculeModal>
+  )
+}
+
+CustomContentWrapper.displayName = 'CustomContentWrapper'
+
+CustomContentWrapper.propTypes = {
+  actions: PropTypes.any,
+  content: PropTypes.any,
+  isOpen: PropTypes.bool,
+  setIsOpen: PropTypes.func
+}
+
+export default CustomContentWrapper

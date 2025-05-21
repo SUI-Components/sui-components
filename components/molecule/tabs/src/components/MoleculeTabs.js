8babe73c1db6e56c97be68abaef6a748c71501c5
jordi.munoz@adevinta.com <jordi.munoz@adevinta.com>
2025-05-21 10:05:09 +0200

    feat(molecule/tabs): improve accessibility with keyboard navigation and aria attributes

--- components/molecule/tabs/src/components/MoleculeTabs.js
+++ components/molecule/tabs/src/components/MoleculeTabs.js
@@ -1,4 +1,4 @@
-import {Children, cloneElement, isValidElement} from 'react'
+import {Children, cloneElement, isValidElement, useEffect, useRef} from 'react'
 
 import cx from 'classnames'
 import PropTypes from 'prop-types'
@@ -23,6 +23,59 @@ const MoleculeTabs = ({
   const isVerticalOrientation = type === TYPES.VERTICAL
 
   const [isIntersecting, outerRef] = useOnScreen()
+  const tabRefs = useRef([]) // To store refs of each tab item
+
+  // Initialize or clear tabRefs based on children length
+  useEffect(() => {
+    tabRefs.current = tabRefs.current.slice(0, childrenArray.length)
+  }, [childrenArray.length])
+
+  const focusTab = index => {
+    if (tabRefs.current[index]) {
+      tabRefs.current[index].focus()
+    }
+  }
+
+  const handleKeyDown = event => {
+    const activeTabIndex = childrenArray.findIndex(child => child.props.active)
+    let nextTabIndex = activeTabIndex
+
+    if (event.key === 'ArrowRight' && !isVerticalOrientation) {
+      nextTabIndex = (activeTabIndex + 1) % childrenArray.length
+    } else if (event.key === 'ArrowLeft' && !isVerticalOrientation) {
+      nextTabIndex = (activeTabIndex - 1 + childrenArray.length) % childrenArray.length
+    } else if (event.key === 'ArrowDown' && isVerticalOrientation) {
+      nextTabIndex = (activeTabIndex + 1) % childrenArray.length
+    } else if (event.key === 'ArrowUp' && isVerticalOrientation) {
+      nextTabIndex = (activeTabIndex - 1 + childrenArray.length) % childrenArray.length
+    } else if (event.key === 'Home') {
+      nextTabIndex = 0
+    } else if (event.key === 'End') {
+      nextTabIndex = childrenArray.length - 1
+    }
+
+    if (nextTabIndex !== activeTabIndex) {
+      event.preventDefault()
+      const nextTab = childrenArray[nextTabIndex]
+      if (nextTab && !nextTab.props.disabled) {
+        onChange(event, {numTab: nextTabIndex + 1})
+        focusTab(nextTabIndex)
+      } else {
+        // If the next tab is disabled, try to find the next available tab in that direction
+        let potentialNextTabIndex = nextTabIndex
+        const increment = event.key === 'ArrowRight' || event.key === 'ArrowDown' || event.key === 'End' ? 1 : -1
+        while (childrenArray[potentialNextTabIndex] && childrenArray[potentialNextTabIndex].props.disabled) {
+          potentialNextTabIndex = (potentialNextTabIndex + increment + childrenArray.length) % childrenArray.length
+          // Avoid infinite loop if all other tabs are disabled
+          if (potentialNextTabIndex === activeTabIndex) break
+        }
+        if (childrenArray[potentialNextTabIndex] && !childrenArray[potentialNextTabIndex].props.disabled) {
+          onChange(event, {numTab: potentialNextTabIndex + 1})
+          focusTab(potentialNextTabIndex)
+        }
+      }
+    }
+  }
 
   const extendedChildren = childrenArray
     .filter(child => isValidElement(child))
@@ -33,7 +86,8 @@ const MoleculeTabs = ({
         isIntersecting,
         numTab,
         id,
-        onChange
+        onChange,
+        ref: el => (tabRefs.current[index] = el) // Assign ref to each MoleculeTab
       })
     })
 
@@ -59,6 +113,7 @@ const MoleculeTabs = ({
         className={CLASS_SCROLLER}
         role="tablist"
         aria-orientation={isVerticalOrientation ? TYPES.VERTICAL : TYPES.HORIZONTAL}
+        onKeyDown={handleKeyDown} // Add keydown handler
       >
         {extendedChildren}
       </ul>

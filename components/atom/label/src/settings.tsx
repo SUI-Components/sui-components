aab3484c23b5b68410ef7e015473e5527e53cf35
andresin87 <andresin87@gmail.com>
2025-10-28 14:52:23 +0100

    feat(components/atom/label): add disabled prop

--- components/atom/label/src/settings.tsx
+++ components/atom/label/src/settings.tsx
@@ -1,3 +1,7 @@
+import cx from 'classnames'
+
+import type {AtomLabelProps} from './index'
+
 export const CLASSNAME = 'sui-AtomLabel'
 
 export const TYPES = {
@@ -14,3 +18,22 @@ export const FONT_SIZES = {
   SMALL: 'small',
   XSMALL: 'xsmall'
 } as const
+
+export const getClass = ({
+  inline,
+  type,
+  fontSize,
+  className,
+  disabled
+}: Pick<AtomLabelProps, 'inline' | 'type' | 'fontSize' | 'className' | 'disabled'>) =>
+  cx(
+    CLASSNAME,
+    {
+      [`${CLASSNAME}--disabled`]: Boolean(disabled),
+      [`${CLASSNAME}--${fontSize as string}`]: Boolean(fontSize),
+      [`${CLASSNAME}--${type as string}`]: Boolean(type),
+      [`${CLASSNAME}--inlineLeft`]: inline === 'left',
+      [`${CLASSNAME}--inlineRight`]: inline === 'right'
+    },
+    className
+  )

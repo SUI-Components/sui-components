b67180b886d99ad159559c96645fd20c22558fca
andresin87 <andresin87@gmail.com>
2026-04-22 13:40:11 +0200

    refactor(components/atom/icon): simplify ArticleDesign by removing unused state and RadioButtonGroup

--- components/atom/icon/demo/articles/ArticleDesign.js
+++ components/atom/icon/demo/articles/ArticleDesign.js
@@ -1,26 +1,14 @@
-import {Fragment, useState} from 'react'
+import {Fragment} from 'react'
 import {IconBrandGithub, IconBrandGithubFilled} from '@tabler/icons-react'
 
 import PropTypes from 'prop-types'
 
-import {
-  AntDesignIcon,
-  Article,
-  Cell,
-  Code,
-  Grid,
-  H2,
-  Label,
-  Paragraph,
-  RadioButton,
-  RadioButtonGroup
-} from '@s-ui/documentation-library'
+import {Article, Cell, Code, Grid, H2, Label, Paragraph} from '@s-ui/documentation-library'
 
 import AtomIcon, {ATOM_ICON_COLORS, ATOM_ICON_DESIGNS} from '../../src/index.js'
-import {flexCenteredStyle, ICONS} from '../settings.js'
+import {flexCenteredStyle} from '../settings.js'
 
 const ArticleDesign = ({className}) => {
-  const [selectedIcon, setIcon] = useState(Object.values(ICONS)[0])
   return (
     <Article className={className}>
       <H2>Design</H2>
@@ -32,22 +20,6 @@ const ArticleDesign = ({className}) => {
         <Code>design</Code> prop.
       </Paragraph>
       –––
-      <br />
-      <br />
-      <RadioButtonGroup onChange={(event, value) => setIcon(value)} value={selectedIcon}>
-        {Object.values(ICONS).map(({name, label}, index) => (
-          <RadioButton
-            key={index}
-            value={{name, label}}
-            aria-label={label}
-            label={
-              <AtomIcon>
-                <AntDesignIcon icon={name} style={{color: 'currentColor'}} />
-              </AtomIcon>
-            }
-          />
-        ))}
-      </RadioButtonGroup>
       <br />
       <br />
       <Grid cols={9} gutter={[8, 8]}>
